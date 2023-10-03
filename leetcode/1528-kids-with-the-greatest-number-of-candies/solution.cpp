9571bc15d62e665f1b0eea25e27c7b155b48ca61
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2023-10-03 17:38:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (100.00%)

---
+++ leetcode/1528-kids-with-the-greatest-number-of-candies/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool>result;
+        auto maxValue=max_element(candies.begin(),candies.end());
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]+extraCandies>=*maxValue) result.push_back(true);
+            else result.push_back(false);
+        }
+        return result;
+    }
+};
