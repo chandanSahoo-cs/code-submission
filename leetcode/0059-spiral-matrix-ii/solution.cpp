8a385f969d36af9603f76c5d05ddbada3a4b2ada
Chandan <chandansahoo02468@gmail.com>
2023-10-16 16:25:20 +0000

    [LeetCode Sync] - Spiral Matrix II - Runtime - 0 ms (100.00%), Memory - 6.9 MB (100.00%)

--- leetcode/0059-spiral-matrix-ii/solution.cpp
+++ leetcode/0059-spiral-matrix-ii/solution.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>ans(n,vector<int>(n));
-
-        int i=0,j=0,l=n-1,b=n-1;
+        vector<vector<int>>output(n,vector<int>(n,0));
         int num=1;
-        while(i<=b && j<=l){
-            for(int k=j;k<=l;k++){
-                ans[i][k]=num;
-                num++;
+        int rowBegin=0,rowEnd=n-1,columnBegin=0,columnEnd=n-1;
+        while(num<=n*n){
+            for(int i=columnBegin;i<=columnEnd;i++){
+                output[rowBegin][i]=num++;
             }
-            i++;
-
-            for(int k=i;k<=b;k++){
-                ans[k][l]=num;
-                num++;
+            rowBegin++;
+            for(int i=rowBegin;i<=rowEnd;i++){
+                output[i][columnEnd]=num++;
             }
-            l--;
-
-            for(int k=l;k>=j;k--){
-                ans[b][k]=num;
-                num++;
+            columnEnd--;
+            if(rowBegin<=rowEnd){
+                for(int i=columnEnd;i>=columnBegin;i--){
+                    output[rowEnd][i]=num++;
+                }
+                rowEnd--;
             }
-            b--;
-
-            for(int k=b;k>=i;k--){
-                ans[k][j]=num;
-                num++;
+            if(columnBegin<=columnEnd){
+                for(int i=rowEnd;i>=rowBegin;i--){
+                    output[i][columnBegin]=num++;
+                }
+                columnBegin++;
             }
-            j++;
         }
-
-        return ans;
+        return output;
     }
 };
