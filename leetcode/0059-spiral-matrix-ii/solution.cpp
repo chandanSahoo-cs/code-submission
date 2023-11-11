82311dfb5d6e6ada5a27c43dfecf19c8a9995cef
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2023-11-11 11:14:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7 MB (100.00%)

--- leetcode/0059-spiral-matrix-ii/solution.cpp
+++ leetcode/0059-spiral-matrix-ii/solution.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>output(n,vector<int>(n,0));
-        int num=1;
-        int rowBegin=0,rowEnd=n-1,columnBegin=0,columnEnd=n-1;
-        while(num<=n*n){
-            for(int i=columnBegin;i<=columnEnd;i++){
-                output[rowBegin][i]=num++;
+        int rowBegin = 0;
+        int colBegin = 0;
+        int rowEnd = n;
+        int colEnd = n;
+        int value = 1; // Start from 1, as you want to fill the matrix with consecutive numbers
+        vector<vector<int>> output(n, vector<int>(n, 0)); // Initialize the matrix with zeros
+
+        while (rowBegin < rowEnd && colBegin < colEnd) {
+            // Traverse the top row
+            for (int i = colBegin; i < colEnd; ++i) {
+                output[rowBegin][i] = value++;
             }
-            rowBegin++;
-            for(int i=rowBegin;i<=rowEnd;i++){
-                output[i][columnEnd]=num++;
+            ++rowBegin;
+
+            // Traverse the rightmost column
+            for (int i = rowBegin; i < rowEnd; ++i) {
+                output[i][colEnd - 1] = value++;
             }
-            columnEnd--;
-            if(rowBegin<=rowEnd){
-                for(int i=columnEnd;i>=columnBegin;i--){
-                    output[rowEnd][i]=num++;
+            --colEnd;
+
+            // Traverse the bottom row
+            if (rowBegin < rowEnd) {
+                for (int i = colEnd - 1; i >= colBegin; --i) {
+                    output[rowEnd - 1][i] = value++;
                 }
-                rowEnd--;
+                --rowEnd;
             }
-            if(columnBegin<=columnEnd){
-                for(int i=rowEnd;i>=rowBegin;i--){
-                    output[i][columnBegin]=num++;
+
+            // Traverse the leftmost column
+            if (colBegin < colEnd) {
+                for (int i = rowEnd - 1; i >= rowBegin; --i) {
+                    output[i][colBegin] = value++;
                 }
-                columnBegin++;
+                ++colBegin;
             }
         }
+
         return output;
     }
 };
+
