cd18a4bcc333e835927a4b65456f4f73ed6fa8e5
Chandan <chandansahoo02468@gmail.com>
2023-12-06 18:45:09 +0000

    [LeetCode Sync] - Guess Number Higher or Lower - Runtime - 2 ms (56.79%), Memory - 6.6 MB (100.00%)

--- leetcode/0374-guess-number-higher-or-lower/solution.cpp
+++ leetcode/0374-guess-number-higher-or-lower/solution.cpp
@@ -8,16 +8,14 @@
  */
 
 class Solution {
-#define ll long long
 public:
     int guessNumber(int n) {
-        ll s=1;
-        ll e=n;
-        ll mid;
-        while(s<=e){
-            mid=s+(e-s)/2;
-            if(guess(mid)==-1) e=mid-1;
-            else if(guess(mid)==1) s=mid+1;
+        if(n==1 or n==0) return n;
+        int start=1,end=n,mid=-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(guess(mid)==-1) end=mid-1;
+            else if(guess(mid)==1) start=mid+1;
             else break;
         }
         return mid;
