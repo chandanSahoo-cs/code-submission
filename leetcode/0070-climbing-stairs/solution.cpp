7fc3ef20a0bb0ec59c2bcbfa91a7ce4fcc5b2117
Chandan <chandansahoo02468@gmail.com>
2024-01-18 13:31:10 +0000

    [LeetCode Sync] - Climbing Stairs - Runtime - 2 ms (5.05%), Memory - 6.3 MB (100.00%)

--- leetcode/0070-climbing-stairs/solution.cpp
+++ leetcode/0070-climbing-stairs/solution.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    // int dp[50];
-
-    // int rec(int n, int i){
-        
-    //     if(i>n) return 0;
-    //     if(i==n) return 1;
-
-    //     if(dp[i]!=-1) return dp[i];
-
-    //     int ans = rec(n,i+1)+rec(n,i+2);
-
-    //     return dp[i] = ans;
-    // }
-
     int climbStairs(int n) {
-        vector<int>dp(n+2);
-        dp[n]=1;
-
-        for(int i=n-1;i>=0;i--){
-            dp[i] = dp[i+1]+dp[i+2];
+        int frstTrm=0;
+        int scndTrm=1;
+        int endTrm;
+        for(int i=0;i<n;i++){
+            endTrm=frstTrm+scndTrm;
+            frstTrm=scndTrm;
+            scndTrm=endTrm;
         }
-
-        return dp[0];
+        return scndTrm;
     }
 };
