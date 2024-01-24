1e3db6e931bf8835064d05b047cf17a66239dc20
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2024-01-24 11:41:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.2 MB (100.00%)

--- leetcode/0278-first-bad-version/solution.cpp
+++ leetcode/0278-first-bad-version/solution.cpp
@@ -4,13 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(n==0 or n==1) return n;
-        int start=1, end=n, mid=-1;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(!isBadVersion(mid))start=mid+1;
-            else if(isBadVersion(mid))end=mid-1;
+        int s=1,e=n,store;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(isBadVersion(m)){
+                store=m;
+                e=m-1;
+            }
+            else if(!isBadVersion(m)) s=m+1;
         }
-        return start;
+        return store;
     }
 };
