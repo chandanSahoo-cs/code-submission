d2add7cfd2e7cf1482197fe9694d1d29a20e39c0
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2024-01-24 11:08:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

--- leetcode/0069-sqrtx/solution.cpp
+++ leetcode/0069-sqrtx/solution.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0 or x==1) return x;
-        long long start=1, end=x, mid=-1;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(mid*mid>x) end=mid-1;
-            else if(mid*mid==x) return mid;
-            else start=mid+1;
+        long long store;
+        long long s=0;
+        long long e=x;
+        while(s<=e){
+            long long mid=s+(e-s)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if( mid*mid>x){
+                e=mid-1;
+            }
+            else{
+                store=mid;
+                s=mid+1;
+            }
         }
-        return round(end);
+        return store;
     }
 };
