a55f72a24c1b312388c3c78e38a84da577934c43
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2024-01-29 15:35:01 +0000

    [LeetCode Sync] Runtime - 68 ms (0.63%), Memory - 62 MB (100.00%)

--- leetcode/0882-peak-index-in-a-mountain-array/solution.cpp
+++ leetcode/0882-peak-index-in-a-mountain-array/solution.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(std::vector<int>& arr) {
-        auto mx = std::max_element(arr.begin(), arr.end());
-        return std::distance(arr.begin(), mx);
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int s = 0;
+        int e = nums.size() - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                s = mid + 1;
+            } else {
+                e = mid-1;
+            }
+        }
+        return s;
     }
 };
 
