9207667dd2aca1aeffe6817220876bb31cba9f57
Chandan <chandansahoo02468@gmail.com>
2024-01-30 20:04:31 +0530

    [Codeforces Sync] A - Brick Wall

---
+++ codeforces/A - Brick Wall/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//It is not matter of if, it is matter of when
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define testcases ll int tsts; cin >> tsts; while(tsts--)
+int main() {
+    fastio
+    testcases {
+        ll n,m; cin>>n>>m;
+        cout<<n*(m/2)<<"\n";
+    }
+    return 0;
+}
