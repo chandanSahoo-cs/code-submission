00e7407821c225e1840ed01494383476c20b901e
Chandan <chandansahoo02468@gmail.com>
2024-02-05 16:05:03 +0000

    [LeetCode Sync] - Find First and Last Position of Element in Sorted Array - Runtime - 7 ms (0.08%), Memory - 15.9 MB (100.00%)

--- leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,19 +1,36 @@
 class Solution {
+    #define ll long long
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>answer(2,-1);
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                answer[0]=i;
+        bool check =false;
+        int mnIndx=-1;
+        int mxIndx=-1;
+        ll s=0,e=nums.size()-1;
+        while(s<=e){
+            ll m=s+(e-s)/2;
+            if(nums[m]==target){
+                check=true;
                 break;
             }
+            else if(nums[m]<target) s=m+1;
+            else e=m-1;
         }
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]==target){
-                answer[1]=i;
-                break;
+        if(check){
+            ll s1=0,e1=nums.size()-1;
+            while(s1<=e1){
+                ll m1=s1+(e1-s1)/2;
+                if(nums[m1]>=target) e1=m1-1;
+                else s1=m1+1;
             }
+            mnIndx=s1;
+            ll s2=0,e2=nums.size()-1;
+            while(s2<=e2){
+                ll m2=s2+(e2-s2)/2;
+                if(nums[m2]<=target) s2=m2+1;
+                else e2=m2-1;
+            }
+            mxIndx=e2;
         }
-        return answer;
+        return {mnIndx,mxIndx};
     }
 };
