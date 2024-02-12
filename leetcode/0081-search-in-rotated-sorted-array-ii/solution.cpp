f51dca94e13e0007d2c56e6c1b9fb05d3cb10a96
Chandan <chandansahoo02468@gmail.com>
2024-02-12 16:17:11 +0000

    [LeetCode Sync] - Search in Rotated Sorted Array II - Runtime - 0 ms (100.00%), Memory - 17.9 MB (80.61%)

--- leetcode/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ leetcode/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -1,23 +1,34 @@
 class Solution {
+    #define ll long long
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0,r = n-1;
-         
-        while(l<=r){
-            int m = l+(r-l)/2;
-
-            if(nums[m]==target) return true;
-            else if(nums[l]==nums[m] && nums[m]==nums[r]) l++,r--;
-            else if(nums[l]<=nums[m]){
-                if(nums[l]<=target && target<nums[m]) r = m-1;
-                else l = m+1; 
-            }else{
-                if(nums[m]<target && target<=nums[r]) l = m+1;
-                else r = m-1; 
+        set<int>num1={nums.begin(),nums.end()};
+        nums={num1.begin(),num1.end()};
+        ll s=0,e=nums.size()-1;
+        ll temp=-1;
+        if(nums[s]>=nums[e] && nums.size()!=1) {
+            while(s<=e){
+                ll m=s+(e-s)/2;
+                if(nums[m]>=nums[s] && nums[m]>=nums[e] && nums[m]>nums[m+1]){
+                    temp=m;
+                    break;
+                }
+                else if (nums[m]<nums[s]) e=m-1;
+                else s=m+1;
+            }
+            if(nums[temp]>target && nums[nums.size()-1]>=target){
+                s=temp+1,e=nums.size()-1;
+            }
+            else{
+                s=0,e=temp;
             }
         }
-
+        while(s<=e){
+            ll m=s+(e-s)/2;
+            if(nums[m]==target) return true;
+            else if(nums[m]>target) e=m-1;
+            else s=m+1;
+        }
         return false;
     }
 };
