72d77134b211f62361dece3e37494dcda357163a
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2024-02-17 08:37:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.5 MB (100.00%)

--- leetcode/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ leetcode/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -2,11 +2,18 @@ class Solution {
     #define ll long long
 public:
     bool search(vector<int>& nums, int target) {
-        set<int>num1={nums.begin(),nums.end()};
-        nums={num1.begin(),num1.end()};
         ll s=0,e=nums.size()-1;
         ll temp=-1;
-        if(nums[s]>=nums[e] && nums.size()!=1) {
+        
+        while(s<e-1){
+            if(nums[s]==nums[e]){
+                if(nums[s] == target || nums[e] ==  target) return true;
+                s++;
+                e--;
+            }
+            else break;
+        }
+        if(nums[s]>nums[e] ) {
             while(s<=e){
                 ll m=s+(e-s)/2;
                 if(nums[m]>=nums[s] && nums[m]>=nums[e] && nums[m]>nums[m+1]){
