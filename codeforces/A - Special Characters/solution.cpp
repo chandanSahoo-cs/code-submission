c10293d4f58859350eaf8c179fd7564cd99db00f
Chandan <chandansahoo02468@gmail.com>
2024-03-16 13:35:29 +0530

    [Codeforces Sync] A - Special Characters

---
+++ codeforces/A - Special Characters/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+//It is not matter of if, it is matter of when
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define testcases ll int tsts; cin >> tsts; while(tsts--)
+int main() {
+    fastio
+    testcases {
+        ll n; cin>>n;
+        if(n%2!=0) cout<<"NO"<<"\n";
+        else{
+            ll temp=n/2;
+            string s="";
+            while(temp>0){
+                s+=static_cast<char>('Z'-temp);
+                s+=static_cast<char>('Z'-temp);
+                temp--;
+            }
+            cout<<"YES"<<"\n";
+            cout<<s<<"\n";
+        }
+    }
+    return 0;
+}
