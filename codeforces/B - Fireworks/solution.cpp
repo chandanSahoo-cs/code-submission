7a408705d34b41c38fe002eaa38a5ec5f0c269a1
Chandan <chandansahoo02468@gmail.com>
2024-03-20 13:21:46 +0530

    [Codeforces Sync] B - Fireworks

---
+++ codeforces/B - Fireworks/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//It is not matter of if, it is matter of when
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define testcases ll int tsts; cin >> tsts; while(tsts--)
+int main() {
+    fastio
+    testcases {
+        ll a,b,m; cin>>a>>b>>m;
+        cout<<(a+m)/a+(b+m)/b<<"\n";
+    }
+    return 0;
+}
