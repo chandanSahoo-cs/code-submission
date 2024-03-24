759c609c267b03897c5d9ad31416e06536503159
Chandan <chandansahoo02468@gmail.com>
2024-03-24 23:13:41 +0530

    [Codeforces Sync] A - Destroying Bridges

---
+++ codeforces/A - Destroying Bridges/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+//It is not matter of if, it is matter of when
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define testcases ll int tsts; cin >> tsts; while(tsts--)
+int main() {
+    fastio
+    testcases {
+        ll n,k; cin>>n>>k;
+        cout<<((n-1>k)?n:1)<<"\n";
+    }
+    return 0;
+}
