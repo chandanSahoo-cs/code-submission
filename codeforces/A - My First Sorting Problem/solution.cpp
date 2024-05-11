60b16e283c527d77d7a18ef41dff0af85499bd88
Chandan <chandansahoo02468@gmail.com>
2024-05-11 12:01:12 +0530

    [Codeforces Sync] A - My First Sorting Problem

---
+++ codeforces/A - My First Sorting Problem/solution.cpp
@@ -0,0 +1,15 @@
+//**It is not matter of if, it is matter of when**
+// #include <chandan/skills.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define testcases ll int tsts; cin >> tsts; while(tsts--)
+int main() {
+    fastio
+    testcases {
+        ll x,y; cin>>x>>y;
+        cout<<min(x,y)<<" "<<max(x,y)<<"\n";
+    }
+    return 0;
+}
