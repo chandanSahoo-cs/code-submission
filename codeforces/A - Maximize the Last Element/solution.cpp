5aca291c1139663a4e5356711e9f95d357eb4ac1
Chandan <chandansahoo02468@gmail.com>
2024-07-28 21:23:54 +0530

    [Codeforces Sync] A - Maximize the Last Element

---
+++ codeforces/A - Maximize the Last Element/solution.cpp
@@ -0,0 +1,28 @@
+/*It is not the matter of if, it is the matter of when*/
+/**/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define testcases ll tsts; cin >> tsts; while(tsts--)
+#define all(x) begin(x), end(x)
+// #define push_back(x) emplace_back(x);
+#define push(x) emplace(x);
+#define insert(x) emplace(x);
+ 
+int main() {
+    fastio
+    testcases {
+        ll n; cin>>n;
+        vector<ll>store(n);
+        for(auto &ele:store) cin>>ele;
+        ll mx=0;
+        for(ll i=0;i<n;i++){
+            if((i+1)%2!=0){
+                mx=max(mx,store[i]);
+            }
+        }
+        cout<<mx<<"\n";
+    }
+    return 0;
+}
