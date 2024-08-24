2f233d85c76a6ddbe48cfa709fd78a6168922b72
Chandan <chandansahoo02468@gmail.com>
2024-08-24 16:02:49 +0530

    [Codeforces Sync] A - Gregor and Cryptography

---
+++ codeforces/A - Gregor and Cryptography/solution.cpp
@@ -0,0 +1,45 @@
+/*It is not the matter of if, it the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define int long long
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+void solve()
+{
+    ll n; cin>>n;
+    cout<<2 << " " << (n/2)*2 <<"\n";
+}
+ 
+int32_t main()
+{
+    fastio
+ 
+        int T = 1;
+    cin >> T;
+ 
+    while (T--)
+        solve();
+ 
+    return 0;
+}
