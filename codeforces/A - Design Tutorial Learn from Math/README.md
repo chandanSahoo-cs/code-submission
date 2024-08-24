36f0adcea3a0b1bd0c9074f8f8bb9db4f581920f
Chandan <chandansahoo02468@gmail.com>
2024-08-24 12:51:17 +0530

    [Codeforces Sync] A - Design Tutorial: Learn from Math

---
+++ codeforces/A - Design Tutorial Learn from Math/README.md
@@ -0,0 +1,11 @@
+# [A. Design Tutorial: Learn from Math](https://codeforces.com/contest/472/problem/A)
+
+---
+One way to create a task is to learn from math. You can generate some random math statement or modify some theorems to get something new and build a new task from that.For example, there is a statement called the "Goldbach's conjecture". It says: "each even number no less than four can be expressed as the sum of two primes". Let's modify it. How about a statement like that: "each integer no less than 12 can be expressed as the sum of two composite numbers." Not like the Goldbach's conjecture, I can prove this theorem.You are given an integer n no less than 12, express it as a sum of two composite numbers.
+
+### Input
+InputThe only line contains an integer n (12 ≤ n ≤ 106).
+
+### Output
+OutputOutput two composite integers x and y (1 < x, y < n) such that x + y = n. If there are multiple solutions, you can output any of them.
+NoteIn the first example, 12 = 4 + 8 and both 4, 8 are composite numbers. You can output "6 6" or "8 4" as well.In the second example, 15 = 6 + 9. Note that you can't output "1 14" because 1 is not a composite number.
---
+++ codeforces/A - Design Tutorial Learn from Math/solution.cpp
@@ -0,0 +1,46 @@
+/*It is not the matter of if, it the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define int long long
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+void solve()
+{
+    ll n; cin>>n;
+    if(n%2==0) cout<<4<<" "<<n-4<<"\n";
+    else cout<<9<<" "<<n-9<<"\n";
+}
+ 
+int32_t main()
+{
+    fastio
+ 
+        int T = 1;
+    // cin >> T;
+ 
+    while (T--)
+        solve();
+ 
+    return 0;
+}
