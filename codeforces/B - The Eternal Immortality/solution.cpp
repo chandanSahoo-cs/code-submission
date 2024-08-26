04385f77b13750e84980071214971c2d759b01e9
Chandan <chandansahoo02468@gmail.com>
2024-08-26 23:23:04 +0530

    [Codeforces Sync] B - The Eternal Immortality

---
+++ codeforces/B - The Eternal Immortality/solution.cpp
@@ -0,0 +1,68 @@
+/*It is not the matter of if, it the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)             \
+    ({                            \
+        ll res = 1;              \
+        ll base = (a);           \
+        ll exp = (b);            \
+        while (exp > 0)           \
+        {                         \
+            if (exp % 2 == 1)     \
+                res *= base;      \
+            base *= base;         \
+            exp /= 2;             \
+        }                         \
+        res;                      \
+    })
+ 
+void solve()
+{
+    ll a,b; cin>>a>>b;
+    if(b-a==0) cout<<1<<"\n";
+    else if(b-a>=5 || b%10==0 ) cout<<0<<"\n";
+    else{
+        ll ans=1;
+        for(ll i=0;i<b-a;i++){
+            ans*=(b-i)%10;
+        }
+        cout<<ans%10<<"\n";
+    }
+}
+ 
+int32_t main()
+{
+    fastio
+ 
+        int T = 1;
+    // cin >> T;
+ 
+    while (T--)
+        solve();
+ 
+    return 0;
+}
