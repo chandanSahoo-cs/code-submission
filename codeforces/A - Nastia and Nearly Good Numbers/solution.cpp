6834303109c88c97eeafdafc4853423aaba6f25d
Chandan <chandansahoo02468@gmail.com>
2024-08-26 22:55:15 +0530

    [Codeforces Sync] A - Nastia and Nearly Good Numbers

---
+++ codeforces/A - Nastia and Nearly Good Numbers/solution.cpp
@@ -0,0 +1,69 @@
+/*It is not the matter of if, it the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)             \
+    ({                            \
+        ll res = 1;              \
+        ll base = (a);           \
+        ll exp = (b);            \
+        while (exp > 0)           \
+        {                         \
+            if (exp % 2 == 1)     \
+                res *= base;      \
+            base *= base;         \
+            exp /= 2;             \
+        }                         \
+        res;                      \
+    })
+ 
+void solve()
+{
+    ll a,b; cin>>a>>b;
+    if(a==1){
+        a=b+1;
+    }
+    if(b!=1){
+        cout<<"YES"<<"\n";
+        cout<<a*b<<" "<<a<<" "<<a*b+a<<"\n";
+    }
+    else{
+        cout<<"NO"<<"\n";
+    }
+}
+ 
+int32_t main()
+{
+    fastio
+ 
+        int T = 1;
+    cin >> T;
+ 
+    while (T--)
+        solve();
+ 
+    return 0;
+}
