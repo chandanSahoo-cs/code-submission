e09afa45aa39541df88983e5aa8d1fe6246ce1ee
Chandan <chandansahoo02468@gmail.com>
2024-08-31 18:33:23 +0530

    [Codeforces Sync] A - One and Two

---
+++ codeforces/A - One and Two/solution.cpp
@@ -0,0 +1,82 @@
+/*It is not the matter of if, it is the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)             \
+    ({                            \
+        int res = 1;              \
+        int base = (a);           \
+        int exp = (b);            \
+        while (exp > 0)           \
+        {                         \
+            if (exp % 2 == 1)     \
+                res *= base;      \
+            base *= base;         \
+            exp /= 2;             \
+        }                         \
+        res;                      \
+    })
+ 
+ 
+void solve()
+{
+    ll n; cin>>n;
+    vector<ll>store(n);
+    ll cnt2=0;
+    for(auto &ele:store){
+        cin>>ele;
+        if(ele==2) cnt2++;
+    }
+    if(cnt2%2==0){
+        cnt2=cnt2/2;
+    }
+    else{
+        cout<<-1<<"\n";
+        return;
+    }
+    for(ll i=0;i<n;i++){
+        if(store[i]==2) --cnt2;
+        if(!cnt2){
+            cout<<i+1<<"\n";
+            break;
+        }
+    }
+ 
+}
+ 
+int32_t main()
+{
+    fastio
+ 
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
+ 
