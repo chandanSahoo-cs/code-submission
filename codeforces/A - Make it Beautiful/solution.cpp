d728b533eefef7d0daf2de46dfcda71a034a2289
Chandan <chandansahoo02468@gmail.com>
2024-09-01 13:16:33 +0530

    [Codeforces Sync] A - Make it Beautiful

---
+++ codeforces/A - Make it Beautiful/solution.cpp
@@ -0,0 +1,86 @@
+/*It is not the matter of if, it is the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)             \
+    ({                            \
+        int res = 1;              \
+        int base = (a);           \
+        int exp = (b);            \
+        while (exp > 0)           \
+        {                         \
+            if (exp % 2 == 1)     \
+                res *= base;      \
+            base *= base;         \
+            exp /= 2;             \
+        }                         \
+        res;                      \
+    })
+ 
+ 
+void solve()
+{
+    ll n; cin>>n;
+    vector<ll>store(n);
+    for(auto &ele:store) cin>>ele;
+    set<ll,greater<ll>>st;
+    map<ll,ll>mp;
+    for(auto &ele:store){
+        st.insert(ele);
+        mp[ele]++;
+    }
+    if(st.size()==1){
+        cout<<"NO"<<"\n";
+        return;
+    }
+    else{
+        cout<<"YES"<<"\n";
+    }
+    for(auto ele:st) cout<<ele<<" ";
+    for(auto ele:mp){
+        ele.second--;
+        while(ele.second){
+            cout<<ele.first<<" ";
+            ele.second--;
+        }
+    }
+    cout<<"\n";
+}
+ 
+ 
+int32_t main()
+{
+    fastio
+ 
+    int T;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
+ 
