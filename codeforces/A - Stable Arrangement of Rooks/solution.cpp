dc01e8f9c671d03d448e480751d3b636591a66d3
Chandan <chandansahoo02468@gmail.com>
2024-09-08 17:41:13 +0530

    [Codeforces Sync] A - Stable Arrangement of Rooks

---
+++ codeforces/A - Stable Arrangement of Rooks/solution.cpp
@@ -0,0 +1,76 @@
+/*It is not the matter of if, it is the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+#define int long long
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)             \
+    ({                            \
+        int res = 1;              \
+        int base = (a);           \
+        int exp = (b);            \
+        while (exp > 0)           \
+        {                         \
+            if (exp % 2 == 1)     \
+                res *= base;      \
+            base *= base;         \
+            exp /= 2;             \
+        }                         \
+        res;                      \
+    })
+ 
+void solve()
+{
+    ll a,b; cin>>a>>b;
+    if((a+1)/2<b) cout<<-1<<"\n";
+    else{
+        ll row=0,col=0;
+        for(ll i=0;i<a;i++){
+            for(ll j=0;j<a;j++){
+                if(i==row && j==col && b){
+                    cout<<'R';
+                    row+=2,col+=2;
+                    b--;
+                }
+                else{
+                    cout<<'.';
+                }
+            }
+            cout<<"\n";
+        }
+    }
+}
+ 
+int32_t main()
+{
+    fastio
+    int T=1;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
