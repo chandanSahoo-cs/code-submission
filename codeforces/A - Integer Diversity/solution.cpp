ed853913bb0eb44cd31bb0d9ad7c7927517e3072
Chandan <chandansahoo02468@gmail.com>
2024-09-14 14:07:02 +0530

    [Codeforces Sync] A - Integer Diversity

---
+++ codeforces/A - Integer Diversity/solution.cpp
@@ -0,0 +1,71 @@
+/*It is not the matter of if, it is the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+#define int long long
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)         \
+    ({                        \
+        int res = 1;          \
+        int base = (a);       \
+        int exp = (b);        \
+        while (exp > 0)       \
+        {                     \
+            if (exp % 2 == 1) \
+                res *= base;  \
+            base *= base;     \
+            exp /= 2;         \
+        }                     \
+        res;                  \
+    })
+ 
+ 
+void solve()
+{
+    ll n; cin>>n;
+    vector<ll>store(n);
+    map<ll,ll>mp;
+    for(auto &ele:store) cin>>ele,mp[abs(ele)]++;
+    ll cnt=0;
+    for(auto ele:mp){
+        if(ele.first==0) cnt++;
+        else{
+            if(ele.second>=2) cnt+=2;
+            else cnt++;
+        }
+    }
+    cout<<cnt<<"\n";
+}
+ 
+int32_t main()
+{
+    fastio int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
