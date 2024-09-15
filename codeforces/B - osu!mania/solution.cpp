006f6188897fc65c843b56b588db507ba5034647
Chandan <chandansahoo02468@gmail.com>
2024-09-15 23:08:31 +0530

    [Codeforces Sync] B - osu!mania

---
+++ codeforces/B - osu!mania/solution.cpp
@@ -0,0 +1,69 @@
+/*It is not the matter of if, it is the matter of when*/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+ 
+#define int long long
+#define ll long long
+#define all(x) begin(x), end(x)
+ 
+#define SIEVE(MAXN)                                   \
+    vector<bool> isPrime(MAXN + 1, true);             \
+    void sieveOfEratosthenes()                        \
+    {                                                 \
+        isPrime[0] = isPrime[1] = false;              \
+        for (ll i = 2; i * i <= MAXN; i++)            \
+        {                                             \
+            if (isPrime[i])                           \
+            {                                         \
+                for (ll j = i * i; j <= MAXN; j += i) \
+                {                                     \
+                    isPrime[j] = false;               \
+                }                                     \
+            }                                         \
+        }                                             \
+    }
+ 
+#define BIN_EXP(a, b)             \
+    ({                            \
+        int res = 1;              \
+        int base = (a);           \
+        int exp = (b);            \
+        while (exp > 0)           \
+        {                         \
+            if (exp % 2 == 1)     \
+                res *= base;      \
+            base *= base;         \
+            exp /= 2;             \
+        }                         \
+        res;                      \
+    })
+ 
+void solve()
+{
+    ll n; cin>>n;
+    vector<string>store(n);
+    for(auto &ele:store) cin>>ele;
+    vector<ll>ans;
+    for(ll i=0;i<n;i++){
+        ll temp=store[i].find('#')+1;
+        ans.push_back(temp);
+    }
+    reverse(all(ans));
+    for(auto ele:ans) cout<<ele<<" ";
+    cout<<"\n";
+}
+ 
+int32_t main()
+{
+    fastio
+    int T=1;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
