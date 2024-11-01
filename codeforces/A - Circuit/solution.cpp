366febff292021a6129e6c1a982ca7252f227c62
Chandan <chandansahoo02468@gmail.com>
2024-11-01 19:49:58 +0530

    [Codeforces Sync] A - Circuit

---
+++ codeforces/A - Circuit/solution.cpp
@@ -0,0 +1,67 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+  ll cnt1=0;
+  for(ll i=0;i<2*n;i++){
+    ll ele; cin>>ele;
+    if(ele==1) cnt1++;
+  }
+  deb(cnt1);
+  ll mn=-1;
+  ll mx=-1;
+  if(cnt1>n){
+    cnt1=(2*n-cnt1);
+  }
+  deb(cnt1);
+  if(cnt1%2==0){
+    mn = 0;
+    mx = cnt1;
+  }
+  else{
+    mn = 1;
+    mx = cnt1;
+  }
+  cout<<mn<<" "<<mx<<"\n";  
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
