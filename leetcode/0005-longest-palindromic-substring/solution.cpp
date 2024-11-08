0d0216a736b7796c7624dc60f092b201366ede1c
Chandan <chandansahoo02468@gmail.com>
2024-11-08 20:13:35 +0000

    [LeetCode Sync] - Longest Palindromic Substring - Runtime - 8 ms (68.13%), Memory - 11 MB (47.39%)

--- leetcode/0005-longest-palindromic-substring/solution.cpp
+++ leetcode/0005-longest-palindromic-substring/solution.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    pair<int,int> compute(string &s, int l, int r){
-        int n = s.size();
-        while(l>=0 && r<=n-1 && s[l]==s[r]) l--, r++;
-
-        return {l+1,r-1};
-    }
-
     string longestPalindrome(string s) {
         int n = s.size();
-
-        int l=-1,r=-1,mx=0;
-
-        for(int i=0;i<n;i++){
-            auto [l1,r1] = compute(s,i,i);
-
-            if(r1-l1+1>mx){
-                mx = r1-l1+1;
-                l=l1,r=r1;
+        string ans(1,s[0]);
+        for(int i=0;i<n-1;i++){
+            //For even
+            int l = i;
+            int r = i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                l--;
+                r++;
+            }
+            l++;
+            r--;
+            if(l>=0 && r<n && ans.size()<r-l+1){
+                ans = s.substr(l,r-l+1);
             }
 
-            if(i>0){
-                auto [l2,r2] = compute(s,i-1,i);
-                if(r2-l2+1>mx){
-                    mx = r2-l2+1;
-                    l=l2,r=r2;
-                }
+            //For odd
+            l=i-1;
+            r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                l--;
+                r++;
+            }
+            l++;
+            r--;
+            if(l>=0 && r<n && ans.size()<r-l+1){
+                ans=s.substr(l,r-l+1);
             }
         }
-
-        return s.substr(l,r-l+1);
+        return ans;
     }
 };
