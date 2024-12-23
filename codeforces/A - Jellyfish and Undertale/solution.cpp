bef84fec4fed3f7a8da6c393408c19ec9c4d64f2
Chandan <chandansahoo02468@gmail.com>
2024-12-23 13:30:00 +0530

    [Codeforces Sync] A - Jellyfish and Undertale

---
+++ codeforces/A - Jellyfish and Undertale/solution.cpp
@@ -0,0 +1,56 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll a,b,n; cin>>a>>b>>n;
+  vector<ll>store(n);
+  for(auto &ele:store) cin>>ele;
+  ll sum=b;
+  for(auto ele:store){
+    if(1+ele<=a){
+        sum+=ele;
+    }
+    else sum+=a-1;
+  }
+  cout<<sum<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
