34514912670c22e5dbc46a201437532e41221c8f
Chandan <chandansahoo02468@gmail.com>
2024-12-26 09:32:47 +0000

    [LeetCode Sync] - Delete Node in a Linked List - Runtime - 7 ms (54.96%), Memory - 12.4 MB (56.44%)

--- leetcode/0237-delete-node-in-a-linked-list/solution.cpp
+++ leetcode/0237-delete-node-in-a-linked-list/solution.cpp
@@ -10,7 +10,6 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         node->val = node->next->val;
-        node->next = node->next->next;
-        // node=node->next;
+        node->next= node->next->next;
     }
 };
