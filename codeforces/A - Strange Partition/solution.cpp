f5f66853357f754ff341d924f8e762b39dbdfd50
Chandan <chandansahoo02468@gmail.com>
2024-12-28 12:14:44 +0530

    [Codeforces Sync] A - Strange Partition

---
+++ codeforces/A - Strange Partition/solution.cpp
@@ -0,0 +1,58 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n,x; cin>>n>>x;
+  vector<ll>a(n);
+  for(auto &ele:a) cin>>ele;
+  ll sumBefore = accumulate(all(a),0LL);
+  
+  sumBefore = (sumBefore+x-1)/x;  
+  for(auto &ele:a){
+    ele=(ele+x-1)/x;
+  }
+ 
+  ll sumAfter = accumulate(all(a),0LL);
+ 
+  cout<<sumBefore<<" "<<sumAfter<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
