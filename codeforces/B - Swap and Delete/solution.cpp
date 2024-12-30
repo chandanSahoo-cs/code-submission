a2074e78fb4f8256bad7f70c2cef2178928499a1
Chandan <chandansahoo02468@gmail.com>
2024-12-30 12:29:18 +0530

    [Codeforces Sync] B - Swap and Delete

---
+++ codeforces/B - Swap and Delete/solution.cpp
@@ -0,0 +1,89 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  string s; cin>>s;
+ 
+  ll n = (ll)s.size();
+ 
+  ll cnt0 = 0;
+  ll cnt1 = 0;
+ 
+  for(auto ele:s){
+    if(ele=='0') cnt0++;
+    else cnt1++;
+  }
+ 
+  string t = "";
+ 
+  for(auto ele:s){
+    if(ele=='0'){
+      if(cnt1) t+='1',cnt1--;
+      else {
+        cout<<cnt0<<"\n";
+        return;
+      }
+    }
+    else{
+      if(cnt0) t+='0', cnt0--;
+      else {
+        cout<<cnt1<<"\n";
+        return;
+      }
+    }
+  }
+ 
+  ll ans=0;
+ 
+  ll i=0,j=0;
+ 
+  while(i<n && j<n){
+    if(s[i]==t[j]){
+      ans++;
+    }
+    else i++;
+    j++;
+  }
+ 
+  cout<<ans<<"\n";  
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
