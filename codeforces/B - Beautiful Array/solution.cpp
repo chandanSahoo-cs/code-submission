bd3f7c94d3df9f4212e32b43b7ef0e6371778517
Chandan <chandansahoo02468@gmail.com>
2025-01-01 14:50:47 +0530

    [Codeforces Sync] B - Beautiful Array

---
+++ codeforces/B - Beautiful Array/solution.cpp
@@ -0,0 +1,79 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n,k,b,s; cin>>n>>k>>b>>s;
+  if(b>s/k){
+    cout<<-1<<"\n";
+    return;
+  }
+ 
+  vector<ll>a(n,0);
+  ll i=0;
+  ll t = ((b+1)*k)-1;
+  ll s1 = s;
+  while(s && i!=n){
+    if(s-t>=0){
+      a[i++] = t;
+      s-=t;
+      t = k-1;
+    }
+    else{
+      a[i++]=s;
+      s=0;
+    }
+  }
+  deb(a);
+  ll sum = accumulate(all(a),0LL);
+  deb(sum);
+  deb(s1);
+  if (sum==s1){
+    for(auto ele:a){
+      cout<<ele<<" ";
+    }
+    cout<<"\n";
+  }
+  else{
+    cout<<-1<<"\n";
+  } 
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
