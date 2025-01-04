ebe1a0183973c8f9246cbf7eb924bb325a65892c
Chandan <chandansahoo02468@gmail.com>
2025-01-04 12:28:56 +0530

    [Codeforces Sync] C - Double-ended Strings

---
+++ codeforces/C - Double-ended Strings/solution.cpp
@@ -0,0 +1,62 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  string a,b; cin>>a>>b;
+ 
+  ll n = a.size();
+  ll m = b.size();
+  deb(n,m);
+  ll mx=0;
+ 
+  for(ll i=0;i<n;i++){
+    for(ll j=i;j<n;j++){
+      string t = a.substr(i,j-i+1);
+      if(b.find(t)!=string::npos){
+        mx = max(mx,j-i+1);
+      }
+    }
+  }
+  deb(mx);
+  cout<<n+m-2*mx<<"\n";  
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
