eee60872abc0e852cf3f1b12b29e4f6f6b121d45
Chandan <chandansahoo02468@gmail.com>
2025-01-09 17:03:31 +0530

    [Codeforces Sync] B - Kill Demodogs

---
+++ codeforces/B - Kill Demodogs/solution.cpp
@@ -0,0 +1,47 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+  cout<<(337*((((n*(n+1))%mod*(4*n-1))%mod)))%mod<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
