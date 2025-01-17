efd2d5bf2dac41787736980922305d18e31ed89d
Chandan <chandansahoo02468@gmail.com>
2025-01-17 16:26:33 +0530

    [Codeforces Sync] D - Same Differences

---
+++ codeforces/D - Same Differences/README.md
@@ -0,0 +1,10 @@
+# [D. Same Differences](https://codeforces.com/contest/1520/problem/D)
+
+---
+You are given an array a of n integers. Count the number of pairs of indices (i,j) such that i<j and aj−ai=j−i.
+
+### Input
+InputThe first line contains one integer t (1≤t≤104). Then t test cases follow.The first line of each test case contains one integer n (1≤n≤2⋅105).The second line of each test case contains n integers a1,a2,…,an (1≤ai≤n) — array a.It is guaranteed that the sum of n over all test cases does not exceed 2⋅105.
+
+### Output
+OutputFor each test case output the number of pairs of indices (i,j) such that i<j and aj−ai=j−i.
---
+++ codeforces/D - Same Differences/solution.cpp
@@ -0,0 +1,75 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+  vector<ll>a(n);
+  unordered_map<ll,ll>mp;
+ 
+  for(ll i=0;i<n;i++){
+    cin>>a[i];
+    mp[a[i]-i-1]++;
+  }
+ 
+  ll cnt=0;
+ 
+  for(auto ele:mp){
+    cnt+=ele.second*(ele.second-1);
+  }  
+ 
+  cout<<cnt/2<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
