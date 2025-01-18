07d241678016b72a0db2b608015493c8a5597789
Chandan <chandansahoo02468@gmail.com>
2025-01-18 22:04:46 +0530

    [Codeforces Sync] B - Maximum Sum

--- codeforces/B - Maximum Sum/solution.cpp
+++ codeforces/B - Maximum Sum/solution.cpp
@@ -1,29 +1,77 @@
-#include <bits/stdc++.h>
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
 using namespace std;
-//It is not matter of if, it is matter of when
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) (a*b)/__gcd(a,b)
 #define ll long long
-#define testcases ll int tsts; cin >> tsts; while(tsts--)
-int main() {
-    fastio
-    testcases {
-        ll n,k,mod=1e9+7; cin>>n>>k;
-        vector<ll>store(n);
-        ll sum=0;
-        for(auto &ele:store) {
-            cin>>ele;
-            sum+=ele;
-        }
-        for(ll i=1;i<n;i++){
-            store[i]=max(store[i],store[i]+store[i-1]);
-        }
-        ll mx=*max_element(store.begin(),store.end())<0?0:*max_element(store.begin(),store.end());
-        for(ll i=0;i<k;i++){
-            sum=(sum+mx+mod)%mod;
-            mx=(mx+mx+mod)%mod;
-        }
-        sum=(sum+mod)%mod;
-        cout<<sum<<"\n";
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+    ll n,k; cin>>n>>k;
+    vector<ll>a(n);
+    for(auto &ele:a) cin>>ele;
+    sort(all(a));  
+    vector<ll>suff=a,pref=a;
+    deb(a);
+    for(ll i=1;i<n;i++){
+      pref[i]+=pref[i-1];
     }
+ 
+    for(ll i=n-2;i>=0;i--){
+      suff[i]+=suff[i+1];
+    }  
+ 
+    deb(pref);
+    deb(suff);
+ 
+    ll l=2*k-1;
+    ll r=n;
+ 
+    ll ans = -1;
+ 
+    while(r>=n-k){
+      ll left=0,right=0;
+      if(l>0) left = pref[l];
+      if(r<n) right = suff[r];
+      ans = max(ans,pref.back()-left-right);
+      l-=2;
+      r--;
+    }
+ 
+    cout<<ans<<"\n";
+}   
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
     return 0;
 }
