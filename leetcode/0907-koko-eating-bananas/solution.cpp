5297208bf9f643ca8b6171c737b288793ffc4c9d
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-01-31 18:18:24 +0000

    [LeetCode Sync] Runtime - 19 ms (11.31%), Memory - 23 MB (41.82%)

--- leetcode/0907-koko-eating-bananas/solution.cpp
+++ leetcode/0907-koko-eating-bananas/solution.cpp
@@ -1,17 +1,34 @@
+#define ll long long
+
 class Solution {
-    #define ll long long
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        ll s=1;
-        ll e=*max_element(piles.begin(),piles.end());
-        ll n=piles.size();
-        while(s<=e){
-            ll m=s+(e-s)/2;
-            ll sum=0;
-            for(auto &ele:piles) sum+=(ele+m-1)/m;
-            if(sum>h) s=m+1;
-            else e=m-1;
+    ll check(vector<int>&a,ll n,ll m){
+        ll hours = 0;
+        for(ll i=0;i<n;i++){
+            hours+=(a[i]+m-1)/m;
         }
-        return s;
+
+        return hours;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        ll mx = *max_element(piles.begin(),piles.end());
+        ll n = piles.size();
+        ll s=1,e=mx;
+        ll ans = LLONG_MAX;
+        while(s<=e){
+            ll m = s+(e-s)/2;
+            ll hours = check(piles,n,m);
+            cout<<m<<" "<<hours<<"\n";
+            if(hours<=h){
+                e=m-1;
+                ans = min(m,ans);
+            }
+            else{
+                s=m+1;
+            }
+        }
+
+        return ans;
     }
 };
