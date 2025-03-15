ecfea2823138f1df43f3c5acfac0cef6cab68344
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-03-15 10:39:57 +0000

    [LeetCode Sync] Runtime - 6 ms (24.66%), Memory - 16.5 MB (12.35%)

--- leetcode/0051-n-queens/solution.cpp
+++ leetcode/0051-n-queens/solution.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     vector<vector<string>>ans;
 
-    void fn(int n, int i, int j,vector<int>&c, vector<int>&di1, vector<int>&di2,vector<string>&s){
+    void rec(int n , vector<int>&r, vector<int>&c, vector<int>&di1, vector<int>&di2, vector<string>s,int i){
         if(i==n){
             ans.push_back(s);
             return;
         }
-        if(j==n) return;
 
-        if(!c[j] && !di1[i+j] && !di2[n-i+j]){
+        for(int j=0;j<n;j++){
+            if(c[j] || di1[i+j] || di2[n-i+j]) continue;
+
             s[i][j]='Q';
-            c[j]=1;
-            di1[i+j]=1;
-            di2[n-i+j]=1;
-            fn(n,i+1,0,c,di1,di2,s);
-            s[i][j]='.';
-            c[j]=0;
-            di1[i+j]=0;
-            di2[n-i+j]=0;
-        }
-        fn(n,i,j+1,c,di1,di2,s);
+            r[i]=c[j]=di1[i+j]=di2[n-i+j]=1;
 
+            rec(n,r,c,di1,di2,s,i+1);
+
+            s[i][j]='.';
+            r[i]=c[j]=di1[i+j]=di2[n-i+j]=0;
+        }
         return;
     }
 
-
     vector<vector<string>> solveNQueens(int n) {
-        vector<int>c(n),di1(2*n),di2(2*n);
+        vector<int>row(n),col(n),di1(2*n),di2(2*n);
         vector<string>s(n,string(n,'.'));
-        fn(n,0,0,c,di1,di2,s);
-        return ans;    
+        rec(n,row,col,di1,di2,s,0);
+        return ans;
     }
 };
