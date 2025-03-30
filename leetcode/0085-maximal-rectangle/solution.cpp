fd1816abb8f9b3f4d2f3e868fb74a1cd9e719251
Chandan <chandansahoo02468@gmail.com>
2025-03-30 16:55:50 +0000

    [LeetCode Sync] - Maximal Rectangle - Runtime - 10 ms (42.81%), Memory - 20.9 MB (38.42%)

--- leetcode/0085-maximal-rectangle/solution.cpp
+++ leetcode/0085-maximal-rectangle/solution.cpp
@@ -1,58 +1,55 @@
 class Solution {
 public:
-    int maxArea(vector<int>& keep){
-        int n = keep.size();
+
+    vector<int>left(int n, vector<int>&a){
 
         stack<int>st;
-
-        vector<int>next(n,n-1),prev(n,0);
-
-        for(int i=0;i<n;i++){
-            while(!st.empty() && keep[st.top()]>=keep[i]){
-                st.pop();   
-            }
-
-            if(!st.empty()) prev[i] = st.top()+1;
-            st.push(i);
-        }
-
-        st = stack<int>();
-
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && keep[st.top()]>=keep[i]){
-                st.pop();
-            }
-            if(!st.empty()) next[i] = st.top()-1;
-            st.push(i);
-        }
-
-        int ans = 0;
+        vector<int>ans(n,0);
 
         for(int i=0;i<n;i++){
-            int l = prev[i]==-1?i:prev[i];
-            int r = next[i]==-1?i:next[i];
-
-            ans=max(ans,(r-l+1)*keep[i]);
+            while(!st.empty() && a[st.top()]>=a[i]) st.pop();
+            if(!st.empty()) ans[i]=st.top()+1;
+            st.push(i);
         }
 
         return ans;
     }
 
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+    vector<int> right(int n, vector<int>&a){
+        stack<int>st;
+        vector<int>ans(n,n-1);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && a[st.top()]>=a[i]) st.pop();
+            if(!st.empty()) ans[i]=st.top()-1;
+            st.push(i);
+        }
 
-        int ans = 0;
-        vector<int>keep(m);
+        return ans;
+    }
+
+    int mx(int n, vector<int>&a){
+
+        vector<int>l = left(n,a);
+        vector<int>r = right(n,a);
+
+        int ans=-1;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1'){
-                    keep[j]+=1;
-                }else keep[j]=0;
-            }
+            ans = max(ans,a[i]*(i-l[i]+r[i]-i+1));
+        }
+        return ans;
+    }
 
-            ans = max(ans,maxArea(keep));
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int>sum(m,0);
+        int ans=-1;   
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='0') sum[j]=0;
+                else sum[j]+=(matrix[i][j]-'0');
+            }
+            ans=max(ans,mx(m,sum));
         }
 
         return ans;
