ee0f8fb95e74a40488bd60501454de97a23b2177
Chandan <chandansahoo02468@gmail.com>
2025-04-04 15:54:23 +0000

    [LeetCode Sync] - Count Number of Nice Subarrays - Runtime - 6 ms (70.59%), Memory - 71.5 MB (70.18%)

--- leetcode/1370-count-number-of-nice-subarrays/solution.cpp
+++ leetcode/1370-count-number-of-nice-subarrays/solution.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    int give(vector<int>& nums, int k){
-        int n = nums.size();
-        int i=0,j=0;
-
-        int ans=0;
-        int cnt=0;
-
-        while(j<n){
-            cnt+=(nums[j]&1);
-
+    int give(int n, vector<int>&nums, int k){
+        int l=0,r=0,cnt=0;
+        long long ans=0;
+        if(k<0) return 0; 
+        while(r<n){
+            if(nums[r]&1) cnt++;
             while(cnt>k){
-                cnt-=(nums[i]&1);
-                i++;
+                if(nums[l]&1) cnt--;
+                l++;
             }
-
-            ans+=j-i+1;
-            j++;
+            ans+=r-l+1;
+            r++;
         }
 
         return ans;
     }
 
     int numberOfSubarrays(vector<int>& nums, int k) {
-
-        return give(nums,k)-give(nums,k-1);
+        int n = nums.size();
+        return give(n,nums,k)-give(n,nums,k-1);
     }
 };
