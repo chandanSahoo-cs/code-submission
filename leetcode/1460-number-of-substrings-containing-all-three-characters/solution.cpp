03580f1edafe913d188b11c1a9403efb1ad10e73
Chandan <chandansahoo02468@gmail.com>
2025-04-04 17:27:29 +0000

    [LeetCode Sync] - Number of Substrings Containing All Three Characters - Runtime - 7 ms (64.68%), Memory - 10.9 MB (81.15%)

--- leetcode/1460-number-of-substrings-containing-all-three-characters/solution.cpp
+++ leetcode/1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int compute(string &s, int k){
-        int n = s.size();
-        int l = 0, r = 0;
+    int give(int n, string &s, int k){
+        int l=0,r=0,ans=0;
+        vector<int>ch(3,0);
 
-        vector<int>alpha(3);
-
-        int cnt = 0;
-        
         while(r<n){
-            alpha[s[r]-'a']++;
-
-            while((alpha[0]>0)+(alpha[1]>0)+(alpha[2]>0)>k){
-                alpha[s[l]-'a']--;
+            ch[s[r]-'a']++;
+            while(ch[0]>0 && ch[1]>0 && ch[2]>0){
+                ch[s[l]-'a']--;
                 l++;
+                ans+=n-r;
             }
-            
-            cnt+=r-l+1;
-
             r++;
         }
 
-        return cnt;
+        return ans;
     }
 
     int numberOfSubstrings(string s) {
         int n = s.size();
-        return (n*1LL*(n+1))/2-compute(s,2);
+        return give(n,s,3);
     }
 };
