d9bf70ffd72b6c99d4ac44d092976d7717ca1d70
Chandan <chandansahoo02468@gmail.com>
2025-04-12 17:59:34 +0000

    [LeetCode Sync] - Kth Largest Element in a Stream - Runtime - 12 ms (19.18%), Memory - 33.2 MB (25.34%)

--- leetcode/0789-kth-largest-element-in-a-stream/solution.cpp
+++ leetcode/0789-kth-largest-element-in-a-stream/solution.cpp
@@ -1,24 +1,17 @@
 class KthLargest {
 public:
-    priority_queue<int,vector<int>,greater<int>>pq;
-    int threshold;
-
+    priority_queue<int,vector<int>, greater<int>>pq;
     KthLargest(int k, vector<int>& nums) {
-        threshold = k;
-
-        for(auto ele:nums){
-            pq.push(ele);
-            if(pq.size()>threshold){
-                pq.pop();
-            }
-        }    
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
+        }
+        if(pq.size()<k) pq.push(INT_MIN);
     }
     
     int add(int val) {
         pq.push(val);
-
-        if(pq.size()>threshold) pq.pop();
-
+        pq.pop();
         return pq.top();
     }
 };
