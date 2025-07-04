d06205f210402e0945bd11c24faef5482c60e6d6
Chandan <chandansahoo02468@gmail.com>
2025-07-04 20:28:26 +0530

    [Codeforces Sync] C - Scoring Subsequences

---
+++ codeforces/C - Scoring Subsequences/solution.cpp
@@ -0,0 +1,60 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+    ll n; cin>>n;
+    vector<ll>a(n);
+    for(auto &ele:a) cin>>ele;
+    ll l=1;
+    
+    vector<ll>ans;
+    for(ll i=1;i<=n;i++){
+        while(l<=i && a[l-1]<i-l+1) l++;
+        deb(i-l+1,a[i-1]);
+        ans.push_back(i-l+1);
+    }
+ 
+    for(auto ele:ans){
+        cout<<ele<<" ";
+    }
+ 
+    cout<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
