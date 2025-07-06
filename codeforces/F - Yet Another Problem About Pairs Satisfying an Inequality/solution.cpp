3fd206eeb65cc960e6132a41ef535ac59572aadc
Chandan <chandansahoo02468@gmail.com>
2025-07-06 17:13:34 +0530

    [Codeforces Sync] F - Yet Another Problem About Pairs Satisfying an Inequality

---
+++ codeforces/F - Yet Another Problem About Pairs Satisfying an Inequality/solution.cpp
@@ -0,0 +1,62 @@
+/*It is not the matter of if, it is the matter of when*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+  vector<ll>a(n,0);
+  vector<ll>sml(n,0);
+  ll cnt=0;
+  ll ans=0;
+ 
+  for(ll i=0;i<n;i++){
+    cin>>a[i];
+    sml[i]=cnt;
+    if(i+1>a[i]){
+        cnt++;
+        deb(a[i],i+1);
+        if(a[i]!=0) ans+=sml[a[i]-1];
+    }
+  }
+ 
+  cout<<ans<<"\n";  
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
