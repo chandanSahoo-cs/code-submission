582a6a3b43a9069447a6373577cc5c68d21ea064
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-08-05 19:41:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 52.4 MB (20.28%)

--- leetcode/0055-jump-game/solution.cpp
+++ leetcode/0055-jump-game/solution.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
-    // bool jumpCheck(int i,int val,vector<int>&nums){
-    //     for(int j=1;j<=val;j++){
-    //         if(i+j==nums.size()-1){
-    //             return true;
-    //         }
-    //         else{
-    //             if(jumpCheck(i+j,nums[i+j],nums)){
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
-    // bool canJump(vector<int>& nums) {
-    //     if(nums.size()==1) return true;
-    //     if(nums[0]==nums.size()-1){
-    //         return true;
-    //     }
-    //     else{
-    //         return (jumpCheck(0,nums[0],nums));
-    //     }
-    // }
-    bool canJump(std::vector<int>& nums) {
-        int maxReachable = 0;
+    bool canJump(vector<int>& nums) {
         int n = nums.size();
-
-        for (int i = 0; i < n; ++i) {
-            if (i > maxReachable) return false;
-            maxReachable =max(maxReachable, i + nums[i]);
-            if (maxReachable >= n - 1) return true;
+        int jump=0;   
+        for(int i=0;i<n;i++){
+            jump=max(jump-1,nums[i]);
+            if(jump<0) return false;
+            if(i!=n-1 && jump==0) return false;
         }
-        return false;
+
+        return true;
     }
 };
