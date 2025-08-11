5f27eea7db52345586f9e3d70f5444d1bbbbad43
Chandan <chandansahoo02468@gmail.com>
2025-08-11 15:29:28 +0000

    [LeetCode Sync] - Binary Tree Preorder Traversal - Runtime - 0 ms (100.00%), Memory - 11 MB (44.68%)

--- leetcode/0144-binary-tree-preorder-traversal/solution.cpp
+++ leetcode/0144-binary-tree-preorder-traversal/solution.cpp
@@ -9,34 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ // Iterative Way
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        TreeNode* curr = root;
 
-        vector<int>ans;
-
-        while(curr!=nullptr){
-            if(curr->left==nullptr){
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }else{
-                TreeNode* temp = curr->left;
-
-                while(temp->right && temp->right!=curr){
-                    temp = temp->right;
-                }
-
-                if(temp->right){
-                    temp->right = nullptr;
-                    curr = curr->right;
-                }else{
-                    temp->right = curr;
-                    ans.push_back(curr->val);
-                    curr = curr->left;
-                }
-            }
+    void iterativePreorder(stack<TreeNode*>&st,vector<int>&ans){
+        if(st.empty()) return;
+        
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right!=nullptr) st.push(temp->right);
+            if(temp->left!=nullptr) st.push(temp->left);
         }
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        stack<TreeNode*>st;
+        if(root!=nullptr){
+            st.push(root);
+        }
+        iterativePreorder(st,ans);
 
         return ans;
     }
