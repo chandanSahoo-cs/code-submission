e38bad57473128da6095ce047fabf08ef67f8084
Chandan <chandansahoo02468@gmail.com>
2025-08-14 11:56:43 +0000

    [LeetCode Sync] - Count Complete Tree Nodes - Runtime - 0 ms (100.00%), Memory - 31.4 MB (10.76%)

--- leetcode/0222-count-complete-tree-nodes/solution.cpp
+++ leetcode/0222-count-complete-tree-nodes/solution.cpp
@@ -11,55 +11,16 @@
  */
 class Solution {
 public:
-    int giveLen(TreeNode* root){
-        if(root==nullptr) return 0;
-        return 1+giveLen(root->left);
-    }
+    int traverse(TreeNode* node){
+        if(node==nullptr) return 0;
 
-    bool find(TreeNode* root, int curr, unordered_set<int>&parent,int key){
-        if(curr==key) return true;
+        int left=traverse(node->left);
+        int right=traverse(node->right);
 
-        int lft = 2*curr;
-        int rght = 2*curr+1;
-
-        if(root->left && parent.count(lft)){
-            return find(root->left,lft,parent,key);
-        }
-
-        if(root->right && parent.count(rght)){
-            return find(root->right,rght,parent,key);
-        }
-
-        return false;
+        return left+right+1;
     }
 
     int countNodes(TreeNode* root) {
-        int len = giveLen(root);
-        if(len==0) return 0;
-
-        int nodes = (1<<(len-1))-1;
-
-        int l = nodes+1, r = (1<<len)-1;
-
-        int mx = l;
-
-        while(l<=r){
-            int m = l+(r-l)/2;
-            int s = m;
-
-            unordered_set<int>parent;
-
-            while(s){
-                parent.insert(s);
-                s = s/2;
-            }
-
-            if(find(root,1,parent,m)){
-                mx = m;
-                l=m+1;
-            }else r=m-1;
-        }
-
-        return mx;
+        return traverse(root);
     }
 };
