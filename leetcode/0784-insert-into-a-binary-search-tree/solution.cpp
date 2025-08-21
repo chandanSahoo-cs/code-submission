25b670ca3ab1e9a17d1c9f5063c4afe02599906f
Chandan <chandansahoo02468@gmail.com>
2025-08-21 16:22:52 +0000

    [LeetCode Sync] - Insert into a Binary Search Tree - Runtime - 0 ms (100.00%), Memory - 59.4 MB (59.09%)

--- leetcode/0784-insert-into-a-binary-search-tree/solution.cpp
+++ leetcode/0784-insert-into-a-binary-search-tree/solution.cpp
@@ -12,23 +12,26 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        TreeNode* curr = root;
-        if(!curr) return new TreeNode(val);
-
-        while(true){
-            if(curr->val<val){
-                if(curr->right) curr = curr->right;
-                else break;
-            }else{
-                if(curr->left) curr = curr->left;
-                else break;
-            }
+        TreeNode* node = root;
+        if(node==nullptr){
+            return new TreeNode(val);
         }
-
-        if(curr->val<val){
-            curr->right = new TreeNode(val);
-        }else{
-            curr->left = new TreeNode(val);
+        while(node!=nullptr){
+            if(val>node->val){
+                if(node->right!=nullptr){
+                    node=node->right;
+                }else{
+                    node->right=new TreeNode(val);
+                    break;
+                }
+            }else{
+                if(node->left!=nullptr){
+                    node=node->left;
+                }else{
+                    node->left= new TreeNode(val);
+                    break;
+                }
+            }
         }
 
         return root;
