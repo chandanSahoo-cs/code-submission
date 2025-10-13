e6b857bc7dbd2015962e75b2162c45fe6024db48
Chandan <chandansahoo02468@gmail.com>
2025-10-13 15:33:27 +0000

    [LeetCode Sync] - Triangle - Runtime - 0 ms (100.00%), Memory - 12.6 MB (83.26%)

--- leetcode/0120-triangle/solution.cpp
+++ leetcode/0120-triangle/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
-    // int dp[205];
+    int dp[205];
 public:
     int minimumTotal(vector<vector<int>>& a) {
         int n = a.size();
-
-        vector<int>curr(205),prev(205);
+        memset(dp,-1,sizeof(dp));
 
         for(int i=n-1;i>=0;i--){
             int m = a[i].size();
-            for(int j=0;j<=m-1;j++){
-                if(i==n-1) curr[j] = a[i][j];
+            for(int j=0;j<=m;j++){
+                if(j==m) dp[j] = 1e9;
+                else if(i==n-1) dp[j] = a[i][j];
                 else{
-                    int ans = min(prev[j],prev[j+1]);
-                    curr[j]= ans+a[i][j];
+                    int ans = min(dp[j],dp[j+1]);
+                    dp[j]= ans+a[i][j];
                 }
             }
-            prev=curr;
-            curr.assign(205,0);
         }
-        return prev[0];    
+        return dp[0];    
     }
 };
