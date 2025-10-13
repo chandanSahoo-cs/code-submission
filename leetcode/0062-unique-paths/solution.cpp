d7ab0d79d22d347c8b3957f022f0d802fe4f274f
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-10-13 13:54:47 +0000

    [LeetCode Sync] - Unique Paths - Runtime - 0 ms (100.00%), Memory - 7.9 MB (86.68%)

--- leetcode/0062-unique-paths/solution.cpp
+++ leetcode/0062-unique-paths/solution.cpp
@@ -1,32 +1,20 @@
-#define ll long long
-
 class Solution {
+int dp[105][105];
 public:
 
+    int rec(int n, int m, int i, int j){
+        if(i==n || j==m) return 0;
+        if(i==n-1 && j==m-1) return 1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int ans = rec(n,m,i+1,j)+rec(n,m,i,j+1);
+
+        return dp[i][j] = ans;
+    }
+
     int uniquePaths(int m, int n) {
-        int mx = max(m-1,n-1);
-        int mn = min(m-1,n-1);
-
-        ll num = 1;
-        ll denom = 1;
-
-        vector<int>mark(mn+1,1);
-
-        for(int i = (mx+mn);i>=mx+1;i--){
-            int carr = i;
-            for(int j=mn;j>=1;j--){
-                if(mark[j] && carr%j==0){
-                    carr/=j;
-                    mark[j]=0;
-                }
-            }
-            num*=carr;
-        }
-
-        for(int i=1;i<=mn;i++){
-            if(mark[i]) denom*=i;
-        }
-
-        return num/denom;
+        memset(dp,-1,sizeof(dp));
+        return rec(n,m,0,0);
     }
 };
