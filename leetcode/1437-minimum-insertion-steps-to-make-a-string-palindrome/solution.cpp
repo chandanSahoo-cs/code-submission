8704b9bc4fee8eebb7f579810f77ed74be7bbafa
Chandan <chandansahoo02468@gmail.com>
2025-10-15 13:49:14 +0000

    [LeetCode Sync] - Minimum Insertion Steps to Make a String Palindrome - Runtime - 12 ms (97.06%), Memory - 9.9 MB (85.92%)

--- leetcode/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.cpp
+++ leetcode/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.cpp
@@ -1,18 +1,23 @@
 class Solution {
-public:
     int dp[505][505];
+public:
 
     int rec(string &s, int l, int r){
-        if(l>=r) return 0;
+        if(l>r) return 0;
+
         if(dp[l][r]!=-1) return dp[l][r];
 
-        int ans = min(rec(s,l+1,r),rec(s,l,r-1))+1;
+        int ans = 1e9;
 
         if(s[l]==s[r]){
-            ans = min(ans,rec(s,l+1,r-1));
+            ans = rec(s,l+1,r-1);
+        }else{
+            ans = min(ans,1+rec(s,l+1,r));
+            ans = min(ans,1+rec(s,l,r-1));
         }
 
         return dp[l][r] = ans;
+
     }
 
     int minInsertions(string s) {
