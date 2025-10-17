176546287f4a2f012ac111e609e7ef0be901401f
Chandan <chandansahoo02468@gmail.com>
2025-10-17 16:28:49 +0000

    [LeetCode Sync] - Edit Distance - Runtime - 52 ms (11.17%), Memory - 11.8 MB (85.24%)

--- leetcode/0072-edit-distance/solution.cpp
+++ leetcode/0072-edit-distance/solution.cpp
@@ -1,28 +1,33 @@
 class Solution {
-public:
     int dp[505][505];
-    
-    int rec(string &word1, string &word2, int i, int j){
-        int n = word1.size(), m = word2.size();
+public:
 
-        if(i>=n && j>=m) return 0;
+    int rec(string &s1, string &s2, int i,int j){
+
+        int n = s1.size();
+        int m = s2.size();
+
+        if(i==n) return m-j;
+        if(j==m) return n-i;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        int mn = 1e5;
+        int ans = 0;
 
-        if(i<n) mn = min(mn,1+rec(word1,word2,i+1,j));
-        if(j<m){
-            if(i<n) mn = min(mn,(word1[i]!=word2[j])+rec(word1,word2,i+1,j+1));
-            mn = min(mn,1+rec(word1,word2,i,j+1));
+        if(s1[i]==s2[j]){
+            ans = rec(s1,s2,i+1,j+1);
+        }else{
+            ans = min({1+rec(s1,s2,i,j+1),1+rec(s1,s2,i+1,j),1+rec(s1,s2,i+1,j+1)});
         }
 
-        return dp[i][j] = mn;
+
+        return dp[i][j] = ans;
+
     }
 
-    int minDistance(string word1, string word2) {
+    int minDistance(string s1, string s2) {
         memset(dp,-1,sizeof(dp));
 
-        return rec(word1,word2,0,0);
+        return rec(s1,s2,0,0);
     }
 };
