8dc522627d838e288b2536b28738776ad79b0c26
Chandan <chandansahoo02468@gmail.com>
2025-10-24 15:27:07 +0000

    [LeetCode Sync] - Number of Islands - Runtime - 33 ms (19.59%), Memory - 19.3 MB (35.15%)

--- leetcode/0200-number-of-islands/solution.cpp
+++ leetcode/0200-number-of-islands/solution.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(),m = grid[0].size();
-
-        vector<vector<int>>vis(n,vector<int>(m));
-
-        int cnt=0;
+        int n = grid.size();
+        int m = grid[0].size();
 
         queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m));   
 
-        int dr[] = {0,0,-1,1};
-        int dc[] = {-1,1,0,0};
+        int cnt = 0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    cnt++;
+                if(vis[i][j] || grid[i][j]=='0') continue;
+                vis[i][j] = 1;
+                cnt++;
+                q.push({i,j});
 
-                    q.push({i,j});
-                    vis[i][j]=1;
+                while(!q.empty()){
+                    pair<int,int>pr = q.front();
+                    int r = pr.first;
+                    int c = pr.second;
+                    q.pop();
+                    
+                    
+                    if(c+1<m && grid[r][c+1]=='1' && !vis[r][c+1]) {
+                        q.push({r,c+1});
+                        vis[r][c+1]=1;
+                    }
 
-                    while(!q.empty()){
-                        auto [r,c] = q.front();
-                        q.pop();
+                    if(r+1<n && grid[r+1][c]=='1' && !vis[r+1][c]) {
+                        q.push({r+1,c});
+                        vis[r+1][c]=1;
+                    }
 
-                        for(int k=0;k<4;k++){
-                            int tr = r+dr[k];
-                            int tc = c+dc[k];
 
-                            if(tr<0 || tr>=n || tc<0 || tc>=m || grid[tr][tc]=='0' || vis[tr][tc]) continue;
-                            q.push({tr,tc});
-                            vis[tr][tc]=1;
-                        }
+                    if(c-1>=0 && grid[r][c-1]=='1' && !vis[r][c-1]){ 
+                        q.push({r,c-1});
+                        vis[r][c-1]=1;
+                    }
+
+                    if(r-1>=0 && grid[r-1][c]=='1' && !vis[r-1][c]){ 
+                        q.push({r-1,c});
+                        vis[r-1][c]=1;
                     }
                 }
             }
         }
+
 
         return cnt;
     }
