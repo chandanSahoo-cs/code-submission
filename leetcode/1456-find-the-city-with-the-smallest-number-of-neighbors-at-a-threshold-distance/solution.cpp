8c970209b3a9792fa5d3aeb89bce61b265ec02f3
Chandan <chandansahoo02468@gmail.com>
2025-11-15 17:29:33 +0000

    [LeetCode Sync] - Find the City With the Smallest Number of Neighbors at a Threshold Distance - Runtime - 26 ms (16.71%), Memory - 17 MB (55.13%)

--- leetcode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
+++ leetcode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
@@ -1,43 +1,46 @@
-const int INF = 1e9+7;
 class Solution {
+    const int INF=1e8;
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
         vector<vector<int>>adj(n,vector<int>(n,INF));
 
         for(auto ele:edges){
-            adj[ele[0]][ele[1]] = ele[2];
-            adj[ele[1]][ele[0]] = ele[2];
+            adj[ele[0]][ele[1]]=ele[2];
+            adj[ele[1]][ele[0]]=ele[2];
         }
 
         for(int i=0;i<n;i++){
             adj[i][i]=0;
         }
 
-        for(int k=0;k<n;k++){
+        for(int val=0;val<n;val++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    if(adj[i][k]==0 || adj[k][j]==0) continue;
-                    adj[i][j] = min(adj[i][j],adj[i][k]+adj[k][j]);
+                    if(i==val || j==val || adj[i][val]==INF || adj[val][j]==INF) continue;
+                    int curr = adj[i][val]+adj[val][j];
+                    if(adj[i][j]>curr){
+                        adj[i][j]=curr;
+                    }
                 }
             }
         }
 
-        int mnCnt=INT_MAX;
-        int city=-1;
+        vector<int>cnt(n,0);
 
         for(int i=0;i<n;i++){
-            int cnt=0;
             for(int j=0;j<n;j++){
-                if(adj[i][j]<=distanceThreshold) cnt++;
-            }
-
-            if(cnt<=mnCnt){
-                mnCnt = cnt;
-                city = i;
+                if(adj[i][j]<=distanceThreshold){
+                    cnt[i]++;
+                }
             }
         }
 
-        return city;
+        int mn = *min_element(cnt.begin(),cnt.end());
+
+        for(int i=n-1;i>=0;i--){
+            if(cnt[i]==mn) return i;
+        }
+
+        return -1;
     }
 };
