4a346c908d36a6dd514fc19f33a72e9b1cf82d62
Chandan <chandansahoo02468@gmail.com>
2025-11-30 15:16:05 +0530

    [Codeforces Sync] B - WOW Factor

---
+++ codeforces/B - WOW Factor/solution.cpp
@@ -0,0 +1,62 @@
+/*It is happening, right here and now*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) (a*b)/__gcd(a,b)
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+    string s; cin>>s;
+    ll n = s.size();
+ 
+    ll prev=0,curr=0,ans=0;
+ 
+    for(ll i=0;i<n;i++){
+        if(s[i]=='v'){
+            if(i-1>=0 && s[i-1]=='v'){
+                curr++;
+                ans+=prev;
+            }
+        }else{
+            prev+=curr;
+        }
+    }
+ 
+    cout<<ans<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    // cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
