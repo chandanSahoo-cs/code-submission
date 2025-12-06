de72821af541a7e7b20c8e67d2b086202fed9a75
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-06 12:52:06 +0000

    [LeetCode Sync] - Reshape the Matrix - Runtime - 0 ms (100.00%), Memory - 15.1 MB (76.02%)

--- leetcode/0566-reshape-the-matrix/solution.cpp
+++ leetcode/0566-reshape-the-matrix/solution.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int m=mat.size();
-        int n=mat[0].size();
-        if(m*n!=r*c) return mat;
-        vector<vector<int>> ans(r,(vector<int>(c)));
-        int row=0,col=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[row][col]=mat[i][j];
-                col++;
-                if(col==c){
-                    row++;
-                    col=0;
-                }
+        int n = mat.size();
+        int m = mat[0].size();
 
+        if(n*m!=r*c) return mat;
+
+        vector<vector<int>>ans(r,vector<int>(c));
+
+        int kr=0, kc=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[kr][kc]=mat[i][j];
+                kc++;
+                if(kc==c){
+                    kc=0;
+                    kr++;
+                    // if(kr==)
+                }
             }
         }
+
         return ans;
     }
 };
