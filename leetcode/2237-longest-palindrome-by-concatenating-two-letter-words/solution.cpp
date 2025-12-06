70377b1ef9869e30909be376fb28973e9cb602fc
Chandan <chandansahoo02468@gmail.com>
2025-12-06 21:09:54 +0000

    [LeetCode Sync] - Longest Palindrome by Concatenating Two Letter Words - Runtime - 130 ms (30.97%), Memory - 172 MB (25.37%)

--- leetcode/2237-longest-palindrome-by-concatenating-two-letter-words/solution.cpp
+++ leetcode/2237-longest-palindrome-by-concatenating-two-letter-words/solution.cpp
@@ -3,35 +3,64 @@ public:
     int longestPalindrome(vector<string>& words) {
         unordered_map<string,int>mp;
 
-        for(auto ele:words) mp[ele]++;
-
-        int mxOdd=0;
-        int cnt=0;
-
-        for(auto [key,value]:mp){
-            string rKey = key;
-            reverse(rKey.begin(),rKey.end());
-
-            if(mp.find(rKey)==mp.end()) continue;
-
-            int mn = min(value,mp[rKey]);
-
-            if(rKey==key){
-                if(mn%2){
-                    if(mxOdd<mn){
-                        swap(mxOdd,mn);
-                    }
-                    cnt+=max(0,mn-1);
-                }else{
-                    cnt+=mn;
-                }
-            }else{
-                cnt+=mn;
-            }
-
-            // mp[key]=0;
+        for(auto ele:words){
+            if(mp.find(ele)==mp.end()){
+                mp[ele]=1;
+            }else mp[ele]++;
         }
 
-        return 2*(cnt+mxOdd);
+        for(auto [key,value]:mp){
+            cout<<key<<" "<<value<<"\n";
+        }
+
+        int cnt=0;
+        int odd=0;
+
+        // for(auto [key,value]:words){
+        //     int scnd=0;
+
+        //     if(tr[0]==tr[1]){
+        //         if(value%2)
+        //     }
+        // }
+
+        for(auto ele:words){
+            if(mp[ele]==0) continue;
+            int frst=mp[ele];
+            int scnd=0;
+            
+            string tr = "";
+            tr.push_back(ele[1]);
+            tr.push_back(ele[0]);
+
+            if(tr==ele){
+                if(frst%2){
+                    if(odd>frst){
+                        scnd=frst-1;
+                        frst--;
+                    }else{
+                        scnd=max(0,odd-1);
+                        odd=frst;
+                        frst=scnd;
+                    }
+                    // odd = max(odd,frst);
+                    // scnd=mn-1;
+                }else{
+                    scnd = frst;
+                }
+                cnt+=min(frst,scnd);
+
+            }else{
+                if(mp.find(tr)!=mp.end()){
+                    scnd=mp[tr];
+                }
+                cnt+=2*min(frst,scnd);
+            }
+            // cout<<frst<<" "<<scnd<<" "<<cnt<<"\n";
+            mp[ele]=0;
+            mp[tr]=0;
+        }
+
+        return 2*(cnt+odd);
     }
 };
