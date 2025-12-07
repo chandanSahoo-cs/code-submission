c2d243092110d84d5054480642d7db8a0d63d9c1
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-07 12:17:06 +0000

    [LeetCode Sync] - Valid Parentheses - Runtime - 0 ms (100.00%), Memory - 9 MB (37.61%)

--- leetcode/0020-valid-parentheses/solution.cpp
+++ leetcode/0020-valid-parentheses/solution.cpp
@@ -3,14 +3,16 @@ public:
     bool isValid(string s) {
         stack<char>st;
 
-        
-
         for(auto ele:s){
-            if(st.empty()) st.push(ele);
-            else if(st.top()=='(' && ele==')') st.pop();
-            else if(st.top()=='{' && ele=='}') st.pop();
-            else if(st.top()=='[' && ele==']') st.pop();
-            else st.push(ele);
+            if(st.empty() && (ele!=')' || ele!='}' || ele!=']')) st.push(ele);
+            else if(ele=='(' || ele=='{' || ele=='['){
+                st.push(ele);
+            }else{
+                if(st.top()=='(' && ele==')') st.pop();
+                else if(st.top()=='{' && ele=='}') st.pop();
+                else if(st.top()=='[' && ele==']') st.pop();
+                else break;    
+            }
         }
 
         return st.size()==0;
