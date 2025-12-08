52e42c8e6b4c6cd9cb71358265dc84dbe5cd2f6b
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 10:52:07 +0000

    [LeetCode Sync] - Pairs of Songs With Total Durations Divisible by 60 - Runtime - 3 ms (43.93%), Memory - 32 MB (68.07%)

--- leetcode/1055-pairs-of-songs-with-total-durations-divisible-by-60/solution.cpp
+++ leetcode/1055-pairs-of-songs-with-total-durations-divisible-by-60/solution.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         unordered_map<int,int>mp;
-        
-        int n = time.size();
 
-        for(int i=0;i<n;i++){
-            int tr = time[i]%60;
-            if(mp.find(tr)==mp.end()){
-                mp[tr]=1;
-            }else mp[tr]++;
+        for(auto ele:time){
+            mp[ele%60]++;
         }
-
 
         int cnt=0;
 
-        for(int i=0;i<n;i++){
-            int tr = (60-(time[i]%60))%60;
-            int cn = time[i]%60;
-
+        for(auto ele:time){
+            int tr = (60-(ele%60))%60;
+            mp[ele%60]--;
             if(mp.find(tr)!=mp.end()){
-                mp[cn]--;
                 cnt+=mp[tr];
-
-                // cout<<cn<<" "<<tr<<"\n";
             }
         }
 
