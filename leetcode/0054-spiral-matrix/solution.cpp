f58f7d6b3e73356ad9f2075a12e1f956a4b1a4c3
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 09:29:35 +0000

    [LeetCode Sync] - Spiral Matrix - Runtime - 0 ms (100.00%), Memory - 9.4 MB (50.06%)

--- leetcode/0054-spiral-matrix/solution.cpp
+++ leetcode/0054-spiral-matrix/solution.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size()-1;
-        int m = matrix[0].size()-1;
-
+        int n = matrix.size();
+        int m = matrix[0].size();
 
         int i=0,j=0;
         vector<int>ans;
 
-        while(i<=n && j<=m){
-            for(int k=j;k<=m;k++){
+        while(i<n && j<m){
+            for(int k=j;k<m;k++){
                 ans.push_back(matrix[i][k]);
             }
             i++;
-            cout<<i<<","<<n<<","<<j<<","<<m<<"\n";
-            if(i>n || j>m) break;
+            if(i>=n || j>=m) break;
 
-            for(int k=i;k<=n;k++){
-                ans.push_back(matrix[k][m]);
+            for(int k=i;k<n;k++){
+                ans.push_back(matrix[k][m-1]);
             }
             m--;
-            cout<<i<<","<<n<<","<<j<<","<<m<<"\n";
-            if(i>n || j>m) break;
+            if(i>=n || j>=m) break;
 
-            for(int k=m;k>=j;k--){
-                ans.push_back(matrix[n][k]);
+            for(int k=m-1;k>=j;k--){
+                ans.push_back(matrix[n-1][k]);
             }
             n--;
-            cout<<i<<","<<n<<","<<j<<","<<m<<"\n";
-            if(i>n || j>m) break;
+            if(i>=n || j>=m) break;
 
-            for(int k=n;k>=i;k--){
+            for(int k=n-1;k>=i;k--){
                 ans.push_back(matrix[k][j]);
             }
             j++;
-            cout<<i<<","<<n<<","<<j<<","<<m<<"\n";
-            if(i>n || j>m) break;
+            if(i>=n || j>=m) break;
         }
 
         return ans;
