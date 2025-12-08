8fe50883daa7c3c552bc813ff548fb6ff65d3b7f
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 14:47:11 +0000

    [LeetCode Sync] - Max Sum of a Pair With Equal Sum of Digits - Runtime - 35 ms (43.83%), Memory - 73.6 MB (94.93%)

--- leetcode/2473-max-sum-of-a-pair-with-equal-sum-of-digits/solution.cpp
+++ leetcode/2473-max-sum-of-a-pair-with-equal-sum-of-digits/solution.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int digSum(int n){
         int sum=0;
-
+        
         while(n>0){
-            sum+=n%10;
+            sum+=(n%10);
             n/=10;
         }
 
@@ -12,33 +12,30 @@ public:
     }
 
     int maximumSum(vector<int>& nums) {
-        int n = nums.size();
 
-        unordered_map<int,pair<int,int>>mp;
+        map<int,pair<int,int>>mp;
 
-        for(int i=0;i<n;i++){
-            int dig= digSum(nums[i]);
-            if(mp.find(dig)==mp.end()){
-                mp[dig]={-1,nums[i]};
+        for(auto ele:nums){
+            int sum = digSum(ele);
+
+            if(mp.find(sum)==mp.end()){
+                mp[sum] = {-ele-1,ele};
             }else{
-                if(nums[i]>=mp[dig].second){
-                    mp[dig].first=mp[dig].second;
-                    mp[dig].second=nums[i];
-                }else if(nums[i]>=mp[dig].first){
-                    mp[dig].first=nums[i];
+                if(mp[sum].second<=ele){
+                    mp[sum].first = mp[sum].second;
+                    mp[sum].second = ele;
+                }else if(mp[sum].first<ele){
+                    mp[sum].first=ele;
                 }
             }
         }
 
-        int mx=-1;
+        int mx = -1;
 
         for(auto [key,value]:mp){
-            if(value.first==-1) continue;
             mx = max(mx,value.first+value.second);
         }
 
-        return mx;
+        return mx;    
     }
-
-
 };
