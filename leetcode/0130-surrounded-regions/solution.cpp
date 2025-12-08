68b0acd48604497b3f76619daac5670758d3d174
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 16:55:38 +0000

    [LeetCode Sync] - Surrounded Regions - Runtime - 3 ms (25.03%), Memory - 15.8 MB (14.05%)

--- leetcode/0130-surrounded-regions/solution.cpp
+++ leetcode/0130-surrounded-regions/solution.cpp
@@ -1,48 +1,59 @@
 class Solution {
 public:
+    bool boundary(int i, int j, int n,int m){
+        if(i==0 || i==n-1 || j==0 || j==m-1) return true;
+        return false;
+    }
+
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
 
-        vector<vector<int>>vis(n,vector<int>(m));
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
         queue<pair<int,int>>q;
 
+        vector<int>dr = {-1,1,0,0};
+        vector<int>dc = {0,0,-1,1};
+
         for(int i=0;i<n;i++){
-            for(int  j=0;j<m;j++){
-                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O'){
-                    vis[i][j]=1;
+            for(int j=0;j<m;j++){
+                if(boundary(i,j,n,m) && board[i][j]=='O'){
                     q.push({i,j});
                 }
             }
         }
 
         while(!q.empty()){
-            pair<int,int>pr = q.front();
+            auto p = q.front();
             q.pop();
-            int f = pr.first;
-            int s = pr.second;
+            int i = p.first;
+            int j = p.second;
 
-            int dr[] = {0,0,-1,1};
-            int dc[] = {-1,1,0,0};
+            if(vis[i][j]!=0) continue;
+
+            if(board[i][j]=='X'){
+                vis[i][j]=1;
+            }else vis[i][j]=2;
 
             for(int k=0;k<4;k++){
-                int nr = f+dr[k];
-                int nc = s+dc[k];
+                int r = i+dr[k];
+                int c = j+dc[k];
 
-                if(nr<0 || nr>=n || nc<0 || nc>=m || vis[nr][nc] || board[nr][nc]=='X') continue;
-                vis[nr][nc] = 1;
-                q.push({nr,nc});
+                if(r<0 || r>=n || c<0 || c>=m) continue;
+                if(board[r][c]=='X') continue;
+                q.push({r,c});
             }
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j]){
+                if(vis[i][j]!=2){
                     board[i][j]='X';
                 }
             }
         }
 
-        return ;
+        return;    
     }
 };
