66a5a813d98dd2d396a6722a331562c27cf37df7
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 10:45:14 +0000

    [LeetCode Sync] - 4Sum II - Runtime - 95 ms (62.14%), Memory - 28 MB (95.94%)

--- leetcode/0454-4sum-ii/solution.cpp
+++ leetcode/0454-4sum-ii/solution.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    void mark(vector<int>&nums1, vector<int>&nums2, unordered_map<int,int>&fst){
-        int n=nums1.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int tr = nums1[i]+nums2[j];
-                if(fst.find(tr)==fst.end()){
-                    fst[tr]=1;
-                }else fst[tr]++;
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int>mp;
+
+        for(auto ele1:nums1){
+            for(auto ele2:nums2){
+                mp[ele1+ele2]++;
             }
         }
-    }
-
-    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int,int>scnd;
-        mark(nums3,nums4,scnd);
 
         int cnt=0;
-        int n=nums1.size();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int tr = nums1[i]+nums2[j];
-                if(scnd.find(-tr)!=scnd.end()){
-                    cnt+=scnd[-tr];
+        for(auto ele3:nums3){
+            for(auto ele4:nums4){
+                if(mp.find(-ele3-ele4)!=mp.end()){
+                    cnt+=mp[-ele3-ele4];
                 }
             }
         }
