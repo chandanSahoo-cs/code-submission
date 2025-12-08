da3e396adc0da6ab78ef18605f4371c853a9a997
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 14:09:33 +0000

    [LeetCode Sync] - Find Occurrences of an Element in an Array - Runtime - 6 ms (77.20%), Memory - 185.5 MB (53.64%)

--- leetcode/3420-find-occurrences-of-an-element-in-an-array/solution.cpp
+++ leetcode/3420-find-occurrences-of-an-element-in-an-array/solution.cpp
@@ -5,17 +5,15 @@ public:
         vector<int>store;
 
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==x) store.push_back(i);
+            if(x==nums[i]) store.push_back(i);
         }
 
         vector<int>ans;
 
         for(auto ele:queries){
-            if(ele-1>=store.size()){
-                ans.push_back(-1);
-            }else{
+            if(ele-1<store.size()){
                 ans.push_back(store[ele-1]);
-            }
+            }else ans.push_back(-1);
         }
 
         return ans;
