f22ed96f57738cc793a4683c2112adc3f75fbfd8
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-08 14:25:00 +0000

    [LeetCode Sync] - Find the Number of Distinct Colors Among the Balls - Runtime - 168 ms (35.91%), Memory - 171.7 MB (27.73%)

--- leetcode/3434-find-the-number-of-distinct-colors-among-the-balls/solution.cpp
+++ leetcode/3434-find-the-number-of-distinct-colors-among-the-balls/solution.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
         unordered_map<int,int>mark;
-        unordered_map<int,int>mp;
 
+        unordered_map<int,int>mp;
         vector<int>ans;
 
         for(auto ele:queries){
             int x = ele[0];
             int y = ele[1];
 
-            int cmp=-1;
             if(mark.find(x)!=mark.end()){
-                cmp = mark[x];
-            }
-
-            if(cmp!=-1){
-                mp[cmp]--;
-                if(mp[cmp]==0){
-                    mp.erase(cmp);
+                mp[mark[x]]--;
+                if(mp[mark[x]]==0){
+                    mp.erase(mark[x]);
                 }
             }
 
+            mp[y]++;
             mark[x]=y;
-            if(mp.find(y)==mp.end()){
-                mp[y]=1;
-            }else mp[y]++;
 
             ans.push_back(mp.size());
         }
