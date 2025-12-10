81409c2abd6a5aeec948009d075af40afe074bab
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-10 19:17:44 +0000

    [LeetCode Sync] - Merge Intervals - Runtime - 23 ms (7.01%), Memory - 28.6 MB (5.30%)

--- leetcode/0056-merge-intervals/solution.cpp
+++ leetcode/0056-merge-intervals/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    bool check(vector<int>&x,vector<int>&y){
-        return x[0]<=y[1] && y[0]<=x[1];
+    bool check(vector<int>x, vector<int>y){
+        return (x[0]<=y[1] && y[0]<=x[1]);
     }
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
         int n = intervals.size();
         sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans = {intervals[0]};
 
-        vector<vector<int>>ans;
-
-        for(int i=0;i<n;i++){
-            if(i==0){
-                ans.push_back(intervals[i]);
+        for(int i=1;i<n;i++){
+            vector<int>x = ans.back();
+            vector<int>y = intervals[i];
+            if(check(x,y)){
+                int mn = min(x[0],y[0]);
+                int mx = max(x[1],y[1]);
+                ans.pop_back();
+                ans.push_back({mn,mx});
             }else{
-                if(check(ans.back(),intervals[i])){
-                    int x1=ans.back()[0];
-                    int x2=ans.back()[1];
-                    ans.pop_back();
-                    ans.push_back({min(x1,intervals[i][0]),max(intervals[i][1],x2)});
-                }else{
-                    ans.push_back(intervals[i]);
-                } 
+                ans.push_back(y);
             }
         }
 
-        return ans;    
+        return ans;
     }
 };
