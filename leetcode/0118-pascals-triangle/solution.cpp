4af5d8b383abcd10db6b24a81012bdc54061e806
Chandan <chandansahoo02468@gmail.com>
2025-12-11 21:31:23 +0000

    [LeetCode Sync] - Pascal's Triangle - Runtime - 0 ms (100.00%), Memory - 9.5 MB (88.03%)

--- leetcode/0118-pascals-triangle/solution.cpp
+++ leetcode/0118-pascals-triangle/solution.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>>ans(n);
+        vector<vector<int>>ans;
 
         for(int i=0;i<n;i++){
+            vector<int>temp;
+            int keep = 1;
             for(int j=0;j<=i;j++){
-                if(j==0 || j==i){
-                    ans[i].push_back(1);
-                }else{
-                    ans[i].push_back(ans[i-1][j]+ans[i-1][j-1]);
+                if(j==0 || j==i) temp.push_back(1);
+                else{
+                    keep*= i+1-j;
+                    keep/=j;
+                    temp.push_back(keep);
                 }
             }
+            ans.push_back(temp);
         }
 
         return ans;
