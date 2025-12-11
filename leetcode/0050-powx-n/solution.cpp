108cafc99cf313189c43b15624beb200f73897cb
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-11 15:36:11 +0000

    [LeetCode Sync] - Pow(x, n) - Runtime - 0 ms (100.00%), Memory - 8.5 MB (91.82%)

--- leetcode/0050-powx-n/solution.cpp
+++ leetcode/0050-powx-n/solution.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    double ans(double x, int n){
+    double myPow(double x, int n) {
         if(n==0) return 1;
         if(n==1) return x;
-        double p = ans(x,n/2);
-        if(n%2==0){
-            return p*p;
-        }
-        else return x*p*p;
 
-        return 1.00;
-    }
-
-    double myPow(double x, int n) {
-        map<long long,double>mp;
-        if(n<0) return 1/ans(x,n);
-        else return ans(x,n);
+        double ans = myPow(x,abs(n/2));
+        ans*=ans;
+        if(n%2) ans*=x;
+        if(n<0) return 1/ans;
+        return ans;
     }
 };
