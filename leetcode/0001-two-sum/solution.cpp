efce7e7a7b4dfdac497c8db4bd2602cfe5e82d02
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-12 10:28:21 +0000

    [LeetCode Sync] - Two Sum - Runtime - 4 ms (53.55%), Memory - 15.9 MB (5.97%)

--- leetcode/0001-two-sum/solution.cpp
+++ leetcode/0001-two-sum/solution.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, vector<int>>mp;
+        unordered_map<int,int>mp;
+        int n = nums.size();
 
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]].push_back(i);
+        for(int i=0;i<n;i++){
+            mp[nums[i]]=i;
         }
 
-        for(int i=0;i<nums.size();i++){
-            auto it = mp.find(target-nums[i]);
-            if(it!=mp.end()){
-                if(2*nums[i] == target && it->second.size()>1){
-                    return {it->second[0],it->second[1]};
-
-                }
-                // if(2*nums[i]==target){
-                //     if(it->second.size()>1){
-                //         return {it->second[0],it->second[1]};
-                //     }
-                // }
-                else if(2*nums[i]!=target) {
-                    return {i,it->second.back()};
-                }
+        for(int i=0;i<n;i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                if(i!=mp[target-nums[i]]) return {i,mp[target-nums[i]]};
             }
         }
-        return {};
+
+        return {-1,-1};
     }
 };
