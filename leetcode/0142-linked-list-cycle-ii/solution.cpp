232c0d4ef73d0295a477f4e7d48c8245147c7caa
Chandan <chandansahoo02468@gmail.com>
2025-12-13 16:12:37 +0000

    [LeetCode Sync] - Linked List Cycle II - Runtime - 5 ms (82.21%), Memory - 11.4 MB (22.79%)

--- leetcode/0142-linked-list-cycle-ii/solution.cpp
+++ leetcode/0142-linked-list-cycle-ii/solution.cpp
@@ -9,23 +9,26 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        
         ListNode* fast = head;
         ListNode* slow = head;
 
         while(fast && fast->next){
-            fast=fast->next->next;
-            slow=slow->next;
+            fast = fast->next->next;
+            slow = slow->next;
 
-            if(fast==slow) break;
+            if(fast==slow){
+                slow = head;
+
+                while(fast!=slow){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+
+                return slow;
+            }
         }
 
-        if(fast==NULL || fast->next==NULL) return NULL;
-
-        while(head!=slow){
-            head=head->next;
-            slow=slow->next;
-        }
-
-        return head;
+        return nullptr;
     }
 };
