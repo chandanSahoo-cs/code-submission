48a539d6380e52320abbca354f851d36d8ad697e
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-14 20:42:06 +0000

    [LeetCode Sync] - Trapping Rain Water - Runtime - 0 ms (100.00%), Memory - 26 MB (58.40%)

--- leetcode/0042-trapping-rain-water/solution.cpp
+++ leetcode/0042-trapping-rain-water/solution.cpp
@@ -1,58 +1,25 @@
 class Solution {
 public:
-    int  left(int  n,vector<int> &a,int  r){
-        stack<int >st;
-        for(int  i=r-1;i>=0;i--){
-            while(!st.empty() && a[st.top()]<a[i]){
-                st.pop();
-            }
-            st.push(i);
-        }
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        int l = 0,r = n-1;
+        int lMax = 0, rMax = 0;
         
-        int  ans = 0;
-        int  i=0;
-        while(i<r && !st.empty()){
-            i = st.top()+1;
-            int  curr = a[st.top()];
-            st.pop();
-            int  limit=r;
-            if(!st.empty()) limit = st.top();
-            while(i<limit){
-                ans+=curr-a[i];
-                i++;
+        int ans=0;
+        
+        while(l<r){
+            if(height[l]<=height[r]){
+                ans+=max(0,lMax-height[l]);
+                lMax=max(lMax,height[l]);
+                l++;
+            }else{
+                ans+=max(0,rMax-height[r]);
+                rMax=max(rMax,height[r]);
+                r--;
             }
         }
-        return ans;
-    }
-    
-    int  right(int  n, vector<int>&a , int  r){
-        stack<int >st;
-        for(int  i=r+1;i<n;i++){
-            while(!st.empty() && a[st.top()]<a[i]){
-                st.pop();
-            }
-            st.push(i);
-        }
-        int  ans = 0;
-        int  i=n-1;
-        while(i>r && !st.empty()){
-            i=st.top()-1;
-            int  curr = a[st.top()];
-            st.pop();
-            int  limit = r;
-            if(!st.empty()) limit = st.top();
-            while(i>limit){
-                ans+=curr-a[i];
-                i--;
-            }
-        }
-        return ans;
-    }
 
-
-    int trap(vector<int>& h) {
-        int  n = h.size();
-        int  mxInd = max_element(h.begin(),h.end())-h.begin();
-        return left(n,h,mxInd)+right(n,h,mxInd);
+        return ans;
     }
 };
