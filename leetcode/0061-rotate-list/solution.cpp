1af5f987df4c3e13226a9f9919d7d4cced768702
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-14 14:17:00 +0000

    [LeetCode Sync] - Rotate List - Runtime - 0 ms (100.00%), Memory - 16.3 MB (63.30%)

--- leetcode/0061-rotate-list/solution.cpp
+++ leetcode/0061-rotate-list/solution.cpp
@@ -10,52 +10,33 @@
  */
 class Solution {
 public:
-
-    vector<ListNode*>reverseLL(ListNode* head){
-        if(head->next==NULL) return {head,head};
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-
-        while(temp){
-            ListNode* next = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = next;
-        }
-
-        return{prev,head};
-    }
-
     ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* temp = head;
-        if(head==NULL || head->next==NULL || k==0) return head;
-        int len = 0;
+        if(!head) return head;
 
-        while(temp){
+        ListNode* curr = head;
+
+        int len = 1;
+        while(curr && curr->next){
+            curr=curr->next;
             len++;
+        }
+
+        k = k%len;
+        if(k==0) return head;
+        
+        curr->next = head;
+
+
+        int cut = len-k-1;
+
+        ListNode* temp = head;
+        while(cut--){
             temp=temp->next;
         }
 
-        k = len - k%len ;
-        if(k==len) return head;
+        ListNode* next = temp->next;
+        temp->next = nullptr;
 
-        int c = 0;
-        temp = head;
-        while(temp){
-            c++;
-            if(c==k){
-                ListNode*next = temp->next;
-                temp->next = NULL;
-                vector<ListNode*>v1 = reverseLL(head);
-                vector<ListNode*>v2 = reverseLL(next);
-                head=v1[0];
-                v1[1]->next = v2[0];
-            }
-            temp=temp->next;
-        }
-
-        vector<ListNode*>v = reverseLL(head);
-
-        return v[0] ;
+        return next;
     }
 };
