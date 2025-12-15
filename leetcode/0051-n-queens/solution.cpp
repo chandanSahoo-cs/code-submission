1b762adcae7163043798850ccc5387e04b78ff26
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-12-15 12:52:44 +0000

    [LeetCode Sync] - N-Queens - Runtime - 0 ms (100.00%), Memory - 10.4 MB (38.95%)

--- leetcode/0051-n-queens/solution.cpp
+++ leetcode/0051-n-queens/solution.cpp
@@ -1,31 +1,56 @@
 class Solution {
 public:
+    int queen[10];
     vector<vector<string>>ans;
 
-    void rec(int n , vector<int>&r, vector<int>&c, vector<int>&di1, vector<int>&di2, vector<string>s,int i){
-        if(i==n){
+    bool check(int row, int col){
+        for(int i=0;i<row;i++){
+            int pRow = i;
+            int pCol = queen[i];
+
+            if(col==pCol || abs(pRow-row)==abs(pCol-col)) return false;
+        }
+        return true;
+    }
+
+    void rec(int n, int row, int col, vector<string>&s){
+        
+        if(row==n){
+            for(int i=0;i<n;i++){
+                s[i][queen[i]]='Q';
+            }
             ans.push_back(s);
+            for(int i=0;i<n;i++){
+                s[i][queen[i]]='.';
+            }
             return;
         }
 
-        for(int j=0;j<n;j++){
-            if(c[j] || di1[i+j] || di2[n-i+j]) continue;
+        if(col==n) return;
 
-            s[i][j]='Q';
-            r[i]=c[j]=di1[i+j]=di2[n-i+j]=1;
-
-            rec(n,r,c,di1,di2,s,i+1);
-
-            s[i][j]='.';
-            r[i]=c[j]=di1[i+j]=di2[n-i+j]=0;
+        if(check(row, col)){
+            queen[row]=col;
+            rec(n,row+1,0,s);
+            queen[row]=-1;
         }
-        return;
+        rec(n,row,col+1,s);
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        vector<int>row(n),col(n),di1(2*n),di2(2*n);
-        vector<string>s(n,string(n,'.'));
-        rec(n,row,col,di1,di2,s,0);
+        memset(queen,-1,sizeof(queen));
+        vector<string>s;
+
+        string temp = "";
+
+        for(int i=0;i<n;i++){
+            temp.push_back('.');
+        }    
+        for(int i=0;i<n;i++){
+            s.push_back(temp);
+        }
+
+        rec(n,0,0,s);
+
         return ans;
     }
 };
