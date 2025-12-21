572327c849469b35e015140ac4f9502398b346eb
Chandan <chandansahoo02468@gmail.com>
2025-12-21 15:19:27 +0000

    [LeetCode Sync] - Binary Tree Inorder Traversal - Runtime - 0 ms (100.00%), Memory - 10.6 MB (99.13%)

--- leetcode/0094-binary-tree-inorder-traversal/solution.cpp
+++ leetcode/0094-binary-tree-inorder-traversal/solution.cpp
@@ -9,41 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- //Iterative Way
 class Solution {
 public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==nullptr) return {};
+        vector<int>ans;
 
-    void iterativeInorder(stack<TreeNode*>&st, vector<int>&ans){
-        if(st.empty()) return;
+        stack<TreeNode*>st;
+
+        st.push(root);
 
         while(!st.empty()){
-            if(st.top()->left!=nullptr) {
-                TreeNode* temp = st.top()->left;
-                st.top()->left=nullptr;
-                st.push(temp);
-            }
-            else{
-                TreeNode* temp = st.top();
-                st.pop();
-                ans.push_back(temp->val);
 
-                if(temp->right!=nullptr){
-                    st.push(temp->right);
+            TreeNode* node = st.top();
+
+            if(node->left){
+                st.push(node->left);
+                node->left = nullptr;
+            }else{
+                st.pop();
+                ans.push_back(node->val);
+                if(node->right){
+                    st.push(node->right);
                 }
             }
         }
-    }
-
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        stack<TreeNode*>st;
-
-        if(root!=nullptr){
-            st.push(root);
-        }
-
-        iterativeInorder(st,ans);
 
         return ans;
+
     }
 };
