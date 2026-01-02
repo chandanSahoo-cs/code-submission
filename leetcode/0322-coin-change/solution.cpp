a1b3c9a367c8a05f0d75137c00c4709967b08a07
Chandan <chandansahoo02468@gmail.com>
2026-01-02 13:15:16 +0000

    [LeetCode Sync] - Coin Change - Runtime - 25 ms (64.13%), Memory - 17.9 MB (72.82%)

--- leetcode/0322-coin-change/solution.cpp
+++ leetcode/0322-coin-change/solution.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
 
-    // int rec(vector<int>&a, int amt){
-    //     int n = a.size();
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int>dp(amount+1,1e5);
+        dp[0]=0;
 
-
-    //     int ans = INT_MAX;
-    //     bool flag = false;
-
-    //     for(int i=0;i<n;i++){
-    //         if(amt-a[i]<0) continue;
-    //         int temp = rec(a,amt-a[i]);
-    //         if(temp!=-1) {
-    //             flag = true;
-    //             ans = min(temp,ans);
-    //         }
-    //     }
-
-    //     mark[amt]=true;
-    //     if(!flag) return dp[amt] = -1;
-    //     else return dp[amt] = ans+1; 
-    // }
-
-    int coinChange(vector<int>& a, int amount) {
-        int n = a.size();
-        vector<int>dp(amount+1,-1);
-
-        for(int j=0;j<=amount;j++){
-            if(j==0) dp[j]=0;
-            else{
-                int ans = INT_MAX;
-                bool flag = false;
-                for(int i=0;i<n;i++){
-                    if(j-a[i]<0) continue;
-                    int temp = dp[j-a[i]];
-                    if(temp!=-1) {
-                        flag = true;
-                        ans = min(temp,ans);
-                    }
+        for(int i=1;i<=amount;i++){
+            for(int j=n-1;j>=0;j--){
+                if(coins[j]<=i){
+                    dp[i]= min(dp[i],1+dp[i-coins[j]]);
                 }
-
-                if(flag) dp[j] = ans+1;
             }
         }
 
-        return dp[amount];
+        return dp[amount]!=1e5?dp[amount]:-1;
     }
 };
