db3ff05a99cfc0d6ccd382b569525be622e6730d
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-03 12:21:38 +0000

    [LeetCode Sync] - Subsets - Runtime - 3 ms (30.25%), Memory - 12.8 MB (22.60%)

--- leetcode/0078-subsets/solution.cpp
+++ leetcode/0078-subsets/solution.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int m = pow(2,nums.size());
-        vector<vector<int>>ans;
+    vector<vector<int>>ans;
 
-        for(int i=0;i<m;i++){
-            int j = i;
-            vector<int>temp;
-            int k=0;
-            while(j){
-                if(j&1) temp.push_back(nums[k]);
-                j>>=1;
-                k++;
-            }
+    void rec(vector<int>&nums, int i, vector<int>&temp){
+        int n = nums.size();
+        if(i==n){
             ans.push_back(temp);
+            return;
         }
 
-        return ans;
+        rec(nums,i+1,temp);
+        temp.push_back(nums[i]);
+        rec(nums,i+1,temp);
+        temp.pop_back();
+
+        return;
+    }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+        rec(nums,0,temp);
+
+        return ans;    
     }
 };
