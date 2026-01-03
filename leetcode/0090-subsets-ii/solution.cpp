b49ad2473e3a5baa1d7ac8c1c527d14db822f0f9
Chandan <chandansahoo02468@gmail.com>
2026-01-03 13:02:32 +0000

    [LeetCode Sync] - Subsets II - Runtime - 0 ms (100.00%), Memory - 14.1 MB (11.73%)

--- leetcode/0090-subsets-ii/solution.cpp
+++ leetcode/0090-subsets-ii/solution.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     vector<vector<int>>ans;
 
-    void fill(vector<int>&a, int i,vector<int>&s,int skip){
-        if(a.size()==i){
-            ans.push_back(s);
+    void rec(vector<int>&nums, int i, vector<int>&temp, int ignore){
+        int n = nums.size();
+
+        if(i==n){
+            ans.push_back(temp);
             return;
         }
 
-        if(a[i]==skip){
-            fill(a,i+1,s,skip);
+        if(nums[i]==ignore){
+            rec(nums,i+1,temp,ignore);
             return;
         }
-        s.push_back(a[i]);
-        fill(a,i+1,s,11);
-        s.pop_back();
-        fill(a,i+1,s,a[i]);
 
-        return;
+        rec(nums,i+1,temp,nums[i]);
+        temp.push_back(nums[i]);
+        rec(nums,i+1,temp,11);
+        temp.pop_back();
+
+        return ;
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<int>s;
-        fill(nums, 0,s,11);
-        return ans;
+        vector<int>temp;
+        rec(nums,0,temp,11);
+
+        return ans;    
     }
 };
