d0badcd42bdb35251f71093cfc803d1b5cba0d9b
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-03 13:36:30 +0000

    [LeetCode Sync] - Combination Sum II - Runtime - 0 ms (100.00%), Memory - 17.3 MB (9.63%)

--- leetcode/0040-combination-sum-ii/solution.cpp
+++ leetcode/0040-combination-sum-ii/solution.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void  fill(vector<int> &a,int t,int i,vector<int>&s, int last){
-        if(t==0){
-            ans.push_back(s);
+
+    void rec(vector<int>&cand,int tr, int i, vector<int>&temp, int ignore){
+        int n = cand.size();
+
+        if(i==n && tr==0){
+            ans.push_back(temp);
             return;
         }
-        if(a.size()==i) return;
-        if(t<0) return; 
-        if(a[i]==last) {
-            fill(a,t,i+1,s,a[i]);
-            return;
-        };
-        s.push_back(a[i]);
-        fill(a,t-a[i],i+1,s,-1);
-        s.pop_back();
-        fill(a,t,i+1,s,a[i]);
-    }
-    vector<vector<int>> combinationSum2(vector<int>& a, int t) {
-        sort(a.begin(),a.end());
-        vector<int>s;
-        fill(a,t,0,s,-1);
 
-        return ans;
+        if(i==n || tr<0) return;
+
+        if(cand[i]==ignore){
+            rec(cand,tr,i+1,temp,ignore);
+            return;
+        }
+
+        rec(cand,tr,i+1,temp,cand[i]);
+        temp.push_back(cand[i]);
+        rec(cand,tr-cand[i],i+1,temp,51);
+        temp.pop_back();
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int>temp;
+        rec(candidates,target,0,temp,51);
+
+        return ans;    
     }
 };
