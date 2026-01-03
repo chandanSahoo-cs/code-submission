7d86095abcef6d25afca6bed50a77a040b217bc1
Chandan <chandansahoo02468@gmail.com>
2026-01-03 13:18:32 +0000

    [LeetCode Sync] - Combination Sum - Runtime - 6 ms (26.45%), Memory - 18.4 MB (18.24%)

--- leetcode/0039-combination-sum/solution.cpp
+++ leetcode/0039-combination-sum/solution.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    map<pair<pair<int,int>,vector<int>>,int>mp;
 
-    bool giveAns(vector<int>& a,vector<int> s, int t,int i){
-        if(t==0){
-            ans.push_back(s);
-            return 1;
-        }
-        if(a.size()==i) return 0;   
-        if(mp.find({{i,t},s})!=mp.end()) return mp[{{i,t},s}];
-
-        // if(dp[i][t][rep]!=-1){
-        //     return dp[i][t][rep];
-        // }
-
-        bool take=false;
-
-        if(t-a[i]>=0){
-            s.push_back(a[i]);
-            take=giveAns(a,s,t-a[i],i);
-            s.pop_back();
+    void rec(vector<int>& cand, int tr, int i, vector<int>&temp){
+        int n = cand.size();
+        if(tr==0 && i==n){
+            ans.push_back(temp);
+            return;
         }
 
-        bool notTake = giveAns(a,s,t,i+1);
+        if(tr<0 || i==n) return;
 
-        return mp[{{i,t},s}]=(take || notTake);
+        rec(cand,tr,i+1,temp);
+        temp.push_back(cand[i]);
+        rec(cand,tr-cand[i],i,temp);
+        temp.pop_back();
+
+        return;
+
     }
 
-    vector<vector<int>> combinationSum(vector<int>& a, int t) {
-        vector<int>s;
-        giveAns(a,s,t,0);
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        rec(candidates,target, 0, temp);
+
+        return ans;    
     }
 };
