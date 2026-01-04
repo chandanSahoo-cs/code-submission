4406b8906b35af74e53bd052f26022ac48f19cd0
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-04 14:50:25 +0000

    [LeetCode Sync] - Palindrome Partitioning - Runtime - 68 ms (20.15%), Memory - 97.3 MB (14.77%)

--- leetcode/0131-palindrome-partitioning/solution.cpp
+++ leetcode/0131-palindrome-partitioning/solution.cpp
@@ -1,46 +1,63 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    int dp[16][16];
+    int dp[20][20];
     
-    int checkPalindrome(string&s, int l, int r){
-        if(r-l==0) return 1;
-        if(r-l==1){
-            if(s[l]==s[r]) return 1;
-            else return 0;
+    void fillDp(string&s){
+        int n = s.size();
+
+        for(int i=0;i<n;i++){
+            dp[i][i] = 1;
         }
 
-        if(dp[l][r]!=-1) return dp[l][r];
-        return dp[l][r] = s[l]==s[r] && checkPalindrome(s,l+1,r-1);
+        for(int i=0;i<n-1;i++){
+            if(s[i]==s[i+1]) dp[i][i+1] = 1;
+            else dp[i][i+1]=0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<n;j++){
+                if(i+1>=j){
+                    if(i+1!=j && i!=j) dp[i][j]=0;
+                    continue; 
+                }
+                dp[i][j] = (s[i]==s[j] && dp[i+1][j-1]);
+            }
+        }
     }
 
-    void rec(string s, int l, int i, vector<string> &tv){
+    void rec(string &s, int l, int i, vector<string> &tv){
         int n = s.size();
 
         if(i==n-1){
-            if(checkPalindrome(s,l,i)){
+            if(dp[l][i]==1){
                 tv.push_back(s.substr(l));
                 ans.push_back(tv);
                 tv.pop_back();
             }
             return;
         }
-        rec(s,l,i+1,tv);
-
-        if(checkPalindrome(s,l,i)){
+        if(dp[l][i]==1){
             tv.push_back(s.substr(l,i-l+1));
             rec(s,i+1,i+1,tv);
             tv.pop_back();
         }
+
+        rec(s,l,i+1,tv);
+
 
 
         return ;
     }
 
     vector<vector<string>> partition(string s) {
+        int n = s.size();
         memset(dp,-1,sizeof(dp));
+        fillDp(s);
+
         vector<string> tv;
         rec(s,0,0,tv);
+
 
         return ans;
     }
