b45e27502f9dd57dd0fcc6b366e3ec93ef19989b
Chandan <chandansahoo02468@gmail.com>
2026-01-04 14:50:25 +0000

    [LeetCode Sync] - Palindrome Partitioning - Runtime - 68 ms (19.47%), Memory - 97.3 MB (13.02%)

--- leetcode/0131-palindrome-partitioning/solution.cpp
+++ leetcode/0131-palindrome-partitioning/solution.cpp
@@ -1,46 +1,64 @@
 class Solution {
 public:
-    vector<vector<string>>ans;
-
-    bool isPalindrome(string s){
+    vector<vector<string>> ans;
+    int dp[20][20];
+    
+    void fillDp(string&s){
         int n = s.size();
-        int l=0,r=n-1;
 
-        while(l<r){
-            if(s[l++]!=s[r--]) return false;
+        for(int i=0;i<n;i++){
+            dp[i][i] = 1;
         }
 
-        return true;
+        for(int i=0;i<n-1;i++){
+            if(s[i]==s[i+1]) dp[i][i+1] = 1;
+            else dp[i][i+1]=0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<n;j++){
+                if(i+1>=j){
+                    if(i+1!=j && i!=j) dp[i][j]=0;
+                    continue; 
+                }
+                dp[i][j] = (s[i]==s[j] && dp[i+1][j-1]);
+            }
+        }
     }
 
-    void fill(string &s, vector<string> &c, string store, int i){
-        if(s.size()==i){
-            if(isPalindrome(store) && !store.empty()){
-                c.push_back(store);
-                ans.push_back(c);
-                c.pop_back();
+    void rec(string &s, int l, int i, vector<string> &tv){
+        int n = s.size();
+
+        if(i==n-1){
+            if(dp[l][i]==1){
+                tv.push_back(s.substr(l));
+                ans.push_back(tv);
+                tv.pop_back();
             }
             return;
         }
-
-        if(isPalindrome(store)){
-            if(!store.empty()){
-                c.push_back(store);
-                fill(s,c,string(1,s[i]),i+1);
-                c.pop_back();
-            }
+        if(dp[l][i]==1){
+            tv.push_back(s.substr(l,i-l+1));
+            rec(s,i+1,i+1,tv);
+            tv.pop_back();
         }
 
-        fill(s,c,store+s[i],i+1);
+        rec(s,l,i+1,tv);
 
-        return;
+
+
+        return ;
     }
-    
-    vector<vector<string>> partition(string s) {
-        vector<string> c;
-        string store ="";
 
-        fill(s,c,store,0);
+    vector<vector<string>> partition(string s) {
+        int n = s.size();
+        memset(dp,-1,sizeof(dp));
+        fillDp(s);
+
+        vector<string> tv;
+        rec(s,0,0,tv);
+
+
         return ans;
     }
 };
