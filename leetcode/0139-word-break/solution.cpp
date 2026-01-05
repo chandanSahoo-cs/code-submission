a87ff9f8575b519d08cea5d96aa91082eb5a4188
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-05 16:20:11 +0000

    [LeetCode Sync] - Word Break - Runtime - 15 ms (28.62%), Memory - 17.6 MB (20.63%)

--- leetcode/0139-word-break/solution.cpp
+++ leetcode/0139-word-break/solution.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    bool fn(int i, int j, unordered_set<string>&c, string &s,vector<vector<int>> &mark){
-        if(j==s.size()-1){
-            return (c.find(s.substr(i,j-i+1))!=c.end());
-        }
-        bool a1=false,a2=false;
-        if(j-i+1>20) return false;
-        if(mark[i][j]!=-1 ) return mark[i][j];
-        if(c.find(s.substr(i,j-i+1))!=c.end()){
-            a1 = fn(i,j+1,c,s,mark);
-            a2 = fn(j+1,j+1,c,s,mark);
-        }
-        else a1 = a2 = fn(i,j+1,c,s,mark);
-        return mark[i][j] = a1 || a2;
-    }
+    // int dp[305][305];
+
+    // int rec(string &s, unordered_set<string>&st, int l, int r){
+    //     int n = s.size();
+    //     if(r==n-1){
+    //         if(st.find(s.substr(l,r-l+1))!=st.end()) return 1;
+    //         return 0;
+    //     }
+
+    //     if(dp[l][r]!=-1) return dp[l][r];
+
+    //     bool flag=false;
+
+    //     if(st.find(s.substr(l,r-l+1))!=st.end()){
+    //         flag |=rec(s,st,r+1,r+1);
+    //     }
+
+    //     flag |=rec(s,st,l,r+1);
+
+    //     return dp[l][r] = flag;
+
+    // }
 
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<vector<int>>mark(s.size(),vector<int>(s.size(),-1));
-        unordered_set<string> c(wordDict.begin(),wordDict.end());
-        return fn(0,0,c,s,mark);
+        int n = s.size();
+
+        vector<bool>curr(n);
+        unordered_set<string>st(wordDict.begin(),wordDict.end());
+
+       for(int l=0;l<n;l++){
+        curr[l] = (st.find(s.substr(l,n-l))!=st.end());
+       }
+
+        for(int r=n-2; r>=0;r--){
+            for(int l=r;l>=0;l--){
+                if(st.find(s.substr(l,r-l+1))!=st.end()){
+                    curr[l] = curr[l] || curr[r+1];
+                }
+            }
+        }
+
+        return curr[0];
     }
 };
