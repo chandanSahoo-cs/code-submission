51b4ac82be72b6cbe6dee9fb110f1f3ef4b871f5
Chandan <chandansahoo02468@gmail.com>
2026-01-05 12:33:58 +0000

    [LeetCode Sync] - Sudoku Solver - Runtime - 131 ms (90.44%), Memory - 8.8 MB (59.00%)

--- leetcode/0037-sudoku-solver/solution.cpp
+++ leetcode/0037-sudoku-solver/solution.cpp
@@ -1,82 +1,64 @@
 class Solution {
 public:
 
-    bool verify(vector<vector<char>> &b){
-        vector<int>check = {1,2,3,4,5,6,7,8,9};
-        for(int i=0;i<9;i++){
-            vector<int>a;
-            for(int j=0;j<9;j++){
-                a.push_back(b[i][j]-'0');
-            }
-            sort(a.begin(),a.end());
-            if(a!=check) return false;
-        }
+    vector<vector<int>>row;
+    vector<vector<int>>col;
+    vector<vector<int>>box;
 
-        for(int i=0;i<9;i++){
-            vector<int>a;
-            for(int j=0;j<9;j++){
-                a.push_back(b[j][i]-'0');
-            }
-            sort(a.begin(),a.end());
-            if(a!=check) return false;
-        }
+    bool check(int i, int j, int b, char val){
 
-        for(int i=0;i<9;i+=3){
-            vector<int>a;
-            for(int j=0;j<9;j+=3){
-                for(int m=i;m<i+3;m++){
-                    for(int n=j;n<j+3;n++){
-                        a.push_back(b[m][n]-'0');
-                    }
-                }
-            }
-            sort(a.begin(),a.end());
-            if(a!=check) return false;
-        }
+        if(row[i][val-'0'] || col[j][val-'0'] || box[b][val-'0']) return false;
 
         return true;
     }
 
+    bool rec(vector<vector<char>>& board, int i, int j){
+        if(i==9) return 1;
 
-    bool solve(vector<vector<char>> &b, vector<vector<int>> &r,vector<vector<int>> &c, vector<vector<int>> &sg, int i, int j){
-        if(i>=9) return true;
-        if(j>=9) return solve(b,r,c,sg,i+1,0);
-        if(b[i][j]!='.') return solve(b,r,c,sg,i,j+1);
-        int bi = (i/3)*3+(j/3);
-        for(int k=1;k<10;k++){
-            if(!r[i+1][k] && !c[j+1][k] && !sg[bi+1][k]){
-                r[i+1][k]=1;
-                c[j+1][k]=1;
-                sg[bi+1][k]=1;
-                b[i][j] = char(k+'0');
-                if(solve(b,r,c,sg,i,j+1)){
-                    return true;
+        if(j==9){
+            return rec(board,i+1,0);
+        }else if(board[i][j]!='.'){
+            return rec(board,i,j+1);
+        }
+        else{
+            for(char c='1';c<='9';c++){
+                int b = (i/3)*3+j/3;
+                if(check(i,j,b,c)){
+                    board[i][j]=c;
+                    row[i][c-'0']=1;
+                    col[j][c-'0']=1;
+                    box[b][c-'0']=1;
+
+                    int val = rec(board,i,j+1);
+                    if(val) return 1;
+
+                    board[i][j]='.';
+                    row[i][c-'0']=0;
+                    col[j][c-'0']=0;
+                    box[b][c-'0']=0;
                 }
-                r[i+1][k]=0;
-                c[j+1][k]=0;
-                sg[bi+1][k]=0;
-                b[i][j]='.';
             }
         }
 
-        return false;
+        return 0;
     }
 
-    void solveSudoku(vector<vector<char>>& b) {
-        vector<vector<int>>row(10,vector<int>(10));
-        vector<vector<int>>col(10,vector<int>(10));
-        vector<vector<int>>sg(10,vector<int>(10));
+    void solveSudoku(vector<vector<char>>& board) {
+        row.resize(9,vector<int>(10));
+        col.resize(9,vector<int>(10));
+        box.resize(9,vector<int>(10));
 
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(b[i][j]!='.'){
-                    row[i+1][b[i][j]-'0']=1;
-                    col[j+1][b[i][j]-'0']=1;
-                    int bi = (i/3)*3+(j/3);
-                    sg[bi+1][b[i][j]-'0']=1;
-                }
+                if(board[i][j]=='.') continue;
+                int c = board[i][j]-'0';
+                int b = (i/3)*3+j/3;
+                row[i][c] = col[j][c] = box[b][c] = 1;
             }
         }
-        solve(b,row,col,sg,0,0);
+
+        rec(board,0,0);
+
+        return;
     }
 };
