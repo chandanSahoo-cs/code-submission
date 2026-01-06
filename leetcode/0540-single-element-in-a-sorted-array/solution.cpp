26f2364c7b10742c80ef74ffe551e7fefb0c93b0
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-06 15:31:18 +0000

    [LeetCode Sync] - Single Element in a Sorted Array - Runtime - 0 ms (100.00%), Memory - 26.3 MB (32.93%)

--- leetcode/0540-single-element-in-a-sorted-array/solution.cpp
+++ leetcode/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    #define ll long long
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
-        ll s=0,e=nums.size()-1;
-        while(s<=e){
-            ll m=s+(e-s)/2;
-            if(nums[m]!=nums[m-1] && nums[m]!=nums[m+1]){
-                return nums[m];
-                break;
-            }
-            else if(nums[m]==nums[m-1] && m%2!=0) s=m+1;
-            else if(nums[m]==nums[m+1] && m%2==0) s=m+2;
-            else if(nums[m]==nums[m+1] && m%2!=0) e=m-1;
-            else e=m-2;
+        int l=0,r=nums.size()-1;
+
+        while(l<=r){
+            int m = l+(r-l)/2;
+            
+            if((m==0 || nums[m]!=nums[m-1]) && (m==nums.size()-1 || nums[m]!=nums[m+1])) return nums[m];
+            else if((m%2 && nums[m]==nums[m-1]) || (m%2==0 && nums[m]==nums[m+1])){
+                l=m+1;
+            }else r=m-1;
         }
+
         return -1;
     }
 };
