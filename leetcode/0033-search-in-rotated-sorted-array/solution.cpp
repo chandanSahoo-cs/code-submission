5d19d96b63b81a52745ccc709d1185cc294ce55f
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-06 20:59:23 +0000

    [LeetCode Sync] - Search in Rotated Sorted Array - Runtime - 0 ms (100.00%), Memory - 15.3 MB (32.17%)

--- leetcode/0033-search-in-rotated-sorted-array/solution.cpp
+++ leetcode/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,32 +1,25 @@
 class Solution {
-    #define ll long long
 public:
     int search(vector<int>& nums, int target) {
-        ll s=0,e=nums.size()-1;
-        ll temp=-1;
-        if(nums[s]>nums[e] ) {
-            while(s<=e){
-                ll m=s+(e-s)/2;
-                if(nums[m]>=nums[s] && nums[m]>=nums[e] && nums[m]>nums[m+1]){
-                    temp=m;
-                    break;
-                }
-                else if (nums[m]<nums[s]) e=m-1;
-                else s=m+1;
-            }
-            if(nums[temp]>target && nums[nums.size()-1]>=target){
-                s=temp+1,e=nums.size()-1;
-            }
-            else{
-                s=0,e=temp;
-            }
-        }
-        while(s<=e){
-            ll m=s+(e-s)/2;
+        int n = nums.size();
+
+        int l = 0, r = n-1;
+
+        while(l<=r){
+            int m = l+(r-l)/2;
+
             if(nums[m]==target) return m;
-            else if(nums[m]>target) e=m-1;
-            else s=m+1;
+            else if(nums[m]>nums[r]){
+                if(target>=nums[l] && target<nums[m]){
+                    r=m-1;
+                }else l=m+1;
+            }else{
+                if(target<=nums[r] && target>nums[m]){
+                    l=m+1;
+                }else r=m-1;
+            }
         }
+
         return -1;
     }
 };
