6a062d82815a2fb2d3dd7ed92c987433c6096253
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-09 11:48:36 +0000

    [LeetCode Sync] - Lowest Common Ancestor of Deepest Leaves - Runtime - 0 ms (100.00%), Memory - 21.8 MB (98.54%)

---
+++ leetcode/1218-lowest-common-ancestor-of-deepest-leaves/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Solution {
+public:
+
+    pair<TreeNode*,int> findAncestor(TreeNode* root){
+        if(!root) return {root,0};
+        
+        auto [lcaL, depL] = findAncestor(root->left);
+        auto [lcaR, depR] = findAncestor(root->right);
+
+        if(depL>depR){
+            return {lcaL,depL+1};
+        }else if(depR>depL){
+            return {lcaR,depR+1};
+        }
+
+        return {root,depL+1};
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        auto[node, dep] = findAncestor(root);
+
+        return node;    
+    }
+};
