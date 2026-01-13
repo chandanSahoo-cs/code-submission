f421a29d6fea94f27595ed0d2ba140e459ead17e
Chandan <chandansahoo02468@gmail.com>
2026-01-13 10:58:49 +0000

    [LeetCode Sync] - Separate Squares I - Runtime - 106 ms (63.28%), Memory - 198.6 MB (98.51%)

--- leetcode/3763-separate-squares-i/solution.cpp
+++ leetcode/3763-separate-squares-i/solution.cpp
@@ -1,50 +1,43 @@
 class Solution {
 public:
     double separateSquares(vector<vector<int>>& squares) {
-        int n = squares.size();
-        
-        vector<pair<double,double>>vp;
-
-        double totSum=0;
+        double l=1e9, r=0;
+        double totalArea=0;
 
         for(auto &ele:squares){
-            int x = ele[0];
-            int y = ele[1];
-            int l = ele[2];
-
-            totSum+=l*1LL*l;
-
-            vp.push_back({ele[1],l});
-            vp.push_back({ele[1]+ele[2],-l});
+            double y = ele[1];
+            double side = ele[2];
+            l = min(y,l);
+            r = max(y+side,r);
+            totalArea+=side*side;
         }
 
-        sort(vp.begin(),vp.end());
+        double target = totalArea/2.0;
+        double ans = r;
 
-        double reqSum = totSum/(double)2;
-        double currSum = 0;
+        for(int i=0;i<50;i++){
+            double m = l+(r-l)/2;
 
-        int sz = vp.size();
-        int i=0;
-        double x = 0;
+            double upper=0;
 
-        while(i<sz-1){
-            auto [y1,l] = vp[i];
+            for(auto &ele:squares){
+                double y=ele[1],side=ele[2];
 
-            while(i<sz-1 && y1==vp[i].first){
-                x+=vp[i].second;
-                i++;
+                double h2 = side+y-m;
+
+                if(h2>0){
+                    if(side<h2) h2=side;
+                    upper+=side*h2;
+                }
             }
-            
-            double y2 = vp[i].first;
 
-            double tempSum = (y2-y1)*x;
-
-            if(currSum+tempSum>=reqSum){
-                double tr = reqSum-currSum;
-                return y1+tr/x;
-            }else currSum+=tempSum;
+            if(upper<=target){
+                ans = min(ans,m);
+                r=m;
+            }
+            else l=m;
         }
 
-        return 0;
+        return ans;
     }
 };
