4ab3d4c9c638909c42e334c31d4ded9ea3cb1591
Chandan <chandansahoo02468@gmail.com>
2026-01-16 18:17:16 +0000

    [LeetCode Sync] - Sliding Window Maximum - Runtime - 20 ms (69.47%), Memory - 139 MB (92.10%)

--- leetcode/0239-sliding-window-maximum/solution.cpp
+++ leetcode/0239-sliding-window-maximum/solution.cpp
@@ -3,21 +3,20 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
 
-        deque<int> dq;
+        deque<int>dq;
 
         vector<int>ans;
 
         for(int i=0;i<n;i++){
-            if(i>k-1){
-                if(nums[i-k]==dq.front()) dq.pop_front();
+            
+            if(i>=k){
+                while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
             }
-            while(!dq.empty() && dq.back()<nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(nums[i]);
-            if(i>=k-1){
-                ans.push_back(dq.front());  
-            }
+
+            while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
+            dq.push_back(i);
+
+            if(i>=k-1)ans.push_back(nums[dq.front()]);
         }
 
         return ans;
