aba83e166fdfe4e4614e09236453e2c28186121e
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-19 12:54:33 +0000

    [LeetCode Sync] - Reverse Words in a String - Runtime - 10 ms (11.94%), Memory - 9.9 MB (87.47%)

--- leetcode/0151-reverse-words-in-a-string/solution.cpp
+++ leetcode/0151-reverse-words-in-a-string/solution.cpp
@@ -1,30 +1,29 @@
-#define ll long long
 class Solution {
 public:
     string reverseWords(string s) {
-        ll n = s.size();
-        string ans = "";
-        string temp = "";
-        stack<string>st;
+        int n=s.size();
+        reverse(s.begin(),s.end());
 
-        for(ll i=0;i<n;i++){
-            if(s[i]==' ' && temp!=""){
-                st.push(temp);
-                temp = "";
-            }
-            else{
-                if(s[i]!=' ') temp+=s[i];
-            }
-        }
-        if(temp!="") st.push(temp);
+        int l=0;
+        int r=0;
 
-        while(!st.empty()){
-            ans+=st.top();
-            if(st.size()!=1){
-                ans+=' ';
-            }
-            st.pop();
+        while(r<n){
+            if(r!=0 && s[r-1]!=' ' && s[r]==' '){ 
+
+                reverse(s.begin()+l,s.begin()+r);
+
+                while(l<n && s[l]!=' ') l++;
+                
+                l++;
+                r=l;
+
+            }else r++;
         }
-        return ans.substr(0,n);
+
+        reverse(s.begin()+l,s.begin()+r);
+
+        while(s.back()==' ') s.pop_back();
+
+        return s;
     }
 };
