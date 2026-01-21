da9b3347a69aafc1df8abacfe3ff20acc2c96574
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-21 12:20:43 +0000

    [LeetCode Sync] - Binary Tree Inorder Traversal - Runtime - 0 ms (100.00%), Memory - 10.9 MB (32.84%)

--- leetcode/0094-binary-tree-inorder-traversal/solution.cpp
+++ leetcode/0094-binary-tree-inorder-traversal/solution.cpp
@@ -11,31 +11,51 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        if(root==nullptr) return {};
-        vector<int>ans;
+    void inorder(TreeNode* root,vector<int>&ans){
+        TreeNode* curr = root;
 
-        stack<TreeNode*>st;
+        /*
+        case 1 : left is null
+            add the curr->val
+            curr = curr->right
+        case 2 : left is not null;
+            - find the rightmost node of the left subtree , incase of single node, that node is the rightmost node
+            - case 2a : if this rightmode node has thread to curr node,
+                - break the thread
+                - add the curr->val
+                - curr = curr->right
+            - case 2b : if this rightmode node doesn't has thread to curr node
+                - make a thread from this node to curr node;
+                - curr = curr->left
+        */
 
-        st.push(root);
-
-        while(!st.empty()){
-
-            TreeNode* node = st.top();
-
-            if(node->left){
-                st.push(node->left);
-                node->left = nullptr;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                ans.push_back(curr->val);
+                curr = curr->right;
             }else{
-                st.pop();
-                ans.push_back(node->val);
-                if(node->right){
-                    st.push(node->right);
+                TreeNode* temp = curr->left;
+                while(temp->right!=nullptr && temp->right!=curr){
+                    temp = temp->right;
+                }
+
+                if(temp->right==nullptr){
+                    temp->right = curr;
+                    curr = curr->left;
+                }else{
+                    temp->right = nullptr;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
                 }
             }
         }
+        return ;
+    }
 
-        return ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        inorder(root,ans);
 
+        return ans;    
     }
 };
