795ca7c2924167b49c855a9706d2e1de3f6a4433
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-22 12:21:31 +0000

    [LeetCode Sync] - Same Tree - Runtime - 0 ms (100.00%), Memory - 13.1 MB (13.04%)

--- leetcode/0100-same-tree/solution.cpp
+++ leetcode/0100-same-tree/solution.cpp
@@ -11,15 +11,25 @@
  */
 class Solution {
 public:
-    bool giveAns(TreeNode* p,TreeNode*q){
-        if(!p || !q ){
-            return p==q;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<pair<TreeNode*, TreeNode*>>qu;
+
+        qu.push({p,q});
+
+        while(!qu.empty()){
+            auto[node1, node2] = qu.front();
+            qu.pop();
+
+            if(!node1 || !node2){
+                if(node1!=nullptr || node2!=nullptr) return false;
+            }else{
+                if(node1->val!=node2->val) return false;
+                qu.push({node1->left,node2->left});
+                qu.push({node1->right,node2->right});
+            }
+
         }
 
-        return p->val==q->val && giveAns(p->left,q->left) && giveAns(p->right,q->right);
-    }
-
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        return giveAns(p,q);  
+        return true;
     }
 };
