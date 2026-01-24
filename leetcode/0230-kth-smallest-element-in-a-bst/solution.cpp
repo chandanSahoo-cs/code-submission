1557c6677ed0dc4a9158548d277bc6b5aebe1f82
Chandan <chandansahoo02468@gmail.com>
2026-01-24 13:05:43 +0000

    [LeetCode Sync] - Kth Smallest Element in a BST - Runtime - 0 ms (100.00%), Memory - 24.3 MB (68.93%)

--- leetcode/0230-kth-smallest-element-in-a-bst/solution.cpp
+++ leetcode/0230-kth-smallest-element-in-a-bst/solution.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root, int k, int &cnt, int &ans){
-        if(root==nullptr){
-            return;
-        }
-        traverse(root->left,k,cnt,ans);
-        cnt++;
-        if(cnt==k){
-            ans=root->val;
-        }
-        traverse(root->right,k,cnt,ans);
-    }
-    
-    int kthSmallest(TreeNode* root, int k) {
-        int ans=-1;
-        int cnt=0;
+    int ans = -1;
 
-        traverse(root,k,cnt,ans);
+    void search(TreeNode* root, int k, int &ind){
+        if(root==nullptr) return;
+
+        search(root->left,k,ind);
+        ind++;
+        if(ind==k){
+            ans = root->val;
+        }
+
+        search(root->right,k,ind);
+
+        return;
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        int ind = 0;
+        search(root,k,ind);
 
         return ans;
     }
