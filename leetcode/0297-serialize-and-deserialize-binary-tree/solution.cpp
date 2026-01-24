dff27eeea5657716aa3a2cedff8311891d91ea93
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-24 17:58:38 +0000

    [LeetCode Sync] - Serialize and Deserialize Binary Tree - Runtime - 20 ms (90.98%), Memory - 31 MB (98.33%)

--- leetcode/0297-serialize-and-deserialize-binary-tree/solution.cpp
+++ leetcode/0297-serialize-and-deserialize-binary-tree/solution.cpp
@@ -10,96 +10,88 @@
 class Codec {
 public:
 
-    string traverse(TreeNode* root){
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
         if(root==nullptr) return "";
+        string s = "";
 
         queue<TreeNode*>q;
         q.push(root);
-        string s ="";
+
         while(!q.empty()){
-            vector<TreeNode*>temp;
+            TreeNode* temp = q.front();
+            q.pop();
 
-            while(!q.empty()){
-                temp.push_back(q.front());
-                q.pop();
+            if(temp==nullptr){
+                s+='#';
+            }else{
+                s+=to_string(temp->val);
+                q.push(temp->left);
+                q.push(temp->right);
             }
+            s+='|';
 
-            for(auto ele:temp){
-                if(ele==nullptr){
-                    s.push_back('#');
-                }else{
-                    q.push(ele->left);
-                    q.push(ele->right);
-
-                    string t = to_string(ele->val);
-                    for(auto ele:t){
-                        s.push_back(ele);
-                    }
-                }
-                s.push_back('|');
-            }
         }
-        // cout<<s<<"\n";
+
+
         return s;
     }
 
-    TreeNode* attach (string &s, int &itr){
-        string temp = "";
-        while(s[itr]!='|'){
-            if(s[itr]=='#'){
-                itr+=2;
-                return nullptr;
-            }
-            temp.push_back(s[itr]);
-            itr++;
+    TreeNode* giveNum(string &s, int &ind){
+        int strt = ind;
+
+        while(ind<s.size() && s[ind]!='|'){
+            ind++;
         }
-        // cout<<temp<<"\n";
-        int num = stoi(temp);
-        itr++;
-        return new TreeNode(num);
-    }
+        ind++;
 
-    TreeNode* negate(string &s){
-        if(s=="") return nullptr;
+        if(ind-strt-1==1 && s[strt]=='#') return nullptr;
 
-        queue<TreeNode*>q;
-        int itr=0;
+        bool sign = true;
+        int val = 0;
 
-        TreeNode* root = attach(s,itr);
-
-        q.push(root);
-        // itr++;
-
-        while(!q.empty() && itr<s.size()){
-            vector<TreeNode*>temp;
-
-            while(!q.empty()){
-                temp.push_back(q.front());
-                q.pop();
+        while(strt<ind-1){
+            if(s[strt]=='-') sign = false;
+            else{
+                val*=10;
+                val+=s[strt]-'0';
             }
-
-            for(auto ele:temp){
-                ele->left = attach(s,itr);
-                if(ele->left!=nullptr) q.push(ele->left);
-
-                ele->right = attach(s,itr);
-                if(ele->right!=nullptr) q.push(ele->right);
-            }
+            strt++;
         }
+
+        val = sign?val:-val;
+
+        TreeNode* root = new TreeNode(val);
 
         return root;
     }
 
-
-
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        return traverse(root);
-    }
-
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        return negate(data);
+        if(data=="") return nullptr;
+
+        int ind=0;
+
+        TreeNode* root = giveNum(data,ind);
+
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(ind<data.size()){
+            int sz = q.size();
+
+            for(int i=0;i<sz;i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                curr->left = giveNum(data,ind);
+                curr->right = giveNum(data,ind);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+        }
+
+        return root;
     }
 };
 
