24e2f49ec9eb92a55b898c115b74e1cd6685800d
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-25 10:30:12 +0000

    [LeetCode Sync] - Find Median from Data Stream - Runtime - 112 ms (11.08%), Memory - 148.6 MB (63.33%)

--- leetcode/0295-find-median-from-data-stream/solution.cpp
+++ leetcode/0295-find-median-from-data-stream/solution.cpp
@@ -1,30 +1,26 @@
 class MedianFinder {
 public:
-    priority_queue<int,vector<int>,greater<>>mnheap;
-    priority_queue<int>mxheap;
-
-    MedianFinder() {
-    }
+    priority_queue<int>pq1;
+    priority_queue<int,vector<int>,greater<int>>pq2;
+    MedianFinder() {}
     
     void addNum(int num) {
-        mnheap.push(num);
+        pq1.push(num);
+        pq2.push(pq1.top());
+        pq1.pop();
 
-        mxheap.push(mnheap.top());
-        mnheap.pop();
-
-        if(mxheap.size()>mnheap.size()){
-            mnheap.push(mxheap.top());
-            mxheap.pop();
+        if(pq1.size()<pq2.size()){
+            pq1.push(pq2.top());
+            pq2.pop();
         }
-
     }
     
     double findMedian() {
-        if(mnheap.size()>mxheap.size()){
-            return (double)mnheap.top();
+        if((pq1.size()+pq2.size())%2){
+            return pq1.top();
         }
-
-        return ((double)mnheap.top()+mxheap.top())/2;
+        
+        return double(pq1.top()+pq2.top())/2;
     }
 };
 
