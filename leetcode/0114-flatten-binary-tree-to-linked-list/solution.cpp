a560471913e1b1df215c87de1bea34610fbec9ae
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-25 09:54:10 +0000

    [LeetCode Sync] - Flatten Binary Tree to Linked List - Runtime - 0 ms (100.00%), Memory - 17.5 MB (77.07%)

--- leetcode/0114-flatten-binary-tree-to-linked-list/solution.cpp
+++ leetcode/0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -11,37 +11,23 @@
  */
 class Solution {
 public:
-
     void flatten(TreeNode* root) {
-        if(!root) return ;
-
         TreeNode* curr = root;
 
-        while(curr){
-            if(!curr->left){
-                curr = curr->right;
-            }else{
+        while(curr!=nullptr){
+            if(curr->left){
                 TreeNode* temp = curr->left;
 
-                while(temp->right!=nullptr && temp->right!=curr){
+                while(temp->right){
                     temp = temp->right;
                 }
 
                 temp->right = curr->right;
                 curr->right = curr->left;
                 curr->left = nullptr;
-                curr = curr->right;
-
-                // if(temp->right==nullptr){
-                //     temp->right = curr;
-                //     curr = curr->left;
-                // }else{
-                // }
             }
+
+            curr = curr->right;
         }
-
-
-
-        return ;    
     }
 };
