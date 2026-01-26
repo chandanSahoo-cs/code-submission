e5e86c7a45a95f453ab58d88d2d30f4c6fcb8b25
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-26 08:17:58 +0000

    [LeetCode Sync] - Minimum Absolute Difference - Runtime - 20 ms (14.01%), Memory - 36.5 MB (89.01%)

---
+++ leetcode/1306-minimum-absolute-difference/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int n = arr.size();
+        int mnDiff = INT_MAX;
+        
+        sort(arr.begin(),arr.end());
+
+        for(int i=1;i<n;i++){
+            mnDiff = min(mnDiff,arr[i]-arr[i-1]);
+        }
+
+        vector<vector<int>>ans;
+
+        for(int i=1;i<n;i++){
+            if(arr[i]-arr[i-1]==mnDiff){
+                ans.push_back({arr[i-1],arr[i]});
+            }
+        }
+
+        return ans;
+    }
+};
