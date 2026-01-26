129d934cad75b378033ca130a2d6c6b203a5e1de
Chandan <chandansahoo02468@gmail.com>
2026-01-26 13:07:04 +0000

    [LeetCode Sync] - Number of Enclaves - Runtime - 11 ms (38.37%), Memory - 31.7 MB (77.68%)

--- leetcode/1073-number-of-enclaves/solution.cpp
+++ leetcode/1073-number-of-enclaves/solution.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m =grid[0].size();
+        int n = grid.size(), m = grid[0].size();
 
-        vector<vector<int>>vis(n,vector<int>(m));
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
         queue<pair<int,int>>q;
-
-        int tot = 0;
-
-        int cnt=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 || i==n-1 || j==0 || j==m-1){
-                    if(grid[i][j]==1){
-                        vis[i][j] = 1;
-                        q.push({i,j});
-                        cnt++;
-                    }
+                if(grid[i][j]==1 && (i==0 || j==0 || i==n-1 || j==m-1)){
+                    q.push({i,j});
+                    vis[i][j]=1;
                 }
-                tot+=grid[i][j];
             }
         }
 
+        int dr[] = {0,0,-1,1};
+        int dc[] = {-1,1,0,0};
+
         while(!q.empty()){
-            pair<int,int>pr = q.front();
+            auto [r,c] = q.front();
             q.pop();
 
-            int f = pr.first;
-            int s = pr.second;
-
-            int dr[] = {0,0,-1,1};
-            int dc[] = {-1,1,0,0};
-
             for(int k=0;k<4;k++){
-                int nr = f+dr[k];
-                int nc = s+dc[k];
+                int row = r+dr[k];
+                int col = c+dc[k];
 
-                if(nr>=n || nr<0 || nc>=m || nc<0 || vis[nr][nc] || !grid[nr][nc]) continue;
-                q.push({nr,nc});
-                vis[nr][nc]=1;
-                cnt++;
+                if(row<0 || row>=n || col<0 || col>=m || vis[row][col] || !grid[row][col]) continue;
+
+                vis[row][col]=1;
+                q.push({row,col});
+            }
+        }
+
+        int tot = 0;
+        int cnt = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                tot+=grid[i][j];
+                cnt+=vis[i][j];
             }
         }
 
