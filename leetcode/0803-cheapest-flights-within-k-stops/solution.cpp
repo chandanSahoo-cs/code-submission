8768aba63d7557c263e850e66f922631f809dc9d
Chandan <chandansahoo02468@gmail.com>
2026-01-29 12:14:18 +0000

    [LeetCode Sync] - Cheapest Flights Within K Stops - Runtime - 3 ms (63.51%), Memory - 18.4 MB (34.68%)

--- leetcode/0803-cheapest-flights-within-k-stops/solution.cpp
+++ leetcode/0803-cheapest-flights-within-k-stops/solution.cpp
@@ -1,5 +1,5 @@
+const int INF = 1e9+7;
 class Solution {
-    const int INF = 1e8;
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adj(n);
@@ -8,30 +8,27 @@ public:
             adj[ele[0]].push_back({ele[1],ele[2]});
         }
 
-        queue<vector<int>>q;
+        vector<int>vis(n,INF);
+        queue<tuple<int,int,int>>q;
 
-        vector<int>dist(n,INF);
-
-        q.push({k,0,src});
-        dist[src]=0;
+        q.push({0,0,src});
+        vis[src] = 0;
 
         while(!q.empty()){
-            vector<int>v = q.front();
+            auto [currK,price,from] = q.front();
             q.pop();
 
-            int currK = v[0];
-            int cost = v[1];
-            int node = v[2];
+            if(currK>k) continue;
 
-            for(auto ele:adj[node]){
-                if(currK>=0 && dist[ele.first]>ele.second+cost){
-                    dist[ele.first] = ele.second+cost;
-                    q.push({currK-1,ele.second+cost,ele.first});
+
+            for(auto [v,p]:adj[from]){
+                if(vis[v]>p+price){
+                    q.push({currK+1,p+price,v});
+                    vis[v]=p+price;
                 }
             }
         }
 
-
-        return dist[dst]==INF?-1:dist[dst];
+        return vis[dst]==INF?-1:vis[dst];
     }
 };
