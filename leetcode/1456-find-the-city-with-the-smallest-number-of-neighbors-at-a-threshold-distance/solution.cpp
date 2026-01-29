0e9402a3783581aa10f57d9ef87988a47e0e9aa4
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-29 16:37:50 +0000

    [LeetCode Sync] - Find the City With the Smallest Number of Neighbors at a Threshold Distance - Runtime - 14 ms (60.89%), Memory - 17 MB (56.89%)

--- leetcode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
+++ leetcode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
@@ -1,46 +1,43 @@
+const int INF = 1e9+7;
 class Solution {
-    const int INF=1e8;
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
         vector<vector<int>>adj(n,vector<int>(n,INF));
 
         for(auto ele:edges){
-            adj[ele[0]][ele[1]]=ele[2];
-            adj[ele[1]][ele[0]]=ele[2];
+            adj[ele[0]][ele[1]] = ele[2];
+            adj[ele[1]][ele[0]] = ele[2];
         }
 
         for(int i=0;i<n;i++){
             adj[i][i]=0;
         }
 
-        for(int val=0;val<n;val++){
+        for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    if(i==val || j==val || adj[i][val]==INF || adj[val][j]==INF) continue;
-                    int curr = adj[i][val]+adj[val][j];
-                    if(adj[i][j]>curr){
-                        adj[i][j]=curr;
-                    }
+                    if(adj[i][k]==0 || adj[k][j]==0) continue;
+                    adj[i][j] = min(adj[i][j],adj[i][k]+adj[k][j]);
                 }
             }
         }
 
-        vector<int>cnt(n,0);
+        int mnCnt=INT_MAX;
+        int city=-1;
 
         for(int i=0;i<n;i++){
+            int cnt=0;
             for(int j=0;j<n;j++){
-                if(adj[i][j]<=distanceThreshold){
-                    cnt[i]++;
-                }
+                if(adj[i][j]<=distanceThreshold) cnt++;
+            }
+
+            if(cnt<=mnCnt){
+                mnCnt = cnt;
+                city = i;
             }
         }
 
-        int mn = *min_element(cnt.begin(),cnt.end());
-
-        for(int i=n-1;i>=0;i--){
-            if(cnt[i]==mn) return i;
-        }
-
-        return -1;
+        return city;
     }
 };
