3041c6a58d87d22221e584c37e3fe97feb5e317a
Chandan <chandansahoo02468@gmail.com>
2026-01-31 16:29:15 +0000

    [LeetCode Sync] - Swim in Rising Water - Runtime - 11 ms (36.16%), Memory - 13.7 MB (52.14%)

--- leetcode/0794-swim-in-rising-water/solution.cpp
+++ leetcode/0794-swim-in-rising-water/solution.cpp
@@ -1,39 +1,38 @@
+const int INF = 1e9+7;
+
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        priority_queue<vector<int>,vector<vector<int>>, greater<vector<int>>>pq;
+        vector<vector<int>>dist(n,vector<int>(n,INF));
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>>pq;
 
-        vector<vector<int>>mark(n,vector<int>(n,1e8));
-        mark[0][0]=grid[0][0];
+        pq.push({grid[0][0],0,0});
+        dist[0][0] = grid[0][0];
 
-        pq.push({0,0,grid[0][0]});
+        int dr[] = {0,0,-1,1};
+        int dc[] = {-1,1,0,0};
 
         while(!pq.empty()){
-            auto node = pq.top();
+            auto [w,i,j] = pq.top();
             pq.pop();
 
-            int i = node[0];
-            int j = node[1];
-            int lev = node[2];
-
-            int dr[] = {0,0,-1,1};
-            int dc[] = {-1,1,0,0};
+            if(dist[i][j]<w) continue;
 
             for(int k=0;k<4;k++){
-                int nr = i+dr[k];
-                int nc = j+dc[k];
+                int r = i+dr[k];
+                int c = j+dc[k];
 
-                if(nr<0 || nr>=n || nc<0 || nc>=n) continue;
+                if(r<0 || r>=n || c<0 || c>=n) continue;
 
-                if(mark[nr][nc]>lev){
-                    mark[nr][nc]=lev;
-                    pq.push({nr,nc,max(lev,grid[nr][nc])});
+                if(dist[r][c]>max(w,grid[r][c])){
+                    dist[r][c] = max(w,grid[r][c]);
+                    pq.push({dist[r][c],r,c});
                 }
-            }
+            } 
         }
 
-        return max(mark[n-1][n-1],grid[n-1][n-1]);
+        return dist[n-1][n-1];
     }
 };
