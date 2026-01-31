4defd49109dc4cfad44cbc158e0e601f845a9c0d
Chandan <chandansahoo02468@gmail.com>
2026-01-31 13:01:42 +0000

    [LeetCode Sync] - Accounts Merge - Runtime - 43 ms (31.43%), Memory - 38.6 MB (88.45%)

--- leetcode/0721-accounts-merge/solution.cpp
+++ leetcode/0721-accounts-merge/solution.cpp
@@ -1,9 +1,9 @@
-class DisjointSet{
+class DSU{
     vector<int>sz,parent;
 
     public:
-    DisjointSet(int n){
-        sz.resize(n);
+    DSU(int n){
+        sz.resize(n,1);
         parent.resize(n);
 
         for(int i=0;i<n;i++){
@@ -13,10 +13,11 @@ class DisjointSet{
 
     int findParent(int u){
         if(parent[u]==u) return u;
+
         return parent[u]=findParent(parent[u]);
     }
 
-    void Union(int u, int v){
+    void unionBySz(int u, int v){
         int ult_u = findParent(u);
         int ult_v = findParent(v);
 
@@ -27,45 +28,58 @@ class DisjointSet{
             sz[ult_u]+=sz[ult_v];
         }else{
             parent[ult_u]=ult_v;
-            sz[ult_v]+=ult_u;
+            sz[ult_v]+=sz[ult_u];
         }
+
+        return;
+    }
+
+    int totSets(){
+        int cnt=0;
+
+        for(int i=0;i<parent.size();i++){
+            if(i==parent[i]) cnt++;
+        }
+
+        return cnt;
     }
 };
 
+
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& a) {
-        int n = a.size();
-        
-        DisjointSet ds(n);
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
 
-        map<string, int>mp;
+        DSU ds(n);
+
+        map<string,int>mp;
 
         for(int i=0;i<n;i++){
-            int m = a[i].size();
-            for(int j=1;j<m;j++){
-                if(mp.find(a[i][j])!=mp.end()){
-                    ds.Union(mp[a[i][j]],i);
+            for(int j=1;j<accounts[i].size();j++){
+                if(mp.find(accounts[i][j])!=mp.end()){
+                    ds.unionBySz(mp[accounts[i][j]],i);
                 }else{
-                    mp[a[i][j]]=i;
+                    mp[accounts[i][j]]=i;
                 }
             }
         }
+
         vector<vector<string>>store(n);
 
         for(int i=0;i<n;i++){
-            store[i].push_back(a[i][0]);
+            store[i].push_back(accounts[i][0]);
         }
 
         for(auto [key,value]:mp){
-            int ult = ds.findParent(value);
-            store[ult].push_back(key);
+            int ind = ds.findParent(value);
+            store[ind].push_back(key);
         }
 
         vector<vector<string>>ans;
 
-        for(int i=0;i<n;i++){
-            if(store[i].size()>1) ans.push_back(store[i]);
+        for(auto ele:store){
+            if(ele.size()>1) ans.push_back(ele);
         }
 
         return ans;
