4f59eeefb2fcfc3543bd61b44ef2204f531df200
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-01-31 07:53:44 +0000

    [LeetCode Sync] - Find Smallest Letter Greater Than Target - Runtime - 0 ms (100.00%), Memory - 19.7 MB (60.62%)

--- leetcode/0745-find-smallest-letter-greater-than-target/solution.cpp
+++ leetcode/0745-find-smallest-letter-greater-than-target/solution.cpp
@@ -1,19 +1,12 @@
 class Solution {
-    #define ll long long
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        ll s=0,e=letters.size()-1; bool flag=false;
-        while(s<=e){
-            ll m=s+(e-s)/2;
-            if(letters[m]>target) {
-                flag=true;
-                e=m-1;
-            }
-            else{
-                s=m+1;
-            }
+        int n = letters.size();
+
+        for(int i=0;i<n;i++){
+            if(letters[i]>target) return letters[i];
         }
-        if(flag) return letters[s];
-        else return letters[0];
+
+        return letters[0];
     }
 };
