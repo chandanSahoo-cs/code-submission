defc3933451d61df3583a01329031064d9fe9709
Chandan <chandansahoo02468@gmail.com>
2026-02-28 10:15:01 +0000

    [LeetCode Sync] - Maximum Sum BST in Binary Tree - Runtime - 2 ms (76.01%), Memory - 104.7 MB (69.83%)

--- leetcode/1475-maximum-sum-bst-in-binary-tree/solution.cpp
+++ leetcode/1475-maximum-sum-bst-in-binary-tree/solution.cpp
@@ -12,18 +12,20 @@
 class Solution {
 public:
     int sum = 0;
-    tuple<int,int,int> search(TreeNode* root){
-        if(root==nullptr) return {0,INT_MIN,INT_MAX};
-
-        auto [ sumL, largestL, smallestL] = search(root->left);
-        auto [ sumR, largestR, smallestR] = search(root->right);
-
-        if(root->val>largestL && root->val<smallestR){
-            sum = max(sum,sumL+sumR+root->val);
-            return {sumL+sumR+root->val,max(root->val,largestR),min(root->val,smallestL)};
+    tuple<int,int,int>search(TreeNode* root){
+        if(root==nullptr){
+            return {0,INT_MIN,INT_MAX};
         }
 
-        return {0,INT_MAX,INT_MIN};
+        auto [sumL, largestL,smallestL] = search(root->left);
+        auto [sumR, largestR, smallestR] = search(root->right);
+
+        if(root->val>largestL && root->val<smallestR){
+            sum = max(sum,root->val+sumL+sumR);
+            return {root->val+sumL+sumR,max(root->val,largestR),min(root->val,smallestL)};
+        }
+
+        return {-1,INT_MAX,INT_MIN};
     }
 
     int maxSumBST(TreeNode* root) {
