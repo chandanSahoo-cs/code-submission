7cbf1ecf3047b2c1acded53b8bdcc66fd18816c0
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-11 00:17:16 +0000

    [LeetCode Sync] - Complement of Base 10 Integer - Runtime - 0 ms (100.00%), Memory - 8.1 MB (6.12%)

---
+++ leetcode/1054-complement-of-base-10-integer/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0) return 1;
+
+        int ans = 0;
+        int len = log2(n);
+
+
+        for(int i=len;i>=0;i--){
+            ans*=2;
+            ans+=!((1<<i)&n);
+        }
+
+        return ans;
+    }
+};
+
+/*
+101
+*/
