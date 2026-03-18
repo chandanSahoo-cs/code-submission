7848c73c0e4ac1b5acb1c0661b3c2b446219b64d
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-18 01:17:01 +0000

    [LeetCode Sync] - Count Submatrices with Top-Left Element and Sum Less Than k - Runtime - 7 ms (77.67%), Memory - 90.4 MB (96.53%)

---
+++ leetcode/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        // vector<vector<int>>grid = grid;
+
+        int n = grid.size(), m = grid[0].size();
+        int cnt = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i>0){
+                    grid[i][j]+=grid[i-1][j];
+                }
+
+                if(j>0){
+                    grid[i][j]+=grid[i][j-1];
+                }
+
+                if(i>0 && j>0){
+                    grid[i][j]-=grid[i-1][j-1];
+                }
+
+                if(grid[i][j]<=k) cnt++;
+            }
+        }
+
+        return cnt;
+
+
+    }
+};
