5ee9ce55cb3a12290640a07ba2bc2e06a4cd4cba
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-22 01:35:29 +0000

    [LeetCode Sync] - Determine Whether Matrix Can Be Obtained By Rotation - Runtime - 0 ms (100.00%), Memory - 14.5 MB (73.69%)

--- leetcode/2015-determine-whether-matrix-can-be-obtained-by-rotation/solution.cpp
+++ leetcode/2015-determine-whether-matrix-can-be-obtained-by-rotation/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
-        for(int rot=0;rot<4;rot++){
-            for(int i=0;i<mat.size();i++){
-                for(int j=i+1;j<mat.size();j++){
-                    swap(mat[i][j],mat[j][i]);
-                }
+        int n = mat.size();
+
+        bool flag90=true;
+        bool flag180=true;
+        bool flag270=true;
+        bool flag0=true;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]!=target[i][j]) flag0=false;
+                if(mat[j][n-1-i]!=target[i][j]) flag90=false;
+                if(mat[n-1-i][n-1-j]!=target[i][j]) flag180=false;
+                if(mat[n-1-j][i]!=target[i][j]) flag270=false;
             }
-            
-            for(int i=0;i<mat.size();i++){
-                reverse(mat[i].begin(),mat[i].end());
-            }
-            
-            if(mat==target)
-                return true;
         }
-        return false;
+
+        return flag0 | flag90 | flag180 | flag270;
     }
 };
-
