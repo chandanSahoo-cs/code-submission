9c82a2847b0c3c5b873549bcafc41fa6009b8c5d
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-27 15:01:26 +0000

    [LeetCode Sync] - Rotting Oranges - Runtime - 0 ms (100.00%), Memory - 17.3 MB (18.70%)

--- leetcode/1036-rotting-oranges/solution.cpp
+++ leetcode/1036-rotting-oranges/solution.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>>mark(n,vector<int>(m));
+        int n = grid.size(), m = grid[0].size();
 
         queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m));
+
+        bool flag = false;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2 || grid[i][j]==0){
+                if(grid[i][j]==1 || grid[i][j]==2) flag=true;
+                if(grid[i][j]==2){
                     q.push({i,j});
-                    mark[i][j]=1;
+                    vis[i][j]=1;
                 }
             }
         }
+
+        if(!flag) return 0;
+
+        int dr[] = {0,0,-1,1};
+        int dc[] = {-1,1,0,0};
 
         int level = 0;
 
         while(!q.empty()){
             int sz = q.size();
             level++;
+
             for(int i=0;i<sz;i++){
                 auto [r,c] = q.front();
                 q.pop();
-                if(grid[r][c]==0) continue;
-
-                int dr[] = {0,0,1,-1};
-                int dc[] = {-1,1,0,0};
 
                 for(int k=0;k<4;k++){
-                    int row = r+dr[k];
-                    int col = c+dc[k];
+                    int tr = r+dr[k];
+                    int tc = c+dc[k];
 
-                    if(row<0 || row>=n || col<0 || col>=m || grid[row][col]==0 || mark[row][col]) continue;
-                    mark[row][col] = 1;
-                    q.push({row,col});
-                } 
+                    if(tr<0 || tr>=n || tc<0 || tc>=m || !grid[tr][tc] || vis[tr][tc]) continue;
+                    q.push({tr,tc});
+                    vis[tr][tc]=1;
+                }
             }
         }
-
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && !mark[i][j]) return -1;
+                if(grid[i][j]==1 && !vis[i][j]) return -1;
             }
         }
 
