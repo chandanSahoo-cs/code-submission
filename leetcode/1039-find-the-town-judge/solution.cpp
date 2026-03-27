791df13d23f45e84ad227230fc9e0dd2e59a9d75
Chandan <chandansahoo02468@gmail.com>
2026-03-27 10:27:13 +0000

    [LeetCode Sync] - Find the Town Judge - Runtime - 52 ms (7.01%), Memory - 131.3 MB (5.04%)

--- leetcode/1039-find-the-town-judge/solution.cpp
+++ leetcode/1039-find-the-town-judge/solution.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        
-        vector<int>indegree(n+1);
-        vector<int>outdegree(n+1);
+        vector<vector<int>>adj(n+1,vector<int>(n+1));
 
-        for(auto &ele:trust){
-            indegree[ele[1]]++;
-            outdegree[ele[0]]++;
+        for(auto ele:trust){
+            adj[ele[0]][ele[1]]=1;
+        }
+
+        int l = 1;
+
+        for(int i=1;i<=n;i++){
+            if(adj[l][i]){
+                l=i;
+            }
         }
 
         for(int i=1;i<=n;i++){
-            if(indegree[i]==n-1 && outdegree[i]==0) return i;
+            if(i==l) continue;
+            if(adj[l][i] || !adj[i][l]) return -1;
         }
-        return -1;
+
+        return l;
     }
 };
