814f6de6b6771b53f788c3a33aaf33871de4c0cc
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-30 11:33:20 +0000

    [LeetCode Sync] - Network Delay Time - Runtime - 84 ms (55.18%), Memory - 44.5 MB (54.00%)

--- leetcode/0744-network-delay-time/solution.cpp
+++ leetcode/0744-network-delay-time/solution.cpp
@@ -1,39 +1,40 @@
-const int INF = 1e9+7;
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
         vector<vector<pair<int,int>>>adj(n+1);
+        vector<int>dist(n+1,INT_MAX);
 
-        for(auto ele:times){
+        for(auto &ele:times){
             adj[ele[0]].push_back({ele[1],ele[2]});
         }
 
-        set<pair<int,int>>st;
-        vector<int>vis(n+1,INF);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
 
-        st.insert({k,0});
-        vis[k]=0;
+        pq.push({0,k});
+        dist[k]=0;
 
-        while(!st.empty()){
-            auto [u,cost] = *st.begin();
-            st.erase(st.begin());
+        while(!pq.empty()){
+            auto [w,u]= pq.top();
+            pq.pop();
 
-            if(vis[u]<cost) continue;
+            if(dist[u]<w) continue;
 
-            for(auto [v,c]:adj[u]){
-                if(vis[v]>c+cost){
-                    st.erase({v,vis[v]});
-                    vis[v]=c+cost;
-                    st.insert({v,c+cost});
+            for(auto &[v,weigh]:adj[u]){
+                if(weigh+w<dist[v]){
+                    dist[v] = weigh+w;
+                    pq.push({dist[v],v});
                 }
             }
         }
 
         int mx = 0;
+
         for(int i=1;i<=n;i++){
-            mx = max(mx,vis[i]);
+            mx = max(dist[i],mx);
+            if(dist[i]==INT_MAX) return -1;
         }
 
-        return mx==INF?-1:mx;
+        return mx;
     }
 };
