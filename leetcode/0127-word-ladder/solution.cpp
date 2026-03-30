ec9b0dac4bf5ded33e01c0a7384c1c0b03c7ba24
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-30 10:08:44 +0000

    [LeetCode Sync] - Word Ladder - Runtime - 77 ms (44.12%), Memory - 21.3 MB (32.96%)

--- leetcode/0127-word-ladder/solution.cpp
+++ leetcode/0127-word-ladder/solution.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         int n = wordList.size();
-
+        
         unordered_set<string>st(wordList.begin(),wordList.end());
-        unordered_map<string,int>mp;
+        unordered_set<string>vis;
 
         queue<string>q;
-        q.push(beginWord);
-        mp[beginWord]=1;
-
-        int level= 1;
         
+        q.push(beginWord);
+        vis.insert(beginWord);
+
+        int level = 1;
+
         while(!q.empty()){
             int sz = q.size();
             level++;
-            for(int j=0;j<sz;j++){
-                string curr = q.front();
+
+            for(int k=0;k<sz;k++){
+                string u = q.front();
                 q.pop();
 
-                int len = curr.size();
+                for(int i=0;i<u.size();i++){
+                    char curr = u[i];
 
-                for(int i=0;i<len;i++){
-                    string temp = curr;
                     for(char c='a';c<='z';c++){
-                        temp[i] = c;
-                        if(st.count(temp) && mp.find(temp)==mp.end()){
-                            if(temp==endWord) return level;
-                            mp[temp]=level;
-                            q.push(temp);
+                        u[i]=c;
+                        if(st.count(u) && !vis.count(u)){
+                            if(u==endWord) return level;
+                            q.push(u);
+                            vis.insert(u);
                         }
                     }
-                }
+                    u[i]=curr;
+                } 
             }
         }
 
