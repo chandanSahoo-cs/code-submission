14280ac49d3a9eb80c5fa5699f443019091c30ea
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-03-31 13:53:21 +0000

    [LeetCode Sync] - Swim in Rising Water - Runtime - 7 ms (64.41%), Memory - 13.8 MB (55.41%)

--- leetcode/0794-swim-in-rising-water/solution.cpp
+++ leetcode/0794-swim-in-rising-water/solution.cpp
@@ -1,36 +1,31 @@
-const int INF = 1e9+7;
-
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        vector<vector<int>>dist(n,vector<int>(n,INF));
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
         priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>>pq;
 
         pq.push({grid[0][0],0,0});
-        dist[0][0] = grid[0][0];
+        dist[0][0]=grid[0][0];
 
         int dr[] = {0,0,-1,1};
         int dc[] = {-1,1,0,0};
 
         while(!pq.empty()){
-            auto [w,i,j] = pq.top();
+            auto [w,r,c] = pq.top();
             pq.pop();
 
-            if(dist[i][j]<w) continue;
+            if(dist[r][c]<w) continue;
 
             for(int k=0;k<4;k++){
-                int r = i+dr[k];
-                int c = j+dc[k];
+                int tr = r+dr[k];
+                int tc = c+dc[k];
 
-                if(r<0 || r>=n || c<0 || c>=n) continue;
-
-                if(dist[r][c]>max(w,grid[r][c])){
-                    dist[r][c] = max(w,grid[r][c]);
-                    pq.push({dist[r][c],r,c});
-                }
-            } 
+                if(tr<0 || tr>=n || tc<0 || tc>=n || dist[tr][tc]<=max(w,grid[tr][tc])) continue;
+                dist[tr][tc] = max(w,grid[tr][tc]);
+                pq.push({dist[tr][tc],tr,tc});
+            }
         }
 
         return dist[n-1][n-1];
