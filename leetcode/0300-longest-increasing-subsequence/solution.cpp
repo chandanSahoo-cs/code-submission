e65c390f25a2f7daed6c2e3e96b389f60182f59d
Chandan <chandansahoo02468@gmail.com>
2026-04-10 16:02:58 +0000

    [LeetCode Sync] - Longest Increasing Subsequence - Runtime - 3 ms (79.36%), Memory - 14.3 MB (71.09%)

--- leetcode/0300-longest-increasing-subsequence/solution.cpp
+++ leetcode/0300-longest-increasing-subsequence/solution.cpp
@@ -1,39 +1,20 @@
 class Solution {
-    int search(vector<int>&a, int val){
-        int l=0,r=a.size()-1;
-        int ind=a.size();
-
-        while(l<=r){
-            int m = l+(r-l)/2;
-            if(a[m]<val){
-                l=m+1;
-            }else{
-                ind=m;
-                r=m-1;
-            }
-        }
-
-        return ind;
-    }
-
 public:
-    int lengthOfLIS(vector<int>& a) {
-        int n = a.size();
-        vector<int>dp(n,1e9);
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>lis(n,1e9);
 
         for(int i=0;i<n;i++){
-            int ind = search(dp,a[i]);
-            dp[ind] = a[i];
+            int ind = lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
+            lis[ind] = nums[i];
         }
 
-        int ans=1;
+        int len=0;
 
-        for(int i=0;i<n && dp[i]<1e9;i++){
-            ans=i+1;
+        for(int i=0;i<n && lis[i]<1e9;i++){
+            len=i;
         }
 
-        return ans;
-
+        return len+1;
     }
 };
-
