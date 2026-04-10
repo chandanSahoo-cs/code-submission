aa53156322231a01a6f6a5d2d9f804e2fc37985b
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-10 10:25:57 +0000

    [LeetCode Sync] - House Robber - Runtime - 0 ms (100.00%), Memory - 10.5 MB (59.72%)

--- leetcode/0198-house-robber/solution.cpp
+++ leetcode/0198-house-robber/solution.cpp
@@ -1,22 +1,14 @@
 class Solution {
-int dp[105];    
 public:
-    int rec(int n, vector<int>&a, int i){
-        if(i>=n) return 0;
-        if(i==n-1) return a[i];
 
-        if(dp[i]!=-1) return dp[i];
-        int ans=0;
-        //take
-        ans = max(ans,a[i]+rec(n,a,i+2));
-        // not take
-        ans = max(ans,rec(n,a,i+1));
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n+2);
 
-        return dp[i] = ans;
-    }
+        for(int i=n-1;i>=0;i--){
+            dp[i] = max(dp[i+1],nums[i]+dp[i+2]);
+        }
 
-    int rob(vector<int>& a) {
-        memset(dp,-1,sizeof(dp));
-        return rec(a.size(),a,0);
+        return dp[0];    
     }
 };
