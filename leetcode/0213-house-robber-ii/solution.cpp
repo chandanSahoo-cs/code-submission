99d8712d17f9aa891a867f3d7a2cb61be27fab29
Chandan <chandansahoo02468@gmail.com>
2026-04-10 10:40:11 +0000

    [LeetCode Sync] - House Robber II - Runtime - 1 ms (10.62%), Memory - 10.7 MB (61.36%)

--- leetcode/0213-house-robber-ii/solution.cpp
+++ leetcode/0213-house-robber-ii/solution.cpp
@@ -1,28 +1,18 @@
 class Solution {
-    int dp[105];
 public:
-    int rec(int n, vector<int>&a, int i){
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
 
-        for(int j=n-1;j>=i;j--){
-            if(j==n-1) dp[j]=a[j];
-            else{
-                dp[j]=a[j];
-                dp[j]=max(dp[j],dp[j+1]);
-                if(j+2<n){
-                    dp[j] = max(dp[j],a[j]+dp[j+2]);
-                }
+        vector<int>dp0(n+2),dp1(n+2);
+
+        for(int i=n-1;i>=0;i--){
+            dp1[i] = max(dp1[i+1],nums[i]+dp1[i+2]);
+            if(i<n-1){
+                dp0[i] = max(dp0[i+1],nums[i]+dp0[i+2]);
             }
         }
 
-        return dp[i];
-    }
-
-    int rob(vector<int>& a) {
-        int n = a.size();
-        if(n==1) return a[0];
-        int p1 = rec(n-1,a,0);
-        int p2 = rec(n,a,1);
-
-        return max(p1,p2);
+        return max(dp0[0],dp1[1]);
     }
 };
