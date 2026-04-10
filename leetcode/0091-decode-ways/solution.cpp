3c21ce209cb9b91c255817e2ededf1a0fbf855d2
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-10 11:59:18 +0000

    [LeetCode Sync] - Decode Ways - Runtime - 0 ms (100.00%), Memory - 8.5 MB (79.79%)

--- leetcode/0091-decode-ways/solution.cpp
+++ leetcode/0091-decode-ways/solution.cpp
@@ -2,36 +2,20 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n, 0);
-        if (s[0] == '0')
-            return 0;
-        dp[0] = 1;
-        if(n==1 )return dp[0];
-        int x = s[0] - '0';
-        x = x * 10 + (s[1] - '0');
-        if(x == 10 || x == 20 || (x>= 27 && x % 10 !=0 )
-          )dp[1] = 1;
-        else if(x>10 && x <27 )dp[1] = 2;
-        else if(x>= 27 && x % 10 ==0 )dp[1]=0;
-        if(dp[1] == 0) return 0;
-        x = (s[1] - '0');
-        
-        for (int i = 2; i < n; i++) {
-        
-            x = x * 10 + (s[i] - '0');
-                if (x < 27 && x >= 10)
-                    dp[i] = dp[i - 1] + dp[i-2];
-                else
-                    dp[i] = dp[i - 1];
-                x = (s[i] - '0');
-            if(x == 0)
-            {
-                dp[i] -= dp[i-1];
-                if(dp[i] <= 0) return dp[i] ;
-                
+        vector<int>dp(n+2);
+        dp[n]=dp[n+1]=1;
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+                continue;
             }
+            int ans = dp[i+1];
+
+            if(i<n-1 && (s[i]=='1' || (s[i]=='2' && s[i+1]<='6'))) ans+=dp[i+2];
+            dp[i] = ans;
         }
 
-        return dp[n - 1];
+        return dp[0];
     }
 };
