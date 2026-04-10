f994570520023958fef511bbac7da558b9ad8e96
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-10 11:26:10 +0000

    [LeetCode Sync] - Longest Palindromic Substring - Runtime - 3 ms (98.04%), Memory - 9.3 MB (92.85%)

--- leetcode/0005-longest-palindromic-substring/solution.cpp
+++ leetcode/0005-longest-palindromic-substring/solution.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    pair<int,int> givePalindrome(string &s, int l, int r){
+    pair<int,int> compute(string &s, int l, int r){
         int n = s.size();
+        while(l>=0 && r<=n-1 && s[l]==s[r]) l--, r++;
 
-        while(l>=0 && r<n && s[l]==s[r]){
-            l--;
-            r++;
-        }
-        l++;
-        r--;
-
-        return {l,r};
+        return {l+1,r-1};
     }
 
     string longestPalindrome(string s) {
-       int n = s.size();
+        int n = s.size();
 
-       int l=0,r=0;
-       int mx = 1;
+        int l=-1,r=-1,mx=0;
 
-       for(int i=0;i<n;i++){
-            auto [l1,r1] = givePalindrome(s,i,i);
+        for(int i=0;i<n;i++){
+            auto [l1,r1] = compute(s,i,i);
+
             if(r1-l1+1>mx){
-                mx = max(mx,r1-l1+1);
+                mx = r1-l1+1;
                 l=l1,r=r1;
             }
 
-            if(i!=0){
-                auto [l2,r2] = givePalindrome(s,i,i-1);
+            if(i>0){
+                auto [l2,r2] = compute(s,i-1,i);
                 if(r2-l2+1>mx){
-                    mx = max(mx,r2-l2+1);
+                    mx = r2-l2+1;
                     l=l2,r=r2;
                 }
             }
-       }
+        }
 
-       return s.substr(l,r-l+1); 
+        return s.substr(l,r-l+1);
     }
 };
