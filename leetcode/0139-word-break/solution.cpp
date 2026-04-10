6674f845659bdefd680af0705cb53ca9c393707f
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-10 14:53:11 +0000

    [LeetCode Sync] - Word Break - Runtime - 11 ms (48.06%), Memory - 16.2 MB (30.27%)

--- leetcode/0139-word-break/solution.cpp
+++ leetcode/0139-word-break/solution.cpp
@@ -1,46 +1,19 @@
 class Solution {
 public:
-    // int dp[305][305];
-
-    // int rec(string &s, unordered_set<string>&st, int l, int r){
-    //     int n = s.size();
-    //     if(r==n-1){
-    //         if(st.find(s.substr(l,r-l+1))!=st.end()) return 1;
-    //         return 0;
-    //     }
-
-    //     if(dp[l][r]!=-1) return dp[l][r];
-
-    //     bool flag=false;
-
-    //     if(st.find(s.substr(l,r-l+1))!=st.end()){
-    //         flag |=rec(s,st,r+1,r+1);
-    //     }
-
-    //     flag |=rec(s,st,l,r+1);
-
-    //     return dp[l][r] = flag;
-
-    // }
-
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
 
-        vector<bool>curr(n);
         unordered_set<string>st(wordDict.begin(),wordDict.end());
+        vector<int>dp(n+1);
+        dp[n]=1;
 
-       for(int l=0;l<n;l++){
-        curr[l] = (st.find(s.substr(l,n-l))!=st.end());
-       }
-
-        for(int r=n-2; r>=0;r--){
-            for(int l=r;l>=0;l--){
-                if(st.find(s.substr(l,r-l+1))!=st.end()){
-                    curr[l] = curr[l] || curr[r+1];
-                }
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                dp[i] = dp[i] || (st.count(s.substr(i,j-i+1)) && dp[j+1]);
+                if(dp[i]) break;
             }
         }
 
-        return curr[0];
+        return dp[0];
     }
 };
