daa94013a5eeb7c721ec77032e2a4c6cc237240e
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-11 16:40:44 +0000

    [LeetCode Sync] - Word Break II - Runtime - 0 ms (100.00%), Memory - 9.9 MB (94.73%)

--- leetcode/0140-word-break-ii/solution.cpp
+++ leetcode/0140-word-break-ii/solution.cpp
@@ -1,69 +1,51 @@
 class Solution {
 public:
     vector<string>ans;
-    int dp[305][305];
-
-    bool rec(string &s, unordered_set<string>&st, int l, int r){
+    int dp[25];
+    bool rec(vector<string>& wordDict, string &s, int i){
         int n = s.size();
 
-        if(r==n-1){
-            return st.count(s.substr(l,r-l+1));
+        if(i>=n) return true;
+
+        if(dp[i]!=-1) return dp[i];
+
+        
+        for(auto &ele:wordDict){
+            int m = ele.size();
+            if(ele==s.substr(i,m) && rec(wordDict,s,i+m)){
+                return dp[i]=true;
+            }
         }
 
-        if(dp[l][r]!=-1) return dp[l][r];
-
-        bool flag=false;
-
-        if(st.count(s.substr(l,r-l+1))){
-            flag|=rec(s,st,r+1,r+1);
-        }
-
-        flag|=rec(s,st,l,r+1);
-
-        return dp[l][r] = flag;
+        return dp[i] = false; 
     }
 
-    void give(string &s,unordered_set<string>&st, int l, int r, string keep){
+    void print(vector<string> &wordDict, string &s, int i, string &path){
         int n = s.size();
-
-        string t = s.substr(l,r-l+1);
-        if(r==n-1){
-            if(st.count(t)){
-                keep+=t;
-                ans.push_back(keep);
-                keep.erase(keep.size()-t.size());
-            }
-
+        if(i>=n){
+            ans.push_back(path);
             return;
         }
 
-        if(st.count(t)){
-            if(rec(s,st,r+1,r+1)){
-                keep+=t;
-                keep.push_back(' ');
-
-                give(s,st,r+1,r+1,keep);
-
-                keep.erase(keep.size()-t.size());
-                keep.pop_back();
+        for(auto &ele:wordDict){
+            int m = ele.size();
+            string temp = s.substr(i,m);
+            if(ele==temp && rec(wordDict,s,i+m)){
+                if(path.size()>0) path+=' ';
+                path+=temp;
+                print(wordDict,s,i+m,path);
+                while(m--) path.pop_back();
+                if(path.size()>1) path.pop_back();
             }
-        }
-
-        if(rec(s,st,l,r+1)){
-            give(s,st,l,r+1,keep);
         }
 
         return;
     }
 
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        unordered_set<string>st(wordDict.begin(),wordDict.end());
-
         memset(dp,-1,sizeof(dp));
-
-        string keep = "";
-        give(s,st,0,0,keep);
+        string path;
+        print(wordDict,s,0,path);
 
         return ans;
     }
