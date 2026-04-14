5eee4c07d6bcacff03ddf90f3f1a3399c73c9b45
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-14 12:59:35 +0000

    [LeetCode Sync] - Best Time to Buy and Sell Stock with Cooldown - Runtime - 0 ms (100.00%), Memory - 16.2 MB (33.54%)

--- leetcode/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ leetcode/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,28 +1,18 @@
 class Solution {
-int dp[5005][2];
 public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n+2,vector<int>(2));
 
-    int rec(vector<int>&a, int i, int p){
-        int n = a.size();
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                int p = j?-prices[i]+dp[i+1][!j]:prices[i]+dp[i+2][!j];
+                int np = dp[i+1][j];
 
-        if(i>=n) return 0;
-
-        if(dp[i][p]!=-1) return dp[i][p];
-
-        int ans = 0;
-
-        if(p){
-            ans = max(rec(a,i+1,1),-a[i]+rec(a,i+1,0));
-        }else{
-            ans = max(rec(a,i+1,0),a[i]+rec(a,i+2,1));
+                dp[i][j] = max(p,np);
+            }
         }
 
-        return dp[i][p] = ans;
-    }
-
-    int maxProfit(vector<int>& a) {
-        memset(dp,-1,sizeof(dp));
-
-        return rec(a,0,1);
+        return dp[0][1]; 
     }
 };
