43b9570211143e33c54fb084cb22ae19235f5237
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-18 14:19:30 +0000

    [LeetCode Sync] - Target Sum - Runtime - 3 ms (84.64%), Memory - 12.4 MB (63.74%)

--- leetcode/0494-target-sum/solution.cpp
+++ leetcode/0494-target-sum/solution.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int dp[25][4005];
-    int buffer = 2000;
-
-    int rec(vector<int>& nums, int target, int i){
-        int n = nums.size();
-
-        if(i==n){
-            if(target==0) return 1;
-            return 0;
-        }
-
-        if(dp[i][target+buffer]!=-1) return dp[i][target+buffer];
-
-        return dp[i][target+buffer] = rec(nums,target-nums[i],i+1)+rec(nums,target+nums[i],i+1);
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        memset(dp,-1,sizeof(dp));
-        return rec(nums,target,0);    
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        
+        if((sum+target)&1) return 0;
+        int t = (sum+target)>>1;
+
+        if(t<0) return 0;
+
+        vector<int>curr(t+1),next(t+1);
+        next[0]=1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=t;j++){
+                int ans = 0;
+                if(j-nums[i]>=0){
+                  ans += next[j-nums[i]];  
+                }
+                ans+=next[j];
+                curr[j] = ans;
+            }
+            next = curr;
+        }  
+
+        return next[t];
     }
 };
