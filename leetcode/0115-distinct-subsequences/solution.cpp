db6105b9a5dba74766d26c906d34f644312e6245
Chandan <chandansahoo02468@gmail.com>
2026-04-18 11:43:15 +0000

    [LeetCode Sync] - Distinct Subsequences - Runtime - 18 ms (75.30%), Memory - 8.9 MB (94.67%)

--- leetcode/0115-distinct-subsequences/solution.cpp
+++ leetcode/0115-distinct-subsequences/solution.cpp
@@ -1,34 +1,23 @@
 class Solution {
-    int dp[1005][1005];
 public:
-
-    int rec(string &s, string &t,int i,int j){
-        int n = s.size();
-        int m = t.size();
-
-        if(j==m) return 1;
-        if(i==n) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int ans=rec(s,t,i+1,j);
-
-        if(s[i]==t[j]){
-            ans+=rec(s,t,i+1,j+1);
-        }
-
-        return dp[i][j] = ans;
-    }
-
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
+        int n = s.size(), m = t.size();
 
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                dp[i][j]=-1;
+        vector<long long>next(m+1), curr(m+1);
+        next[m]=curr[m]=1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int cnt = next[j];
+                if(s[i]==t[j]){
+                    cnt+=next[j+1];
+                }
+
+                curr[j] = cnt;
             }
+            next = curr;
         }
-        return rec(s,t,0,0);
+
+        return next[0];
     }
 };
