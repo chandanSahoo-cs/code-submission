e95efccc6775c694dc407de1ee91aaedf3abeb6c
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-18 07:56:02 +0000

    [LeetCode Sync] - Mirror Distance of an Integer - Runtime - 0 ms (100.00%), Memory - 8.6 MB (56.26%)

---
+++ leetcode/4168-mirror-distance-of-an-integer/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rev(int n){
+        int ans = 0;
+
+        while(n){
+            ans*=10;
+            ans+=n%10;
+            n/=10;
+        }
+
+        return ans;
+    }
+
+    int mirrorDistance(int n) {
+        return abs(n-rev(n));    
+    }
+};
