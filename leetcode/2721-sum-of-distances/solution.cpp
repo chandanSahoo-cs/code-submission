7defcd20fde155bb5cd6476657c5b06e2e815f4b
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-04-23 14:27:32 +0000

    [LeetCode Sync] - Sum of Distances - Runtime - 81 ms (28.77%), Memory - 115.5 MB (50.71%)

---
+++ leetcode/2721-sum-of-distances/solution.cpp
@@ -0,0 +1,43 @@
+#define ll long long
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        ll n = nums.size();
+
+        unordered_map<int,vector<ll>>mp;
+        vector<int>ind(n);
+
+        for(int i=0;i<n;i++){
+            if(mp.count(nums[i])){
+                ind[i] = mp[nums[i]].size();
+            }else{
+                ind[i] = 0;
+            }
+
+            mp[nums[i]].push_back(i);
+        }
+
+        for( auto &[key,value]:mp){
+            for(int i=1;i<value.size();i++){
+                value[i]+=value[i-1];
+            }
+        }
+
+        vector<ll>ans(n);
+
+        for(int i=0;i<n;i++){
+            ll idx = ind[i];
+            ll m = mp[nums[i]].size();
+
+            ll left = idx>0?mp[nums[i]][idx-1]:0;
+            ll right = mp[nums[i]][m-1]-mp[nums[i]][idx];
+
+            ans[i] = ((idx<<1)-m+1)*i-left+right;
+        }
+        // idx*i - (m-1-idx)*i
+        // (idx-(m-1-idx))*i
+        // (idx-m+1+idx)*i
+        // (idx*2-m+1)
+        return ans;
+    }
+};
