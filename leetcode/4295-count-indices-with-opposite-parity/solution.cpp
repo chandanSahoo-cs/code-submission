553d14a73a39c193764a8def856fe7d1cd7cb562
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-05-03 02:32:52 +0000

    [LeetCode Sync] - Count Indices With Opposite Parity - Runtime - 3 ms (0.00%), Memory - 32.6 MB (100.00%)

---
+++ leetcode/4295-count-indices-with-opposite-parity/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countOppositeParity(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>ans(n);
+        
+        for(int i=0;i<n;i++){
+            int cnt = 0;
+            for(int j=i+1;j<n;j++){
+                if((nums[i]&1) != (nums[j]&1)) cnt++;
+            }
+            ans[i] = cnt;
+        }
+
+        return ans;
+    }
+};
