b5fd684ff13a7849c67821b3d929035935af84de
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-05-03 04:28:54 +0000

    [LeetCode Sync] - Rotate String - Runtime - 0 ms (100.00%), Memory - 8.2 MB (36.35%)

--- leetcode/0812-rotate-string/solution.cpp
+++ leetcode/0812-rotate-string/solution.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.size();
-        int m  = goal.size();
-        if(n!=m) return false;
-        s+=s;
-        return s.find(goal) != string::npos;
+        if(s.size()!=goal.size()) return false;
+
+        string p = s+s;
+
+        return p.find(goal)!=string::npos;
     }
 };
