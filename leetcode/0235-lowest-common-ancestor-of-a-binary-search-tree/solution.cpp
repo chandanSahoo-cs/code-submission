ede1f97902768d1ecbbd253c3f1986c7f36db495
Chandan <chandansahoo02468@gmail.com>
2026-05-09 13:13:11 +0000

    [LeetCode Sync] - Lowest Common Ancestor of a Binary Search Tree - Runtime - 26 ms (29.45%), Memory - 23.5 MB (35.70%)

--- leetcode/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
+++ leetcode/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
@@ -10,22 +10,18 @@
 
 class Solution {
 public:
-    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
-
-        if(root==p || root==q) return root;
-        if(p->val<root->val && q->val>root->val) return root;
-
-        if(p->val<root->val && q->val<root->val) return lca(root->left,p,q);
-        
-        return lca(root->right,p,q);
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(p->val>q->val){
-            TreeNode* temp = p;
-            p = q;
-            q = temp;
+        TreeNode* curr = root;
+
+        while(true){
+            int vl = curr->val;
+            if(p->val<vl && q->val<vl){
+                curr = curr->left;
+            }else if(p->val>vl && q->val>vl){
+                curr = curr->right;
+            }else break;
         }
-        return lca(root,p,q);
+
+        return curr;
     }
 };
