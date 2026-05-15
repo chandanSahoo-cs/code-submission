0833f30318a60520bbec38b80cf96dc7b03639f5
Chandan <chandansahoo02468@gmail.com>
2026-05-15 09:56:56 +0000

    [LeetCode Sync] - Search in Rotated Sorted Array - Runtime - 0 ms (100.00%), Memory - 15.3 MB (37.38%)

--- leetcode/0033-search-in-rotated-sorted-array/solution.cpp
+++ leetcode/0033-search-in-rotated-sorted-array/solution.cpp
@@ -2,22 +2,36 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-
         int l = 0, r = n-1;
+
+        int pivot = r;
+
+        while(l<=r){
+            int m = l+(r-l)/2;
+
+            if(nums[m]>nums[r]) l=m+1;
+            else {
+                if(nums[pivot]>nums[m]){
+                    pivot = m;
+                }
+                r = m-1;
+            }
+        }
+
+        if(nums[0]<=target && pivot-1>=0 && nums[pivot-1]>=target){
+            l = 0, r = pivot-1;
+        }else{
+            l = pivot;
+            r = n-1;
+        }
+
 
         while(l<=r){
             int m = l+(r-l)/2;
 
             if(nums[m]==target) return m;
-            else if(nums[m]>nums[r]){
-                if(target>=nums[l] && target<nums[m]){
-                    r=m-1;
-                }else l=m+1;
-            }else{
-                if(target<=nums[r] && target>nums[m]){
-                    l=m+1;
-                }else r=m-1;
-            }
+            else if(nums[m]>target) r = m-1;
+            else l = m+1;
         }
 
         return -1;
