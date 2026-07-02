f8a1da7d6128d66637e735bac8016bf87a64c4b8
Chandan <chandansahoo02468@gmail.com>
2026-07-02 21:34:36 +0000

    [LeetCode Sync] - Minimum Window Substring - Runtime - 7 ms (60.80%), Memory - 11.9 MB (10.53%)

--- leetcode/0076-minimum-window-substring/solution.cpp
+++ leetcode/0076-minimum-window-substring/solution.cpp
@@ -1,32 +1,64 @@
 class Solution {
 public:
+
+    // bool cmp(unordered_map<char,int>&ref, unordered_map<char,int>&curr){
+
+    //     for(char c='a';c<='z';c++){
+    //         if(!ref.count(c)) continue;
+
+    //         if(!curr.count(c)) return false;
+    //         if(ref[c]>curr[c]) return false;
+    //     }
+
+
+    //     for(char c='A';c<='Z';c++){
+    //         if(!ref.count(c)) continue;
+
+    //         if(!curr.count(c)) return false;
+    //         if(ref[c]>curr[c]) return false;
+    //     }
+
+    //     return true;
+    // }
+
+
     string minWindow(string s, string t) {
-        int n = s.size(),m = t.size();
-        int l=0,r=0,mnl=0,mnr=-1,mnsz=INT_MAX;
+        int n = s.size(), m = t.size();
 
-        vector<int>v(256,0);
-        int cnt=0;
+        unordered_map<char,int>curr,ref;
 
-        for(auto ele:t){
-            v[ele]++;
+        for(auto c:t){
+            ref[c]++;
         }
+
+        int l = 0, r = 0;
+
+        int al=0,ar=n+1;
+
+        int cnt = 0;
 
         while(r<n){
-            if(v[s[r]]>0) cnt++;
-            v[s[r]]--;
+            curr[s[r]]++;
+
+            if(curr[s[r]]<=ref[s[r]]) cnt++;
+            
             while(cnt==m){
-                if(mnsz>r-l+1){
-                    mnsz = r-l+1;
-                    mnl=l;
-                    mnr=r;
+                if(ar-al+1>r-l+1){
+                    ar = r;
+                    al = l;
                 }
-                v[s[l]]++;
-                if(v[s[l]]>0) cnt--;
+
+                curr[s[l]]--;
+
+                if(curr[s[l]]<ref[s[l]]) cnt--; 
                 l++;
             }
+
             r++;
         }
+            
 
-        return s.substr(mnl,mnr-mnl+1);
+        if(ar==n+1) return "";
+        return s.substr(al,ar-al+1);
     }
 };
