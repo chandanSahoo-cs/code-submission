1c112b700b69aed6ef0708f2501ffb95c005837d
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-07-03 20:24:29 +0000

    [LeetCode Sync] - Frequency of the Most Frequent Element - Runtime - 9 ms (99.83%), Memory - 114.7 MB (11.67%)

--- leetcode/1966-frequency-of-the-most-frequent-element/solution.cpp
+++ leetcode/1966-frequency-of-the-most-frequent-element/solution.cpp
@@ -1,29 +1,47 @@
+#define ll long long
+
 class Solution {
-    #define ll long long
 public:
+    void countSort(vector<int>&nums){
+        int mx = *max_element(nums.begin(),nums.end());
+
+        vector<int>freq(mx+1);
+
+        for(auto ele:nums) freq[ele]++;
+
+        int j=0;
+
+        for(int i=0;i<=mx;i++){
+            while(freq[i]--){
+                nums[j++]=i;
+            }
+        }
+
+        return;
+    }
+
     int maxFrequency(vector<int>& nums, int k) {
-        ll n=nums.size();
-        sort(nums.begin(),nums.end());
-        vector<ll>prefix(n);
-        prefix[n-1]=nums[n-1];
-        for(ll i=n-2;i>=0;i--){
-            prefix[i]+=nums[i]+prefix[i+1];
-        }
-        ll cons=n-1,var=n-2;
-        ll cnt=1,ans=1;
-        ll prev=0;
-        while(cons>0 && var>=0){
-            if((cons-var+1)*nums[cons]-(prefix[var]-prev)<=k){
-                cnt++;
-                ans=max(ans,cnt);
-                var--;
+        int n = nums.size();
+
+        countSort(nums);
+
+        int l = 0, r = 0;
+        int ans = 0;
+
+        ll sum = 0;
+
+        while(r<n){
+            sum+=nums[r];
+
+            while((r-l+1)*1LL*nums[r]-sum>k){
+                sum-=nums[l];
+                l++;
             }
-            else{
-                prev=prefix[cons];
-                cons--;
-                cnt--;
-            }
+
+            ans = max(ans,r-l+1);
+            r++;
         }
+
         return ans;
     }
 };
