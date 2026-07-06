95d8246c7fbfe88781f31e62b172aa81c93942ff
Chandan <chandansahoo02468@gmail.com>
2026-07-06 21:05:49 +0530

    [Codeforces Sync] C - Grouping Increases

---
+++ codeforces/C - Grouping Increases/solution.cpp
@@ -0,0 +1,78 @@
+/*It is happening, right here and now*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+  vector<ll>a(n);
+ 
+  for(auto &ele:a) cin>>ele;
+ 
+  vector<int>x={mod},y={mod};
+  
+  int cnt = 0;
+ 
+  /*
+  if(a[i]<=mn) push a[i] in the vector with mn as the last element
+  if(a[i]>mx) push a[i] in the vector with mn as the last element
+  if(mx>=a[i] && a[i]>mn) push a[i] in the vector with mx as the last element
+  */
+ 
+  for(int i=0;i<n;i++){
+    int mx = max(x.back(),y.back());
+    int mn = min(x.back(),y.back());
+ 
+    if(mn>=a[i] || a[i]>mx){
+      if(mn==x.back()){
+        x.push_back(a[i]);
+      }else y.push_back(a[i]);
+ 
+      cnt+=(a[i]>mx);
+    }else{
+      if(mx==y.back()){
+        y.push_back(a[i]);
+      }else x.push_back(a[i]);
+    }
+  }
+ 
+  cout<<cnt<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
