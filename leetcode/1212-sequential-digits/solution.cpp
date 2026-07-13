2c17519a994327131d84ce8417805ff420afb157
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-07-13 08:42:57 +0000

    [LeetCode Sync] - Sequential Digits - Runtime - 0 ms (100.00%), Memory - 8.4 MB (42.92%)

--- leetcode/1212-sequential-digits/solution.cpp
+++ leetcode/1212-sequential-digits/solution.cpp
@@ -1,58 +1,26 @@
 class Solution {
-    #define ll long long
 public:
     vector<int> sequentialDigits(int low, int high) {
-        ll storeL=low;
-        ll sizeL=log10(low);
-        ll frstdgtL=storeL/pow(10,sizeL);
-        if(10-frstdgtL<sizeL+1){
-            sizeL++;
-            frstdgtL=1;
+        queue<int>q;
+
+        for(int i=1;i<=9;i++){
+            q.push(i);
         }
-        ll tempdd=frstdgtL+1;
-        while((log10(frstdgtL))<sizeL){
-            frstdgtL*=10;
-            frstdgtL+=(tempdd);
-            tempdd++;
-        }
-        ll nmbrTdd=1;
-        for(ll i=1;i<=sizeL;i++){
-            nmbrTdd=nmbrTdd*10+1;
-        }
-        if(frstdgtL<low) frstdgtL+=nmbrTdd;
-        int firstDigit = frstdgtL;
-        while (firstDigit >= 10) firstDigit /= 10;
-        if((10-firstDigit)<sizeL+1){
-            sizeL++;
-            frstdgtL=1;
-            tempdd=frstdgtL+1;
-            while((log10(frstdgtL))<sizeL){
-                frstdgtL*=10;
-                frstdgtL+=(tempdd);
-                tempdd++;
-            }
-        }
+
         vector<int>ans;
-        while(frstdgtL<=high){
-            if(frstdgtL%10==0) break;
-            // if(10-(frstdgtL/log10(frstdgtL))<sizeL) break;
-            ans.push_back(frstdgtL);
-            if(frstdgtL%10==9){
-                sizeL++;
-                frstdgtL=1;
-                tempdd=frstdgtL+1;
-                while(log10(frstdgtL)<sizeL){
-                    frstdgtL*=10;
-                    frstdgtL+=(tempdd);
-                    tempdd++;
-                }
-                if(frstdgtL<=high){
-                    ans.push_back(frstdgtL);
-                }
-                nmbrTdd=(nmbrTdd*10)+1;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            int d = u%10;
+
+            if(u>=low && u<=high){
+                ans.push_back(u);
             }
-            frstdgtL+=nmbrTdd;
+            if(d==9) continue;
+            q.push(u*10+d+1);
         }
+
         return ans;
     }
 };
