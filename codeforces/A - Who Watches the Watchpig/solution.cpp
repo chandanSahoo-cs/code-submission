5496678444988f5b2a3dc82b8619e9a725c90c78
Chandan <chandansahoo02468@gmail.com>
2026-07-17 00:12:40 +0530

    [Codeforces Sync] A - Who Watches the Watchpig?

---
+++ codeforces/A - Who Watches the Watchpig/solution.cpp
@@ -0,0 +1,61 @@
+/*It is happening, right here and now*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n,k; cin>>n>>k;
+  string s; cin>>s;
+ 
+  if(2*k>n){
+    cout<<-1<<"\n";
+    return;
+  }
+ 
+  ll sum = 0;
+  for(ll i=0;i<k;i++){
+    sum+=(s[i]=='L');
+    sum+=(s[n-i-1]=='R');
+  }
+ 
+  cout<<sum<<"\n";
+  return;  
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
