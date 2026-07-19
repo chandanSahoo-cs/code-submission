573d9259f9c1ad24ea4039ab82bad43ec2318fef
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-07-19 11:45:36 +0000

    [LeetCode Sync] - Remove Duplicate Letters - Runtime - 0 ms (100.00%), Memory - 9.4 MB (44.24%)

---
+++ leetcode/0316-remove-duplicate-letters/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+
+        vector<int>freq(26,0);
+
+        for(auto c:s) freq[c-'a']++;
+        unordered_set<char>st;
+
+        string t = "";
+
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']--;
+
+            if(st.count(s[i])) continue;
+
+            while(!t.empty() && t.back()>s[i] && freq[t.back()-'a']){
+                st.erase(t.back());
+                t.pop_back();
+            }
+
+            st.insert(s[i]);
+            t+=s[i];
+        }
+
+        return t;
+    }
+};
