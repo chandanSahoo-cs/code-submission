20dd7a87d64db1e4f4aaa85e541f495a7af88201
Chandan <chandansahoo02468@gmail.com>
2026-07-24 22:45:31 +0530

    [Codeforces Sync] B - Restore the Permutation by Merger

---
+++ codeforces/B - Restore the Permutation by Merger/solution.cpp
@@ -0,0 +1,62 @@
+/*It is happening, right here and now*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+  vector<ll>a(2*n);
+ 
+  for(auto &ele:a) cin>>ele;
+ 
+  vector<int>mark(n+1);
+  
+  for(auto ele:a){
+    if(!mark[ele]){
+      cout<<ele<<" ";
+      mark[ele] = 1;
+    }
+  }
+ 
+  cout<<"\n";
+ 
+  return;
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
