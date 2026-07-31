8fe863d0d069cfdf054869c56ab7325df827ab0f
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2026-07-31 20:51:48 +0000

    [LeetCode Sync] - Course Schedule II - Runtime - 1 ms (80.59%), Memory - 18.3 MB (57.00%)

--- leetcode/0210-course-schedule-ii/solution.cpp
+++ leetcode/0210-course-schedule-ii/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
         vector<vector<int>>adj(n);
-        vector<int>in(n);
-
-        for(auto &ele:pre){
+        vector<int>indegree(n);
+        for(auto &ele:p){
             adj[ele[1]].push_back(ele[0]);
-            in[ele[0]]++;
+            indegree[ele[0]]++;
         }
 
         queue<int>q;
         vector<int>ans;
 
         for(int i=0;i<n;i++){
-            if(!in[i]){
+            if(indegree[i]==0){
                 q.push(i);
                 ans.push_back(i);
             }
@@ -24,8 +23,8 @@ public:
             q.pop();
 
             for(auto v:adj[u]){
-                in[v]--;
-                if(!in[v]){
+                indegree[v]--;
+                if(indegree[v]==0){
                     q.push(v);
                     ans.push_back(v);
                 }
@@ -34,11 +33,6 @@ public:
 
         if(ans.size()<n) return {};
 
-        // reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-
-/*
-1->2->3->4->5->6
-*/
