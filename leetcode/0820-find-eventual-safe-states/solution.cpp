938d1a910ae94cf32f9c7706247806d8fda2e35f
Chandan <chandansahoo02468@gmail.com>
2026-07-31 21:38:56 +0000

    [LeetCode Sync] - Find Eventual Safe States - Runtime - 3 ms (94.23%), Memory - 53.1 MB (65.98%)

--- leetcode/0820-find-eventual-safe-states/solution.cpp
+++ leetcode/0820-find-eventual-safe-states/solution.cpp
@@ -1,24 +1,22 @@
-/*Personal note: Meine yeh mark krdiya ki yeh node visited hai, but sath hi sath meine yeh unmark kridya ki woh cycle ka part nhi (agar hai toh)*/
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adj, vector<int>&vis, vector<int>&pathVis, vector<int>&ans, int curr){
+    vector<int>ans;
+    bool dfs(vector<vector<int>>&adj, vector<int>&vis, vector<int>&pathVis, int curr){
         if(pathVis[curr]) return true;
         if(vis[curr]) return false;
 
-        vis[curr]=1;
-        pathVis[curr]=1;
+        pathVis[curr] = vis[curr] = 1;
 
         bool flag = false;
-        for(auto ele:adj[curr]){
-            flag |= dfs(adj,vis,pathVis,ans,ele);
+        for(auto v:adj[curr]){
+            flag |= dfs(adj,vis,pathVis,v);
             if(flag) break;
         }
-        
+
         if(!flag){
             ans.push_back(curr);
-            pathVis[curr]=0;
+            pathVis[curr] = 0;
         }
-
 
         return flag;
     }
@@ -26,18 +24,14 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
 
-        vector<int>vis(n);
-        vector<int>pathVis(n);
-
-        vector<int>ans;
+        vector<int>vis(n),pathVis(n);
 
         for(int i=0;i<n;i++){
             if(vis[i]) continue;
-            dfs(graph,vis,pathVis,ans,i);
+            dfs(graph,vis,pathVis,i);
         }
 
         sort(ans.begin(),ans.end());
-
 
         return ans;
     }
