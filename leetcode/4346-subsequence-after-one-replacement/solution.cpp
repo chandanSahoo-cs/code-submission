43c6097e1a78513274002352536fa727d2322f72
Chandan <chandansahoo02468@gmail.com>
2026-08-08 14:15:28 +0000

    [LeetCode Sync] - Subsequence After One Replacement - Runtime - 20 ms (55.43%), Memory - 43.5 MB (43.58%)

--- leetcode/4346-subsequence-after-one-replacement/solution.cpp
+++ leetcode/4346-subsequence-after-one-replacement/solution.cpp
@@ -7,33 +7,34 @@ public:
         vector<int>pref(n,m);
         vector<int>suff(n,-1);
 
-        int i=0;
+        int i = 0;
 
         for(int j=0;j<m;j++){
-            if(i==n) break;
             if(s[i]==t[j]){
-                pref[i]=j;
+                pref[i] = j;
                 i++;
             }
+            if(i>=n) break;
         }
 
         i = n-1;
 
         for(int j=m-1;j>=0;j--){
-            if(i==-1) break;
             if(s[i]==t[j]){
-                suff[i]=j;
+                suff[i] = j;
                 i--;
             }
+
+            if(i<0) break;
         }
-        
+
         for(int i=0;i<n;i++){
-            int left = i==0?-1:pref[i-1];
-            int right = i==n-1?m:suff[i+1];
+            int left = i-1<0?-1:pref[i-1];
+            int right = i+1==n?m:suff[i+1];
 
             if(right-left>=2) return true;
-        }
-        
+        }   
+
         return false;
     }
 };
