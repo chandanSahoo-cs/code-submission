8fa397f1270ea3319d8984e6c23069e531e80092
Chandan <chandansahoo02468@gmail.com>
2026-08-25 22:31:10 +0530

    [Codeforces Sync] C - Johnny and Another Rating Drop

---
+++ codeforces/C - Johnny and Another Rating Drop/solution.cpp
@@ -0,0 +1,62 @@
+/*It is happening, right here and now*/
+        
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
+#define all(v) v.begin(),v.end()
+#define lcd(a,b) a*(b/__gcd(a,b))
+#define ll long long
+ 
+const int mod = 1e9+7;
+ 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+ 
+void realmsDomain(){
+  ll n; cin>>n;
+ 
+  ll sum = 1;
+  ll cnt = (n+1)/2;
+  deb(cnt);
+  ll i = 0;
+ 
+  while(sum+(1LL<<i)<=n){
+    sum+=(1LL<<i);
+    deb(sum);
+    ll div = (1LL<<(i+2));
+ 
+    cnt+=((n-sum)/div)*(i+2)+(i+2);
+    i++;
+  }
+ 
+  cout<<cnt<<"\n";
+}
+ 
+int main() {
+    clock_t time_req = clock();
+    velociraptor
+ 
+ 
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+ 
+    ll tsts = 1 ; 
+ 
+    cin>>tsts;    
+ 
+    for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
+        realmsDomain();
+    }
+ 
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+ 
+    return 0;
+}
